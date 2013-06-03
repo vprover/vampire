c714122f9f0940a4fed3002b0f66ef9b1ceb15eb
Andrei Voronkov <andrei.voronkov@gmail.com>
2013-06-03 19:19:57 +0000

    qq

--- CASC/SpawningCM.cpp
+++ CASC/SpawningCM.cpp
@@ -22,18 +22,18 @@
 using namespace CASC;
 
 SpawningCM::SpawningCM(string executable)
-: _executable(executable)
+  : _executable(executable)
 {
   CALL("SpawningCM::SpawningCM");
 
-  if(!system(0)) {
+  if (!system(0)) {
     USER_ERROR("The spawning CASC mode is not supported on this system (the \"int system(const char *)\" function is not available)");
   }
 
-  if(env.options->inputFile()=="") {
+  if (env.options->inputFile() == "") {
     USER_ERROR("Value of the --input_file option must be specified for the CASC mode in Windows.");
   }
-  _inputFile=env.options->inputFile();
+  _inputFile = env.options->inputFile();
 
   //we just need to extract property from the problem
   ScopedPtr<Problem> prb(UIHelper::getInputProblem(*env.options));
@@ -44,12 +44,11 @@ bool SpawningCM::runSlice(Options& opt)
 {
   CALL("SpawningCM::runSlice");
 
-  string strategy=opt.generateTestId();
+  string strategy = opt.generateTestId();
+  string cmdLine = _executable + " --decode " + strategy + " --input_file " + _inputFile;
 
-  string cmdLine=_executable+" -decode "+strategy+" -input_file "+_inputFile;
-
-  if(env.options->include()!="") {
-    cmdLine+=" -include "+env.options->include();
+  if (env.options->include() != "") {
+    cmdLine += " --include " + env.options->include();
   }
 
 #if COMPILER_MSVC
@@ -60,32 +59,32 @@ bool SpawningCM::runSlice(Options& opt)
   System::ignoreSIGINT();
 #endif
 
-  int res=system(cmdLine.c_str());
+  int res = system(cmdLine.c_str());
 
 #if COMPILER_MSVC
   //restore signal handler for Ctrl+C
   System::heedSIGINT();
 
-  if(res==3)  {
+  if (res == 3)  {
     //if child Vampire was terminated by SIGINT (Ctrl+C), we also terminate
     //(3 is the return value for this case; see documentation for the
     //@b vampireReturnValue global variable)
     handleSIGINT();
   }
 
-  if(res==0) {
+  if (res == 0) {
     return true;
   }
 #else
-  if( (WIFSIGNALED(res) && WTERMSIG(res)==SIGINT) ||
-      (WIFEXITED(res) && WEXITSTATUS(res)==3) )  {
+  if ( (WIFSIGNALED(res) && WTERMSIG(res) == SIGINT) ||
+      (WIFEXITED(res) && WEXITSTATUS(res) == 3) )  {
     //if child Vampire was terminated by SIGINT (Ctrl+C), we also terminate
     //(3 is the return value for this case; see documentation for the
     //@b vampireReturnValue global variable)
     handleSIGINT();
   }
 
-  if(WIFEXITED(res) && WEXITSTATUS(res)==0) {
+  if (WIFEXITED(res) && WEXITSTATUS(res) == 0) {
     //if Vampire succeeds, its return value is zero
     return true;
   }
