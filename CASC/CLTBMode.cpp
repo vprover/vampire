85c90590c4ff3781c15718b32d7936300f7656b3
Andrei Voronkov <andrei.voronkov@gmail.com>
2013-06-05 16:19:28 +0000

    qq

--- CASC/CLTBMode.cpp
+++ CASC/CLTBMode.cpp
@@ -1590,7 +1590,6 @@ void CLTBProblem::perform(int terminationTime)
 
   System::registerForSIGHUPOnParentDeath();
 
-  env.timer->start();
   env.timer->makeChildrenIncluded();
   TimeCounter::reinitialize();
 
@@ -1898,7 +1897,6 @@ void CLTBProblem::runSlice(Options& strategyOpt)
   UIHelper::cascModeChild=true;
 
   int resultValue=1;
-  env.timer->start();
   TimeCounter::reinitialize();
   Timer::setTimeLimitEnforcement(true);
 
