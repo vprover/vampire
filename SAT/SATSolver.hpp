65f1e842e43be9603ffeb2b977dd619be7aea2c6
Martin Suda <sudam@cs.man.ac.uk>
2014-09-11 12:06:20 +0100

    Corrected wrong implementation of addAssumptions(SATLiteral,bool)

--- SAT/SATSolver.hpp
+++ SAT/SATSolver.hpp
@@ -77,12 +77,7 @@ public:
 
   void addAssumption(SATLiteral lit, bool onlyPropagate=false) {
     CALL("SATSolver::addAssumption(SATLiteral,bool)");
-    if(onlyPropagate) {
-    addAssumption(lit, false);
-    }
-    else {
-    	addAssumption(lit, UINT_MAX);
-    }
+    addAssumption(lit, onlyPropagate ? 0 : UINT_MAX);
   }
   /**
    * Add an assumption into the solver. If conflictCountLimit==0,
