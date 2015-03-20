0adf4b14250731f364bef18688f22157b8db4315
Giles <--global>
2015-03-20 11:42:53 +0000

    final debug of distinct expansion

--- Shell/DistinctGroupExpansion.cpp
+++ Shell/DistinctGroupExpansion.cpp
@@ -56,7 +56,7 @@ bool DistinctGroupExpansion::apply(UnitList*& units)
 
   for(unsigned i=0;i<group_members.size();i++){
     Stack<unsigned>* members = group_members[i];
-    if(members->size() > 1){
+    if(members->size() > 0){
  
       // If the non-empty distinct group represents numbers then we need to keep
       // the distinct processing later as new numbers can be generated from the
@@ -65,7 +65,7 @@ bool DistinctGroupExpansion::apply(UnitList*& units)
 
       // This 5 is a magic number, if it is changed then the corresponding
       // 6 should be changed in Kernel::Signature::Symol::addToDistinctGroup
-      if( (members->size() < 5 || env.options->bfnt())){
+      if( members->size()>1 && ((members->size() < 5 || env.options->bfnt()))){
         added=true;
         Formula* expansion = expand(*members);
         if(env.options->showPreprocessing()){
