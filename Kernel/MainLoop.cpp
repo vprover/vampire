207c31cca875c2bd913407335a4b9d7bb1643e5b
Giles <--global>
2015-06-02 13:10:02 +0100

    Fix user error

--- Kernel/MainLoop.cpp
+++ Kernel/MainLoop.cpp
@@ -138,7 +138,7 @@ MainLoop* MainLoop::createFromOptions(Problem& prb, const Options& opt)
     res = new IGAlgorithm(prb, opt);
     break;
   case Options::SaturationAlgorithm::FINITE_MODEL_BUILDING:
-    if(prb.hasInterpretedOperations()){ 
+    if(prb.hasInterpretedOperations() || env.sorts->hasSort()){ 
       USER_ERROR("Finite Model Builder (sa=fmb) cannot be used with Theory problems"); 
     }
     res = new FiniteModelBuilder(prb,opt);
