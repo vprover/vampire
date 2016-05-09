9ea29ef7708089016867030202a77af948be7d80
Giles <giles.reger@manchester.ac.uk>
2016-05-10 00:27:16 +0100

    adding new header files

--- z3/api/z3.h
+++ z3/api/z3.h
@@ -15,29 +15,23 @@ Author:
     Leonardo de Moura (leonardo) 2007-06-8
 
 Notes:
-    
+
 --*/
 
-#ifndef _Z3__H_
-#define _Z3__H_
+#ifndef Z3_H_
+#define Z3_H_
 
 #include<stdio.h>
 #include"z3_macros.h"
 #include"z3_api.h"
+#include"z3_ast_containers.h"
 #include"z3_algebraic.h"
 #include"z3_polynomial.h"
 #include"z3_rcf.h"
+#include"z3_fixedpoint.h"
+#include"z3_optimization.h"
 #include"z3_interp.h"
-
-#undef __in
-#undef __out
-#undef __inout
-#undef __in_z
-#undef __out_z
-#undef __ecount
-#undef __in_ecount
-#undef __out_ecount
-#undef __inout_ecount
+#include"z3_fpa.h"
 
 #endif
 
