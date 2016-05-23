85c10c2cb9560e9c37394822381a73c608f51332
Giles <--global>
2016-05-23 10:58:36 +0100

    add

---
+++ SAT/Z3MainLoop.hpp
@@ -0,0 +1,48 @@
+/**
+ * @file Z3MainLoop.hpp
+ * Defines class Z3MainLoop.
+ */
+
+#ifndef __Z3MainLoop__
+#define __Z3MainLoop__
+
+#if VZ3
+
+#include "Forwards.hpp"
+
+#include "Lib/Allocator.hpp"
+
+#include "Kernel/MainLoop.hpp"
+#include "Kernel/Problem.hpp"
+
+#include "SAT/Z3Interfacing.hpp"
+
+#include "Shell/Options.hpp"
+#include "Shell/Statistics.hpp"
+
+namespace SAT{
+
+using namespace Kernel;
+using namespace Shell;
+using namespace Lib;
+
+class Z3MainLoop : public MainLoop 
+{
+public:
+  CLASS_NAME(Z3MainLoop);
+  USE_ALLOCATOR(Z3MainLoop);  
+  
+  Z3MainLoop(Problem& prb, const Options& opt);
+  ~Z3MainLoop();
+
+protected:
+  virtual void init();
+  virtual MainLoopResult runImpl();
+//private:
+
+};
+
+} // end SAT namespace
+
+#endif // if VZ3
+#endif // __Z3MainLoop__
