4360c9e7033c406f2df2ee566c6524291b5dabb9
Simon Robillard <simon.robillard@chalmers.se>
2017-09-29 14:26:48 +0200

    change List::destroy() to static function to allow calls on empty lists

--- Debug/RuntimeStatistics.cpp
+++ Debug/RuntimeStatistics.cpp
@@ -28,7 +28,7 @@ void RSMultiCounter::print(ostream& out)
 RSMultiStatistic::~RSMultiStatistic()
 {
   for(size_t i=0;i<_values.size();i++) {
-    _values[i]->destroy();
+    ValList::destroy(_values[i]);
   }
 }
 
