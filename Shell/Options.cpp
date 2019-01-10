74d2f5a57760d452f09641cc1545b19bc837fe9e
Martin Suda <sudam@cs.man.ac.uk>
2019-01-10 15:16:29 +0100

    option name typo

--- Shell/Options.cpp
+++ Shell/Options.cpp
@@ -842,7 +842,7 @@ void Options::Options::init()
     _ageWeightRatio.reliesOn(_saturationAlgorithm.is(notEqual(SaturationAlgorithm::INST_GEN))->Or<int>(_instGenWithResolution.is(equal(true))));
     _ageWeightRatio.setRandomChoices({"8:1","5:1","4:1","3:1","2:1","3:2","5:4","1","2:3","2","3","4","5","6","7","8","10","12","14","16","20","24","28","32","40","50","64","128","1024"});
 
-    _ageWeightBlending = ChoiceOptionValue<AgeWeightBlending>("age_weigh_blending","awb",
+    _ageWeightBlending = ChoiceOptionValue<AgeWeightBlending>("age_weight_blending","awb",
                           AgeWeightBlending::QUEUES,{"linear","loglinear","queues"});
     _ageWeightBlending.tag(OptionTag::SATURATION);
     _lookup.insert(&_ageWeightBlending);
