c6c4e8447bf8c00ea0ff3499f4283b90b9328db2
Martin Suda <sudam@cs.man.ac.uk>
2019-02-09 15:07:25 +0100

    disable global new and delete here. Who cares anyway?

--- Lib/Allocator.cpp
+++ Lib/Allocator.cpp
@@ -1080,6 +1080,7 @@ unsigned VAllocator::Descriptor::hash (const void* addr)
 
 #if VDEBUG
 
+ /**
 void* operator new(size_t sz) {    
   ASS_REP(VAllocator::_tolerantZone > 0,"Attempted to use global new operator, thus bypassing VAllocator!");
   // Please read: https://github.com/easychair/vampire/wiki/Attempted-to-use-global-new-operator,-thus-bypassing-VAllocator!
@@ -1135,6 +1136,7 @@ void operator delete[](void* obj) throw() {
     DEALLOC_UNKNOWN(obj,"global new[]");
   }
 }
+*/
 
 #endif
 
