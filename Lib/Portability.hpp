73af83e739778cffa523cd89b0972e7197c2221f
Martin Suda <sudam@cs.man.ac.uk>
2019-02-11 11:31:10 +0100

    defining macro to 0 is evil (other libs may check for definednedness)

--- Lib/Portability.hpp
+++ Lib/Portability.hpp
@@ -24,6 +24,7 @@
 //////////////////////////////////////////////////////
 // Detect compiler
 
+/*
 #ifndef __APPLE__
 # define __APPLE__ 0
 #endif
@@ -31,6 +32,7 @@
 #ifndef __CYGWIN__
 # define __CYGWIN__ 0
 #endif
+*/
 
 //////////////////////////////////////////////////////
 // Detect architecture
