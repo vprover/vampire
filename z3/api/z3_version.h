a5a195dc101020802f4f86a0c79497b7d0fefb4e
Martin <marty@logic.at>
2019-04-17 11:16:21 +0200

    add missing files to z3 api

---
+++ z3/api/z3_version.h
@@ -0,0 +1,8 @@
+// automatically generated file.
+#define Z3_MAJOR_VERSION   4
+#define Z3_MINOR_VERSION   8
+#define Z3_BUILD_NUMBER    4
+#define Z3_REVISION_NUMBER 0
+
+#define Z3_FULL_VERSION    "4.8.4.0"
+/* #undef Z3GITHASH */
