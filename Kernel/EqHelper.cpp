517ad0a24cdbbff0fbf488d00f022bdd117c6510
Martin Suda <sudam@cs.man.ac.uk>
2019-08-20 13:35:40 +0200

    rewriteblas are only the non-small sides of equalities, even for negative ones

--- Kernel/EqHelper.cpp
+++ Kernel/EqHelper.cpp
@@ -180,7 +180,7 @@ TermIterator EqHelper::getRewritableSubtermIterator(Literal* lit, const Ordering
 {
   CALL("EqHelper::getRewritableSubtermIterator");
 
-  if (lit->isEquality() && lit->isPositive()) {
+  if (lit->isEquality()) {
     TermList sel;
     switch(ord.getEqualityArgumentOrder(lit)) {
     case Ordering::INCOMPARABLE:
