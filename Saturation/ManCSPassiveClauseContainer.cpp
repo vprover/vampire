c212f688f657dd22cc1c7e3f26ed0358ebf6be92
Bernhard Gleiss <bgleiss@forsyte.tuwien.ac.at>
2019-10-14 13:49:00 +0200

    add license preamble

--- Saturation/ManCSPassiveClauseContainer.cpp
+++ Saturation/ManCSPassiveClauseContainer.cpp
@@ -1,4 +1,25 @@
 
+/*
+ * File ManCSPassiveClauseContainer.cpp.
+ *
+ * This file is part of the source code of the software program
+ * Vampire. It is protected by applicable
+ * copyright laws.
+ *
+ * This source code is distributed under the licence found here
+ * https://vprover.github.io/license.html
+ * and in the source directory
+ *
+ * In summary, you are allowed to use Vampire for non-commercial
+ * purposes but not allowed to distribute, modify, copy, create derivatives,
+ * or use in competitions. 
+ * For other uses of Vampire please contact developers for a different
+ * licence, which we will make an effort to provide. 
+ */
+/**
+ * @file ManCSPassiveClauseContainer.cpp
+ * Defines the class ManCSPassiveClauseContainer
+ */
 #include <iostream>
 #include "ManCSPassiveClauseContainer.hpp"
 #include "Lib/VirtualIterator.hpp"
--- Saturation/ManCSPassiveClauseContainer.hpp
+++ Saturation/ManCSPassiveClauseContainer.hpp
@@ -1,3 +1,25 @@
+/*
+ * File ManCSPassiveClauseContainer.hpp.
+ *
+ * This file is part of the source code of the software program
+ * Vampire. It is protected by applicable
+ * copyright laws.
+ *
+ * This source code is distributed under the licence found here
+ * https://vprover.github.io/license.html
+ * and in the source directory
+ *
+ * In summary, you are allowed to use Vampire for non-commercial
+ * purposes but not allowed to distribute, modify, copy, create derivatives,
+ * or use in competitions. 
+ * For other uses of Vampire please contact developers for a different
+ * licence, which we will make an effort to provide. 
+ */
+/**
+ * @file ManCSPassiveClauseContainer.hpp
+ * Defines the class ManCSPassiveClauseContainer
+ */
+
 #ifndef __MANCSPASSIVECLAUSECONTAINER__
 #define __MANCSPASSIVECLAUSECONTAINER__
 
