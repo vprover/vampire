ae252fb42b91d0554dfdee5491096f89461fd1c7
Martin Suda <sudam@cs.man.ac.uk>
2020-01-31 16:43:41 +0100

    getting ready for avatar

--- Saturation/SaturationAlgorithm.hpp
+++ Saturation/SaturationAlgorithm.hpp
@@ -50,6 +50,8 @@
 #include<iostream>
 #endif
 
+#define DEBUG_MODEL 1
+
 namespace Saturation
 {
 
@@ -175,11 +177,11 @@ private:
   class PartialSimplificationPerformer;
 
   static SaturationAlgorithm* s_instance;
-protected:
+public:
   torch::jit::script::Module _model;
   DHSet<Clause*> _reported; // set of clauses the _model already knows about
   void talkToKarel(Clause*,bool eval = true);
-
+protected:
   bool _completeOptionSettings;
   int _startTime;
   bool _clauseActivationInProgress;
