a1812512f980104a04aa506ceb9acae26ce2d38d
Johannes Schoisswohl <johannes.sch@gmx.net>
2020-03-24 16:03:45 +0000

    reformatting

--- vtest.cpp
+++ vtest.cpp
@@ -2,18 +2,25 @@
 /*
  * File vtest.cpp.
  *
- * This file is part of the source code of the software program
+ * This file is part of the source code of the software
+ * program
  * Vampire. It is protected by applicable
  * copyright laws.
  *
+ *
  * This source code is distributed under the licence found here
+ *
  * https://vprover.github.io/license.html
  * and in the source directory
  *
+ *
  * In summary, you are allowed to use Vampire for non-commercial
- * purposes but not allowed to distribute, modify, copy, create derivatives,
- * or use in competitions. 
- * For other uses of Vampire please contact developers for a different
+ * purposes but
+ * not allowed to distribute, modify, copy, create derivatives,
+ * or use in
+ * competitions. 
+ * For other uses of Vampire please contact developers for a
+ * different
  * licence, which we will make an effort to provide. 
  */
 /**
@@ -112,26 +119,17 @@ int main(int argc, char *argv[]) {
   readAndFilterGlobalOpts(args);
 
   try {
-    auto& tests = *UnitTesting::instance();
+
     if (args.size() == 1) {
-      bool ok = UnitTesting::instance()->runAllTests(cout);
-      if (!ok) {
-        exit(-1);
-      }
+      UnitTesting::instance()->runAllTests(cout);
     } else if (args.size() == 2 || args.size() == 3) {
       if (!strcmp(args[1], "-l")) {
-        tests.printTestNames(cout);
+        UnitTesting::instance()->printTestNames(cout);
       } else {
-        auto unit = tests.get(args[args.size() - 1]);
-        if (!unit) {
+        if (!UnitTesting::instance()->runTest(args[args.size() - 1], cout)) {
           cout << "Unknown test name: " << args[1] << endl;
           cout << "Run \"" << args[0]
                << " -l\" for the list of available tests." << endl;
-        } else {
-          bool ok = tests.runUnit(unit, cout);
-          if (!ok) {
-            exit(-1);
-          }
         }
       }
     } else {
