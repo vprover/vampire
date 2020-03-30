98c285cadcf8101c965c5208a85ace7176188383
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2020-03-30 19:52:33 +0100

    added DBG macro and related classes

---
+++ Debug/PrintDebug.hpp
@@ -0,0 +1,74 @@
+
+#ifndef __PRINT_DEBUG_HPP__
+#define __PRINT_DEBUG_HPP__
+
+#include <iostream>
+
+#if VDEBUG
+#define DBG(...) Debug::dbg(#__VA_ARGS__, " = ", __VA_ARGS__);
+#else 
+#define DBG(...) 
+#endif 
+#include "Lib/vstd.h"
+
+namespace Debug {
+
+template <class A> struct print_debug {
+  void operator()(ostream &out, const A &a) const { out << a; }
+};
+
+template <class A> struct print_debug<vvec<A>> {
+  void operator()(ostream &out, const vvec<A> &a) const {
+    out << "[";
+    auto i = a.begin();
+    if (i != a.end()) {
+      out << *i;
+      i++;
+      for (; i != a.end(); i++) {
+        out << ", " << *i;
+      }
+    }
+    out << "]";
+  }
+};
+
+template <class A> struct print_debug<refw<A>> {
+  void operator()(ostream &out, const refw<A> &a) const {
+    print_debug<A>{}(out, a.get());
+  }
+};
+
+
+template <class A> struct print_debug<rc<A>> {
+  void operator()(ostream &out, const rc<A> &a) const {
+    print_debug<A>{}(out, *a.get());
+  }
+};
+
+template <class... As> struct dbgr {
+  static void debug(ostream &out, const As &... as);
+};
+
+template <> struct dbgr<> {
+  static void debug(ostream &out) {}
+};
+
+template <class A, class... As> struct dbgr<A, As...> {
+  static void debug(ostream &out, const A &a, const As &... as) {
+    print_debug<A>{}(out, a);
+    dbgr<As...>::debug(out, as...);
+  }
+};
+
+template <class... As> void dbg(const As &... as) {
+#if VDEBUG
+  std::cout << "[ dbg  ]\t";
+  dbgr<As...>::debug(std::cout, as...);
+  std::cout << std::endl;
+#endif 
+}
+
+}
+
+
+#endif // __PRINT_DEBUG_HPP__
