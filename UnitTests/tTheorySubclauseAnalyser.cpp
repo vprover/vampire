2832d211e3c0c93d83495f75944c26d1f7e65616
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2020-03-30 19:52:01 +0100

    bug fixes

--- UnitTests/tTheorySubclauseAnalyser.cpp
+++ UnitTests/tTheorySubclauseAnalyser.cpp
@@ -21,6 +21,7 @@
 #include "Lib/Environment.hpp"
 #include "Shell/Analysis/TheorySubclauseAnalyser.hpp"
 #include "Test/UnitTesting.hpp"
+#include "Debug/PrintDebug.hpp"
 
 #define UNIT_ID TheorySubclauseAnalyser
 UT_CREATE;
@@ -31,16 +32,22 @@ using namespace Kernel;
 using namespace Shell;
 using namespace Inferences;
 
-#define $plus(...)                                                             \
-  (AbsTerm::fun(plus, vvec<refw<AbsTerm>>{__VA_ARGS__})) // TODO
-#define $times(...)                                                            \
-  (AbsTerm::fun(times, vvec<refw<AbsTerm>>{__VA_ARGS__})) // TODO
 #define $leq(...)                                                              \
   (create_abs_lit(true, sym_leq, vvec<refw<AbsTerm>>{__VA_ARGS__})) // TODO
+#define $equal(...)                                                             \
+  (create_abs_lit(true, 0,  vvec<refw<AbsTerm>>{__VA_ARGS__})) // TODO
+
+#define $sum(...)                                                             \
+  (AbsTerm::fun(sum, vvec<refw<AbsTerm>>{__VA_ARGS__})) // TODO
+#define $times(...)                                                            \
+  (AbsTerm::fun(times, vvec<refw<AbsTerm>>{__VA_ARGS__})) // TODO
 #define f(...)                                                                 \
   (AbsTerm::fun(symbol_f, vvec<refw<AbsTerm>>{__VA_ARGS__})) // TODO
 #define g(...)                                                                 \
-  (AbsTerm::fun(symbol_g, vvec<refw<AbsTerm>>{__VA_ARGS__})) // TODO
+  (AbsTerm::fun(symbol_g, vvec<refw<AbsTerm>>{__VA_ARGS__}))
+
+#define n(i) (AbsTerm::fun(env.signature->addIntegerConstant(0), vvec<refw<AbsTerm>>{}))
+
 #define x0 (AbsTerm::var(0))
 #define x1 (AbsTerm::var(1))
 #define x2 (AbsTerm::var(2))
@@ -64,6 +71,7 @@ using namespace Inferences;
 #define x20 (AbsTerm::var(20))
 #define x21 (AbsTerm::var(21))
 
+
 #define SETUP                                                                  \
   unsigned symbol_f = env.signature->addFunction("f", 1);                      \
   unsigned symbol_g = env.signature->addFunction("g", 1);                      \
@@ -71,45 +79,46 @@ using namespace Inferences;
       Interpretation::INT_LESS_EQUAL, "$leq");                                 \
   unsigned times = env.signature->addInterpretedFunction(                      \
       Interpretation::INT_MULTIPLY, "$times");                                 \
-  unsigned plus = env.signature->addInterpretedFunction(                       \
-      Interpretation::INT_PLUS, "$plus");
+  unsigned sum = env.signature->addInterpretedFunction(                       \
+      Interpretation::INT_PLUS, "$sum");
 
 TEST_FUN(test_cmp_basic_term){
 
-  SETUP
+    SETUP
 
 #define TO_TEST cmp2, cmp3, cmp4
 
 #define DO_TEST(cmp_i)                                                         \
   {                                                                            \
     CALL(#cmp_i)                                                               \
-    auto eq = [=](AbsTerm &l, AbsTerm &r) -> bool {                 \
-      return cmp_i<AbsLiteral>{}($leq(l), $leq(r)) == CMP_EQUIV;                              \
+    auto eq = [=](AbsTerm &l, AbsTerm &r) -> bool {                            \
+      return cmp_i<AbsLiteral>{}($leq(l), $leq(r)) == CMP_EQUIV;               \
     };                                                                         \
                                                                                \
-    ASS(eq($plus(x1, $plus(x2, x3)), $plus(x1, $plus(x2, x3))))                \
-    ASS(!eq($plus(x1, $times(x2, x3)), $times(x1, $plus(x2, x3))))             \
+    ASS(eq($sum(x1, $sum(x2, x3)), $sum(x1, $sum(x2, x3))))                \
+    ASS(!eq($sum(x1, $times(x2, x3)), $times(x1, $sum(x2, x3))))             \
   }
 
-  MAP(DO_TEST, TO_TEST)
+        MAP(DO_TEST, TO_TEST)
 
 #undef DO_TEST
 #undef TO_TEST
 }
 
-TEST_FUN(test_cmp_rectified_term){
-  SETUP
+TEST_FUN(test_cmp_rectified_term){SETUP
 
 #define TO_TEST cmp3, cmp4
 
 #define DO_TEST(cmp_i)                                                         \
   {                                                                            \
     CALL(#cmp_i)                                                               \
-    auto eq = [=](AbsTerm &l, AbsTerm &r) -> bool {                 \
-      return cmp_i<AbsLiteral>{}($leq(l), $leq(r)) == CMP_EQUIV;                              \
+    auto eq = [=](AbsTerm &l, AbsTerm &r) -> bool {                            \
+      l.rectify();                                                             \
+      r.rectify();                                                             \
+      return cmp_i<AbsLiteral>{}($leq(l), $leq(r)) == CMP_EQUIV;               \
     };                                                                         \
                                                                                \
-    ASS(eq($plus(x2, $times(x19, x7), x1), $plus(x5, $times(x3, x0), x2)))     \
+    ASS(eq($sum(x2, $times(x19, x7), x1), $sum(x5, $times(x3, x0), x2)))     \
   }
 
                                       MAP(DO_TEST, TO_TEST)
@@ -125,14 +134,14 @@ TEST_FUN(test_cmp_rectified_literal){SETUP
 #define DO_TEST(cmp_i)                                                         \
   {                                                                            \
     CALL(#cmp_i)                                                               \
-    auto eq = [=](const AbsLiteral &l, const AbsLiteral &r) -> bool {           \
+    auto eq = [=](const AbsLiteral &l, const AbsLiteral &r) -> bool {          \
       return cmp_i<AbsLiteral>{}(l, r) == CMP_EQUIV;                           \
     };                                                                         \
                                                                                \
-    ASS(!eq($leq($plus(x1, x2), x2), $leq($plus(x1, x2), x3)))                 \
+    ASS(!eq($leq($sum(x1, x2), x2), $leq($sum(x1, x2), x3)))                 \
   }
 
-  MAP(DO_TEST, TO_TEST)
+                                         MAP(DO_TEST, TO_TEST)
 
 #undef DO_TEST
 #undef TO_TEST
@@ -142,13 +151,13 @@ TEST_FUN(test_cmp3) {
   SETUP
 
   auto eq = [=](AbsTerm &l, AbsTerm &r) -> bool {
-      return cmp3<AbsLiteral>{}($leq(l), $leq(r)) == CMP_EQUIV;                              
+    return cmp3<AbsLiteral>{}($leq(l), $leq(r)) == CMP_EQUIV;
   };
 
-  ASS(eq($plus(x2, f(x19), x1), $plus(x5, f(f(x19)), x2)))
-  ASS(eq($plus(x2, f(x19, f(x10)), x1), $plus(x5, f(f(x19)), x2)))
-  ASS(!eq($plus(x1, f(x19, f(x10)), x1), $plus(x5, f(f(x19)), x2)))
-  ASS(!eq($plus(x2, x8, x1), $plus(x5, f(f(x19)), x2)))
+  ASS(eq($sum(x2, f(x19), x1), $sum(x5, f(f(x19)), x2)))
+  ASS(eq($sum(x2, f(x19, f(x10)), x1), $sum(x5, f(f(x19)), x2)))
+  ASS(!eq($sum(x1, f(x19, f(x10)), x1), $sum(x5, f(f(x19)), x2)))
+  ASS(!eq($sum(x2, x8, x1), $sum(x5, f(f(x19)), x2)))
   ASS(eq(f(x1, x2, x2), g(x1, x2, x3)))
 }
 
@@ -156,12 +165,33 @@ TEST_FUN(test_cmp4) {
   SETUP
 
   auto eq = [=](AbsTerm &l, AbsTerm &r) -> bool {
-      return cmp4<AbsLiteral>{}($leq(l), $leq(r)) == CMP_EQUIV;                              
+    return cmp4<AbsLiteral>{}($leq(l), $leq(r)) == CMP_EQUIV;
   };
 
-  ASS(eq($plus(x2, f(x19), x1), $plus(x5, f(f(x19)), x2)))
-  ASS(!eq($plus(x2, f(x19, f(x10)), x1), $plus(x5, f(f(x19)), x2)))
-  ASS(!eq($plus(x1, f(x19, f(x10)), x1), $plus(x5, f(f(x19)), x2)))
-  ASS(!eq($plus(x2, x8, x1), $plus(x5, f(f(x19)), x2)))
+  auto less = [=](AbsTerm &l, AbsTerm &r) -> bool {
+    l.normalize();
+    l.rectify();
+    r.normalize();
+    r.rectify();
+    return cmp4<AbsLiteral>{}($leq(l), $leq(r)) == CMP_LESS;
+  };
+
+
+  ASS(eq($sum(x2, f(x19), x1), $sum(x5, f(f(x19)), x2)))
+  ASS(!eq($sum(x2, f(x19, f(x10)), x1), $sum(x5, f(f(x19)), x2)))
+  ASS(!eq($sum(x1, f(x19, f(x10)), x1), $sum(x5, f(f(x19)), x2)))
+  ASS(!eq($sum(x2, x8, x1), $sum(x5, f(f(x19)), x2)))
   ASS(!eq(f(x1, x2, x2), g(x1, x2, x3)))
+
+  ASS(less($sum(x0), f(x0)))
+  ASS(less(n(-10), f(x0)))
+}
+
+TEST_FUN(test_normalize) {
+  SETUP
+  auto check_norm = [=](AbsLiteral& l, AbsLiteral& r) {
+    normalize_absliteral(l);
+    ASS(equal_absliteral(l, r));
+  };
+  check_norm($equal($sum(f(x0), n(0)), f(x0, x1)), $equal($sum(n(10), f(x0)), f(x0, x1)));
 }
