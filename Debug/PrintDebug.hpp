7fd17f01ffbc3091a7802e1352b38dee0edb4855
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2020-03-30 19:52:33 +0100

    added DBG macro and related classes

--- Debug/PrintDebug.hpp
+++ Debug/PrintDebug.hpp
@@ -14,11 +14,11 @@
 namespace Debug {
 
 template <class A> struct print_debug {
-  void operator()(std::ostream &out, const A &a) const { out << a; }
+  void operator()(ostream &out, const A &a) const { out << a; }
 };
 
 template <class A> struct print_debug<vvec<A>> {
-  void operator()(std::ostream &out, const vvec<A> &a) const {
+  void operator()(ostream &out, const vvec<A> &a) const {
     out << "[";
     auto i = a.begin();
     if (i != a.end()) {
@@ -33,27 +33,28 @@ template <class A> struct print_debug<vvec<A>> {
 };
 
 template <class A> struct print_debug<refw<A>> {
-  void operator()(std::ostream &out, const refw<A> &a) const {
+  void operator()(ostream &out, const refw<A> &a) const {
     print_debug<A>{}(out, a.get());
   }
 };
 
+
 template <class A> struct print_debug<rc<A>> {
-  void operator()(std::ostream &out, const rc<A> &a) const {
+  void operator()(ostream &out, const rc<A> &a) const {
     print_debug<A>{}(out, *a.get());
   }
 };
 
 template <class... As> struct dbgr {
-  static void debug(std::ostream &out, const As &... as);
+  static void debug(ostream &out, const As &... as);
 };
 
 template <> struct dbgr<> {
-  static void debug(std::ostream &out) {}
+  static void debug(ostream &out) {}
 };
 
 template <class A, class... As> struct dbgr<A, As...> {
-  static void debug(std::ostream &out, const A &a, const As &... as) {
+  static void debug(ostream &out, const A &a, const As &... as) {
     print_debug<A>{}(out, a);
     dbgr<As...>::debug(out, as...);
   }
