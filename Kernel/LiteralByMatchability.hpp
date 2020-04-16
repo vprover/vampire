92525600f6748f1a49d0c0b3087e0dde26fc9c74
Jakob Rath <git@jakobrath.eu>
2020-04-16 15:16:04 +0200

    Add license comments

--- Kernel/LiteralByMatchability.hpp
+++ Kernel/LiteralByMatchability.hpp
@@ -1,3 +1,21 @@
+/*
+ * File LiteralByMatchability.cpp.
+ *
+ * This file is part of the source code of the software program
+ * Vampire. It is protected by applicable
+ * copyright laws.
+ *
+ * This source code is distributed under the licence found here
+ * https://vprover.github.io/license.html
+ * and in the source directory
+ *
+ * In summary, you are allowed to use Vampire for non-commercial
+ * purposes but not allowed to distribute, modify, copy, create derivatives,
+ * or use in competitions.
+ * For other uses of Vampire please contact developers for a different
+ * licence, which we will make an effort to provide.
+ */
+
 #ifndef LITERALBYMATCHABILITY_HPP
 #define LITERALBYMATCHABILITY_HPP
 
--- Lib/ScopeGuard.hpp
+++ Lib/ScopeGuard.hpp
@@ -1,3 +1,21 @@
+/*
+ * File ScopeGuard.hpp.
+ *
+ * This file is part of the source code of the software program
+ * Vampire. It is protected by applicable
+ * copyright laws.
+ *
+ * This source code is distributed under the licence found here
+ * https://vprover.github.io/license.html
+ * and in the source directory
+ *
+ * In summary, you are allowed to use Vampire for non-commercial
+ * purposes but not allowed to distribute, modify, copy, create derivatives,
+ * or use in competitions.
+ * For other uses of Vampire please contact developers for a different
+ * licence, which we will make an effort to provide.
+ */
+
 #ifndef SCOPEGUARD_HPP
 #define SCOPEGUARD_HPP
 
