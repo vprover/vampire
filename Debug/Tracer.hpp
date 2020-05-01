495cf36b0d5aa43ba9e82f81f2a98575f5cdb7ba
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2020-05-01 18:11:19 +0100

    added debug logging macro to Tracer

--- Debug/Tracer.hpp
+++ Debug/Tracer.hpp
@@ -50,6 +50,22 @@ enum ControlPointKind {
 }; // enum ControlPointKind
 
 
+template<class... As>
+struct _printDbg {
+  void operator()(const As&... msg);
+};
+
+template<> struct _printDbg<>{
+  void operator()() { }
+};
+
+template<class A, class... As> struct _printDbg<A, As...>{
+  void operator()(const A& a, const As&... as) {
+    cout << a;
+    _printDbg<As...>{}(as...);
+  }
+};
+
 class Tracer {
  public:
   explicit Tracer (const char* fun);
@@ -64,7 +80,6 @@ class Tracer {
   /** stop outputting forced by startOutput */
   static void stopOutput() { _forced = false; }
   static bool canWatch;
-
  private:
   const char* _fun;
   Tracer* _previous;
@@ -86,6 +101,16 @@ class Tracer {
   static bool _forced;
   static void controlPoint (const char*, ControlPointKind);
   static void outputLastControlPoint (ostream& str);
+public:
+  /* prints a message with indent in the of the same size as the current _depth */
+  template<class... A>
+  static void printDbg(A... msg)
+  {
+    for (int i = 0; i< _depth; i++) {
+      cout << "\t";
+    }
+    _printDbg<A...>{}(msg...);
+  }
 };
 
 } // namespace Debug
@@ -93,6 +118,10 @@ class Tracer {
 #  define AUX_CALL_(SEED,Fun) Debug::Tracer _tmp_##SEED##_(Fun);
 #  define AUX_CALL(SEED,Fun) AUX_CALL_(SEED,Fun)
 #  define CALL(Fun) AUX_CALL(__LINE__,Fun)
+#  define DBG(...) \
+  std::cout << "[ debug ] " << __FILE__ <<  "@" << __LINE__ << ":";\
+  Debug::Tracer::printDbg(__VA_ARGS__); \
+  std::cout << std::endl;
 #  define CALLC(Fun,check) if (check){ AUX_CALL(__LINE__,Fun) }
 #  define CONTROL(description) Debug::Tracer::controlPoint(description)
 #  define AFTER(number,command) \
