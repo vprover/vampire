0aeb39307771ad6d74dfb1a0bd08eeabb7210bc8
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2020-06-10 10:56:08 +0100

    set up unittest

---
+++ UnitTests/tQuotientE.cpp
@@ -0,0 +1,47 @@
+#include "Test/UnitTesting.hpp"
+#include "Test/SyntaxSugar.hpp"
+#include "Debug/Tracer.hpp"
+
+#define UNIT_ID QuotientE
+UT_CREATE;
+#define DBGE(x) DBG(#x, "\t=\t", x)
+
+IntegerConstantType quotientE(int lhs, int rhs) {
+  return IntegerConstantType(lhs).quotientE(rhs);
+}
+
+IntegerConstantType remainderE(int lhs, int rhs) {
+  return IntegerConstantType(lhs) - (IntegerConstantType(lhs).quotientE(rhs) * IntegerConstantType(rhs));
+}
+
+bool operator==(IntegerConstantType lhs, int rhs) {
+  return lhs == IntegerConstantType(rhs);
+}
+
+bool operator==(int lhs, IntegerConstantType rhs) {
+  return IntegerConstantType(lhs) == rhs;
+}
+
+bool operator<=(int lhs, IntegerConstantType rhs) {
+  return IntegerConstantType(lhs) <= rhs;
+}
+
+TEST_FUN(test_00) {
+  for (int i = -100; i < 100; i++) {
+    for (int j = -100; j < 100; j++) {
+      if (j != 0) {
+        DBGE(i)
+        DBGE(j)
+        DBGE(quotientE(i, j));
+        DBGE(remainderE(i, j));
+        auto q = quotientE(i, j);
+        auto r = remainderE(i, j);
+        ASS_EQ(q * j + r, i)
+        ASS(0 <= r && r < abs(j))
+      }
+    }
+  }
+}
+
+
+
