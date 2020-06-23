e5bebe2da11f4502ab2c1df7ae485fcb6b1d4733
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2020-06-23 09:31:21 +0100

    important bug fix

--- Inferences/PolynomialNormalization.cpp
+++ Inferences/PolynomialNormalization.cpp
@@ -2,11 +2,13 @@
 #include "Kernel/Clause.hpp"
 #include "Kernel/Ordering.hpp"
 
-#define DEBUG(...) // DBG(__VA_ARGS__)
+#define DEBUG(...) DBG(__VA_ARGS__)
 
 namespace Inferences {
 
 Clause* PolynomialNormalization::simplify(Clause* cl_) {
+  CALL("PolynomialNormalization::simplify(Clause*)")
+  DEBUG("in:  ", *cl_)
   auto& cl = *cl_;
   Stack<Literal*> out(cl.size());
 
@@ -24,6 +26,7 @@ Clause* PolynomialNormalization::simplify(Clause* cl_) {
           return NULL;
         } else {
           /* do not add the literal to the output stack */
+          changed = true;
         }
       } else {
         Literal* simplLit = simpl.unwrapLiteral();
@@ -40,30 +43,6 @@ Clause* PolynomialNormalization::simplify(Clause* cl_) {
         } else {
           out.push(orig);
         }
-
-
-        // if (env.options->literalComparisonMode() == Options::LiteralComparisonMode::REVERSE && cl[i]->isNegative())  {
-        //   //TODO this is so ugly. We should make a constraint between these options
-        //   cmp = Ordering::reverse(cmp);
-        // }
-        // switch(cmp) {
-        //
-        // case Ordering::Result::LESS:
-        //   //TODO shall we add an assertion here?
-        //   out.push(simplLit);
-        //   changed = true;
-        //   break;
-        // case Ordering::Result::INCOMPARABLE:
-        // case Ordering::Result::EQUAL:
-        //   out.push(cl[i]);
-        //   break;
-        // case Ordering::GREATER:
-        //   DBG("orig:  ", *cl[i])
-        //   DBG("simpl: ", *simplLit)
-        //   { ASSERTION_VIOLATION }
-        // default:
-        //   { ASSERTION_VIOLATION }
-        // }
       }
     } catch (MachineArithmeticException) {
       out.push(orig);
@@ -75,7 +54,6 @@ Clause* PolynomialNormalization::simplify(Clause* cl_) {
     return cl_;
   } else {
     auto result = Clause::fromStack(out, SimplifyingInference1(InferenceRule::EVALUATION, cl_));
-    DEBUG("in:  ", *cl_)
     DEBUG("out: ", *result)
     return result;
   }
