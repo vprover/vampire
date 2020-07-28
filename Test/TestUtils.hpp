a743bfa34397afa94130835c5ad57d7c1635c282
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2020-07-28 11:01:43 +0200

    added test utility functions for comparing terms modulo AC

--- Test/TestUtils.hpp
+++ Test/TestUtils.hpp
@@ -36,11 +36,36 @@ public:
   static Kernel::Formula* getUniqueFormula(Kernel::UnitList* units);
   static Kernel::Formula* getUniqueFormula(Api::AnnotatedFormulaIterator afit);
   static Kernel::Formula* getUniqueFormula(Api::Problem prb);
+  
+  /** 
+   * Tests whether two terms are equal modulo associativity and commutativity.
+   * Whether a method is assoc and commut is checked with `TestUtils::isAC(..)`
+   *
+   * !!! exponential runtime !!!
+   */
+  static bool eqModAC(Kernel::TermList lhs, Kernel::TermList rhs);
 
   /**
    * The ... are len of integers, positive -- positive polarity, negative -- negative polarity.
    */
   static SAT::SATClause* buildSATClause(unsigned len,...);
+
+
+private:
+  /**
+   * Tests whether there is a permutation pi s.t. pi(lhs) == rhs, where elements are compared by
+   * elemEq(l,r)
+   * `List` must provide methods 
+   *    - `elem_type operator[](unsigned)` 
+   *    - `unsigned size()`
+   * `Eq`   must provide methods
+   *    - `bool operator(const elem_type&, const elem_type&)`
+   */
+  template<class List, class Eq> 
+  bool permEq(const List& lhs, const List& rhs, Eq elemEq);
+
+  /** returns whether the function f is associative and commutative */
+  static bool isAC(Kernel::Theory::Interpretation f);
 };
 
 }
