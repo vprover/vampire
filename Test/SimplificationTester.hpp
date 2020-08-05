4595d64778c2c1c3641cd22b0e891651f93d41bc
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2020-08-05 12:30:55 +0100

    factored out test class for simplification tests

---
+++ Test/SimplificationTester.hpp
@@ -0,0 +1,81 @@
+/**
+ * This file provides macros and classes used to write nice tests for simplification rules.
+ *
+ * Check out UnitTests/tGaussianElimination.cpp, to see how it is to be used.
+ */
+
+#include "Test/TestUtils.hpp"
+#include "Kernel/Clause.hpp"
+
+namespace Test {
+
+  namespace Simplification {
+
+class SimplificationTester
+{
+public:
+  virtual Kernel::Clause* simplify(Kernel::Clause*) const = 0;
+
+  virtual bool eq(Kernel::Clause const* lhs, Kernel::Clause const* rhs) const 
+  {
+    return TestUtils::eqModAC(lhs, rhs);
+  }
+};
+
+struct Success
+{
+  Kernel::Clause& input;
+  Kernel::Clause& expected;
+
+  void run(const SimplificationTester& simpl) {
+    auto res = simpl.simplify(&input);
+
+    if (!res) {
+      cout  << endl;
+      cout << "[     case ]: " << input.toString() << endl;
+      cout << "[       is ]: NULL (indicates the clause is a tautology)" << endl;
+      cout << "[ expected ]: " << expected.toString() << endl;
+      exit(-1);
+
+    } else if (!simpl.eq(res, &expected)) {
+      cout  << endl;
+      cout << "[     case ]: " << input.toString() << endl;
+      cout << "[       is ]: " << res->toString() << endl;
+      cout << "[ expected ]: " << expected.toString() << endl;
+      exit(-1);
+
+    }
+  }
+};
+
+struct NotApplicable
+{
+  Kernel::Clause& input;
+
+  void run(const SimplificationTester& simpl) {
+    auto res = simpl.simplify(&input);
+    if (res != &input ) {
+      cout  << endl;
+      cout << "[     case ]: " << input.toString() << endl;
+      cout << "[       is ]: " << res->toString() << endl;
+      cout << "[ expected ]: < nop >" << endl;
+      exit(-1);
+    }
+  }
+};
+
+#define REGISTER_SIMPL_TESTER(t) using SimplTester = t;
+
+#define TEST_SIMPLIFY(name, ...)                                                                                        \
+  TEST_FUN(name) {                                                                                                      \
+    SimplTester simpl;                                                                                                  \
+    _Pragma("GCC diagnostic push")                                                                                      \
+    _Pragma("GCC diagnostic ignored \"-Wunused\"")                                                                      \
+      SIMPL_SUGAR                                                                                                       \
+    _Pragma("GCC diagnostic pop")                                                                                       \
+    __VA_ARGS__.run(simpl);                                                                                             \
+  }                                                                                                                     \
+
+} // namespace Simplification
+
+} // namespace Test
--- UnitTests/tGaussianElimination.cpp
+++ UnitTests/tGaussianElimination.cpp
@@ -10,74 +10,35 @@
 #include "Test/SyntaxSugar.hpp"
 #include "Test/TestUtils.hpp"
 #include "Lib/Coproduct.hpp"
-
-#define UNIT_ID GaussianVariableElimination
-UT_CREATE;
+#include "Test/SimplificationTester.hpp"
 
 using namespace std;
 using namespace Kernel;
 using namespace Inferences;
 using namespace Test;
 
-class SimplificationTester
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////// TEST UNIT INITIALIZATION
+/////////////////////////////////////
+
+/** 
+ * NECESSARY: as for every test unit we need to specify a name, and initialize the unit 
+ */
+#define UNIT_ID GaussianVariableElimination
+UT_CREATE;
+
+/** 
+ * NECESSARY: We need a subclass of SimplificationTester
+ */
+class GveSimplTester : public Test::Simplification::SimplificationTester
 {
 public:
-  virtual Clause* simplify(Clause*) const = 0;
 
-  virtual bool eq(Clause const* lhs, Clause const* rhs) const 
+  /**
+   * NECESSARY: performs the simplification
+   */
+  virtual Kernel::Clause* simplify(Kernel::Clause* in) const override 
   {
-    return TestUtils::eqModAC(lhs, rhs);
-  }
-};
-
-struct SimplSuccess
-{
-  Clause& input;
-  Clause& expected;
-
-  void run(const SimplificationTester& simpl) {
-    auto res = simpl.simplify(&input);
-
-    if (!res) {
-      cout  << endl;
-      cout << "[     case ]: " << input.toString() << endl;
-      cout << "[       is ]: NULL (indicates the clause is a tautology)" << endl;
-      cout << "[ expected ]: " << expected.toString() << endl;
-      exit(-1);
-
-    } else if (!simpl.eq(res, &expected)) {
-      cout  << endl;
-      cout << "[     case ]: " << input.toString() << endl;
-      cout << "[       is ]: " << res->toString() << endl;
-      cout << "[ expected ]: " << expected.toString() << endl;
-      exit(-1);
-
-    }
-  }
-};
-
-struct NotApplicable
-{
-  Clause& input;
-
-  void run(const SimplificationTester& simpl) {
-    auto res = simpl.simplify(&input);
-    if (res != &input ) {
-      cout  << endl;
-      cout << "[     case ]: " << input.toString() << endl;
-      cout << "[       is ]: " << res->toString() << endl;
-      cout << "[ expected ]: < nop >" << endl;
-      exit(-1);
-    }
-  }
-};
-
-
-
-class GveSimplTester : public SimplificationTester
-{
-public:
-  virtual Clause* simplify(Clause* in) const override {
     struct FakeOrdering : Kernel::Ordering {
       virtual Result compare(Literal* l, Literal* r) const override { 
         if (l == r) {
@@ -94,59 +55,77 @@ public:
     static PolynomialNormalization eval(ord);
 
     /* applies gve and evaluation until they're not applicable anymore */
-    Clause* last = eval.simplify(in);
-    Clause* latest = eval.simplify(in);
+    Kernel::Clause* last = eval.simplify(in);
+    Kernel::Clause* latest = eval.simplify(in);
     do {
       last = latest;
       latest = eval.simplify(gve.simplify(last));
     } while (latest != last);
     return latest;
   }
+
+  /** 
+   * OPTIONAL: override how equality between clauses is checked. 
+   * Defaults to TestUtils::eqModAC(Clause const*, Clause const*).
+   */
+  virtual bool eq(Kernel::Clause const* lhs, Kernel::Clause const* rhs) const override
+  {
+    return TestUtils::eqModAC(lhs, rhs);
+  }
 };
 
-#define TEST_SIMPLIFY(name, ...) \
-  TEST_FUN(name) { \
-    SimplTester simpl; \
-    SIMPL_SUGAR \
-    __VA_ARGS__.run(simpl); \
-  } \
+/**
+ * NECESSARY: Register our simpl tester as the one to use
+ */
+REGISTER_SIMPL_TESTER(GveSimplTester)
 
-using SimplTester = GveSimplTester;
+/**
+ * NECESSARY: We neet to tell the simplification tester which syntax sugar to import for creating terms & clauses. 
+ * See Test/SyntaxSugar.hpp for which kinds of syntax sugar are available
+ */
+#define SIMPL_SUGAR                                                                                                     \
+  THEORY_SYNTAX_SUGAR(REAL)                                                                                             \
+  THEORY_SYNTAX_SUGAR_FUN(f, 1)                                                                                         \
+  THEORY_SYNTAX_SUGAR_PRED(p, 1)                                                                                        \
 
-#define SIMPL_SUGAR \
-    THEORY_SYNTAX_SUGAR(REAL) \
-      _Pragma("GCC diagnostic push") \
-      _Pragma("GCC diagnostic ignored \"-Wunused\"") \
-        THEORY_SYNTAX_SUGAR_FUN(f, 1) \
-        THEORY_SYNTAX_SUGAR_PRED(p, 1) \
-      _Pragma("GCC diagnostic pop") \
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////// TEST CASES
+/////////////////////////////////////
 
 TEST_SIMPLIFY(gve_test_1,
-    SimplSuccess {
+    /** 
+     * Runs our registered SimplificationTester on .input,
+     * and checks if the output equals .expected.
+     */
+    Simplification::Success {
       .input    = clause({  3 * x != 6, x < y  }),
       .expected = clause({  2 < y  }),
     })
 
 TEST_SIMPLIFY(gve_test_2,
-    NotApplicable {
+    /** 
+     * Runs our registered SimplificationTester on .input,
+     * and fails if any simplification is performed.
+     */
+    Simplification::NotApplicable {
       .input = clause({ 3 * x == 6, x < y }),
     })
 
 TEST_SIMPLIFY(gve_test_3,
-    SimplSuccess {
+    Simplification::Success {
       .input    = clause({  3 * x != 6, x < x  }),
       .expected = clause({  /* 2 < 2 */  }),
     })
 
   // 2x + y = x + y ==> 0 = 2x + y - x - y ==> 0 = x
 TEST_SIMPLIFY(gve_test_4,
-    SimplSuccess {
+    Simplification::Success {
       .input    = clause({  2 * x + y != x + y, p(x) }),
       .expected = clause({  p(0)  }),
     })
 
 TEST_SIMPLIFY(gve_test_uninterpreted,
-    SimplSuccess {
+    Simplification::Success {
       .input    = clause({  3 * f(x) != y, x < y  }),
       .expected = clause({  x < 3 * f(x)  }),
     })
@@ -155,7 +134,7 @@ TEST_SIMPLIFY(gve_test_uninterpreted,
   //         4+y != 5 \/ C[4]
   //                     C[4]
 TEST_SIMPLIFY(gve_test_multiplesteps_1,
-    SimplSuccess {
+    Simplification::Success {
       .input    = clause({  x != 4, x + y != 5, x < f(x)  }),
       .expected = clause({  4 < f(4)  }),
     })
@@ -164,7 +143,7 @@ TEST_SIMPLIFY(gve_test_multiplesteps_1,
   //         4+y != 5 \/ C[4,y]
   //                     C[4,1]
 TEST_SIMPLIFY(gve_test_multiplesteps_2,
-    SimplSuccess {
+    Simplification::Success {
       .input    = clause({  x != 4, x + y !=  5, x < f(y)  }),
       .expected = clause({  4 < f(1)  }),
     })
