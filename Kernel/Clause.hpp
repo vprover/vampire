eeec7fb751e82de81e132c375e30817881b09b6f
Martin Suda <sudam@cs.man.ac.uk>
2020-08-28 13:24:46 +0200

    dont be so strict

--- Kernel/Clause.hpp
+++ Kernel/Clause.hpp
@@ -255,12 +255,9 @@ public:
    * set splits
    * in order to keep all splitting-related functionality separate from Saturation,
    * the splits are not set during clause-construction but are added later by the Splitter-class.
-   * we depend on the invariant that splits are set only once, and that splits are set before clause-weights are
-   * computed and cached (which happens at the first call to weight())
    */
   void setSplits(SplitSet* splits) {
     CALL("Clause::setSplits");
-    ASS(_weight == 0);
     _inference.setSplits(splits);
   }
   
