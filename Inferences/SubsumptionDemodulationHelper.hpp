9ac9df7871eae0112298527a48d72c6518375b14
Jakob Rath <git@jakobrath.eu>
2020-10-07 10:28:08 +0200

    typo

--- Inferences/SubsumptionDemodulationHelper.hpp
+++ Inferences/SubsumptionDemodulationHelper.hpp
@@ -106,7 +106,7 @@ class OverlayBinder
         || m_overlay.find(var) != m_overlay.end();
     }
 
-    void specVar(Var var, TermList term) const
+    void specVar(Var var, TermList term)
     {
       ASSERTION_VIOLATION;
     }
@@ -374,7 +374,7 @@ bool termContainsAllVariablesOfOtherUnderSubst(TermList term, TermList other, Ap
     }
   }
 
-  // check that all vars of other after substition have been collected
+  // check that all vars of other after substitution have been collected
   vit.reset(other);
   while (vit.hasNext()) {
     TermList t = applicator.apply(vit.next().var());
