88949139699bd5830123f2121f85d0c6f511e6ac
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2020-11-11 11:20:33 +0000

    dead code removal

--- Kernel/InterpretedLiteralEvaluator.hpp
+++ Kernel/InterpretedLiteralEvaluator.hpp
@@ -99,51 +99,5 @@ private:
   const bool _normalize;
 };
 
-// struct AnyPoly;
-// using TermEvalResult = Lib::Either<TermList, AnyPoly>;
-// struct NewEvaluator {
-//
-// public:
-//   LitEvalResult evaluate(Literal* in) const;
-//   TermList evaluate(TermList in) const;
-//   TermList evaluate(Term* in) const;
-//
-// private:
-//   struct RecursionState;
-//   LitEvalResult evaluateStep(Literal* in) const;
-//
-//   TermEvalResult evaluateStep(Term* orig, TermEvalResult* evaluatedArgs) const;
-//
-//   template<Theory::Interpretation inter>
-//   LitEvalResult evaluateLit(Literal* lit) const;
-//
-//   template<Theory::Interpretation inter>
-//   TermEvalResult evaluateFun(Term* orig, TermEvalResult* evaluatedArgs) const;
-//
-//   // template<class number>
-//   // TermEvalResult evaluateConst(typename number::ConstantType c) const;
-//
-//   template<class CommutativeMonoid>
-//   TermEvalResult evaluateCommutativeMonoid(Term* orig, TermEvalResult* evaluatedArgs) const;
-//
-//   template<class ConstantType, class EvalIneq> 
-//   LitEvalResult evaluateInequality(Literal* lit, bool strict, EvalIneq evalIneq) const;
-//
-//   template<class ConstantType, class EvalGround>
-//   LitEvalResult tryEvalConstant1(Literal* lit, EvalGround fun) const;
-//
-//   template<class ConstantType, class EvalGround>
-//   LitEvalResult tryEvalConstant2(Literal* lit, EvalGround fun) const;
-//
-//   template<class ConstantType, class EvalGround>
-//   TermEvalResult tryEvalConstant1(Term* orig, TermEvalResult* evaluatedArgs, EvalGround fun) const;
-//
-//   template<class ConstantType, class EvalGround>
-//   TermEvalResult tryEvalConstant2(Term* orig, TermEvalResult* evaluatedArgs, EvalGround fun) const;
-// };
-// }
-//
-// #endif // __InterpretedLiteralEvaluator__
-
 }
 #endif // __InterpretedLiteralEvaluator__
