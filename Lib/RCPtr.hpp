c76344a7d01735767c2278b5ad88ca4e1d129da6
Petra Hozzová <petra.hozzova@tuwien.ac.at>
2020-11-24 14:58:27 +0100

    Removed the "summary" paragraph from file headers

--- Lib/RCPtr.hpp
+++ Lib/RCPtr.hpp
@@ -9,12 +9,6 @@
  * This source code is distributed under the licence found here
  * https://vprover.github.io/license.html
  * and in the source directory
- *
- * In summary, you are allowed to use Vampire for non-commercial
- * purposes but not allowed to distribute, modify, copy, create derivatives,
- * or use in competitions. 
- * For other uses of Vampire please contact developers for a different
- * licence, which we will make an effort to provide. 
  */
 /**
  * @file RCPtr.hpp
