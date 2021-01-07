24489572a44c5f734a331e55c8a47a735dc2308b
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2021-01-07 15:13:15 +0100

    fixed move semantics issues

--- Lib/Iterator.hpp
+++ Lib/Iterator.hpp
@@ -22,6 +22,36 @@
 
 
 namespace Lib {
+
+  // TODO move to reflection
+template<class T>
+struct ElementTypeInfo<std::unique_ptr<T>> {
+  using Type = ELEMENT_TYPE(T);
+};
+
+
+template<class T,
+         typename std::enable_if<
+            !std::is_lvalue_reference<T>::value
+         && !std::is_rvalue_reference<T>::value
+           , bool>::type = true
+         >
+typename std::remove_reference<T>::type&& moveValue( T& t ) noexcept
+{ return std::move(t); }
+
+template<class T,
+         typename std::enable_if<std::is_rvalue_reference<T>::value, bool>::type = true
+         >
+typename std::remove_reference<T>::type&& moveValue( T&& t ) noexcept
+{ return std::move(t); }
+
+template<class T,
+         typename std::enable_if<std::is_lvalue_reference<T>::value, bool>::type = true
+         >
+typename std::remove_reference<T>::type& moveValue( T&& t ) noexcept
+{ return t; }
+
+
 namespace Iterator {
 
 
@@ -32,9 +62,9 @@ template<class Iter>
 struct is_iterator 
 { 
   static constexpr bool value = 
-    std::is_same<decltype(((Iter*)nullptr)->sizeLeft()), Option<unsigned>>::value &&
-    std::is_same<decltype(((Iter*)nullptr)-> hasNext()), bool>::value &&
-    std::is_same<decltype(((Iter*)nullptr)->    next()), ElemT<Iter>>::value;
+    std::is_same<decltype(((Iter const*)nullptr)->sizeLeft()), Option<unsigned>>::value &&
+    std::is_same<decltype(((Iter      *)nullptr)-> hasNext()), bool>::value &&
+    std::is_same<decltype(((Iter      *)nullptr)->    next()), ElemT<Iter>>::value;
 };
 
 #define ASSERT_ITERATOR(...) \
@@ -44,7 +74,7 @@ template<class Iter, class Adaptor>
 typename std::result_of<Adaptor(Iter)>::type operator|(Iter iter, Adaptor adaptor) 
 { 
   ASSERT_ITERATOR(Iter);
-  return adaptor(iter); 
+  return adaptor(std::move(iter)); 
 }
 
 
@@ -52,35 +82,67 @@ typename std::result_of<Adaptor(Iter)>::type operator|(Iter iter, Adaptor adapto
 // BASE ITERATORS
 //////////////////////
 
+
 template<class E>
-class DynIterator 
+class DynIter 
 {
+  template<class El> class Interface 
+  {
+  public:
+    DECL_ELEMENT_TYPE(El);
+    virtual E next() = 0;
+    virtual bool hasNext() = 0;
+    virtual Option<unsigned> sizeLeft() const = 0;
+  };
+
+  template<class Iter> class Implementation : public Interface<ElemT<Iter>>
+  {
+    Iter _iter;
+  public:
+    Implementation(Iter iter) : _iter(std::move(iter)) { }
+    virtual ElemT<Iter> next()                override { return _iter.next(); }
+    virtual bool hasNext()                    override { return _iter.hasNext(); }
+    virtual Option<unsigned> sizeLeft() const override { return _iter.sizeLeft(); }
+  };
+
+
+  std::unique_ptr<Interface<E>> _iter;
 public:
   DECL_ELEMENT_TYPE(E);
-  virtual E next();
-  virtual bool hasNext();
-  virtual Option<unsigned> sizeLeft();
-  ASSERT_ITERATOR(DynIterator);
+  template<class Iter>
+  DynIter(Iter iter) : _iter(new Implementation<Iter>(std::move(iter))) { }
+  inline E next()                          { return _iter->next(); }
+  inline bool hasNext()                    { return _iter->hasNext(); }
+  inline Option<unsigned> sizeLeft() const { return _iter->sizeLeft(); }
 };
 
-template<class T> T&& fwd(typename std::remove_reference<T>::type&  t) { return std::forward<T>(t); }
-template<class T> T&& fwd(typename std::remove_reference<T>::type&& t) { return std::forward<T>(t); }
-template<class T> T&& fwd(typename std::remove_reference<T>::type   t) { return std::move<T>(t); }
+template<class Iter>
+DynIter<ElemT<Iter>> dyn(Iter iter) 
+{ return DynIter<ElemT<Iter>>(std::move(iter)); }
 
 /** Iterator for any object that implements the indexing operator */
 template<class Array, class Idx = unsigned>
 class IndexIter {
+  Array _array;
   Idx _idx;
   Idx _size;
-  Array _array;
 public:
   DECL_ELEMENT_TYPE(decltype(_array[_idx]));
-  IndexIter(Array array, Idx size) : _array(fwd(array)), _idx(0), _size(_array.size()) {}
-  IndexIter(Array array) : IndexIter(fwd(array), _array.size()) {}
+  IndexIter(Array array, Idx size) : _array(std::forward<Array>(array)), _idx(0), _size(_array.size()) {}
+  IndexIter(Array array) : IndexIter(std::forward<Array>(array), array.size()) {}
   bool hasNext() { return _idx < _size; }
-  ElemT<IndexIter> next() { return _array[_idx]; }
-  Option<unsigned> sizeLeft() { return Option<unsigned>(_size - _idx); }
+  ElemT<IndexIter> next() { return _array[_idx++]; }
+  Option<unsigned> sizeLeft() const { return Option<unsigned>(_size - _idx); }
 };
+
+template<class Array>
+auto indexIter(Array array)  -> IndexIter<Array, decltype(array.size())>
+{ return IndexIter<Array, decltype(array.size())>(std::forward<Array>(array)); }
+
+template<class Array, class Idx>
+IndexIter<Array, Idx> indexIter(Array array, Idx size) 
+{ return IndexIter<Array, Idx>(moveValue<Array>(array), size); }
+
 
 // TODO remove me
 template<class Iter>
@@ -91,7 +153,7 @@ public:
   IterWrapper(Iter iter) : _iter(std::move(iter)) {}
   bool hasNext() { return _iter.hasNext(); }
   ElemT<Iter> next() { return _iter.next(); }
-  Option<unsigned> sizeLeft() { return 
+  Option<unsigned> sizeLeft() const { return 
     _iter.knownSize() ? Option<unsigned>(_iter.size())
                       : Option<unsigned>(); }
 };
@@ -139,7 +201,7 @@ public:
   Map(Iter inner, Func func) : _iter(std::move(inner)), _func(std::move(func)) {}
   inline bool hasNext() { return _iter.hasNext(); };
   inline Result next() { return _func(_iter.next()); }
-  inline Option<unsigned> sizeLeft() { return _iter.sizeLeft(); }
+  inline Option<unsigned> sizeLeft() const { return _iter.sizeLeft(); }
 };
 
 template<class F>
@@ -178,7 +240,7 @@ public:
     }
     return false;
   };
-  inline Option<unsigned> sizeLeft() { return Option<unsigned>(); }
+  inline Option<unsigned> sizeLeft() const { return Option<unsigned>(); }
 
   ElemT<Iter> next()
   {
@@ -186,7 +248,6 @@ public:
     ALWAYS(hasNext());
     ASS(_next.isSome());
     ElemT<Iter> out = std::move(_next).unwrap();
-    DBGE(out)
     _next = Option<ElemT<Iter>>();
     return out;
   };
@@ -242,7 +303,7 @@ public:
     return _current.unwrap().next();
   }
 
-  inline Option<unsigned> sizeLeft() 
+  inline Option<unsigned> sizeLeft() const 
   { return Option<unsigned>(); }
 };
 
@@ -289,7 +350,7 @@ public:
     return _iter.next(); 
   }
 
-  inline Option<unsigned> sizeLeft() 
+  inline Option<unsigned> sizeLeft() const
   { return Option<unsigned>(_size); }
 };
 
@@ -313,9 +374,9 @@ public:
   FlatMap(Iter i, F f) 
     : _self(Self(SelfMap(std::move(i), std::move(f)))) {}
 
-  ElemT<Self> next()                 { return _self.next(); }
-  bool hasNext()                     { return _self.hasNext(); }
-  inline Option<unsigned> sizeLeft() { return _self.sizeLeft(); }
+  ElemT<Self> next()                       { return _self.next(); }
+  bool hasNext()                           { return _self.hasNext(); }
+  inline Option<unsigned> sizeLeft() const { return _self.sizeLeft(); }
 };
 
 template<class F>
@@ -346,7 +407,8 @@ class Stl {
     { _cur = tryNext(_iter.unwrap()); }
 
     ElemT<Iter> operator*() 
-    { return _cur.unwrap(); } 
+    // TODO here { return moveValue<ElemT<Iter>>(_cur.unwrap()); } 
+    { return std::move(_cur.unwrap()); } 
 
     friend bool operator!=(StlIter const& lhs, StlIter const& rhs) 
     { return !(lhs == rhs); }
@@ -396,8 +458,8 @@ public:
   };
 
 
-  inline Option<unsigned> leftSize()
-  { return _inn.leftSize(); };
+  inline Option<unsigned> sizeLeft() const
+  { return _inn.sizeLeft(); };
 
 private:
   Inner _inn;
@@ -460,9 +522,12 @@ public:
     ASSERTION_VIOLATION
   }
 
-  Option<unsigned> sizeLeft() { 
+  Option<unsigned> sizeLeft() const 
+  {
     return _i1.sizeLeft().andThen([&](unsigned i){ 
-      return _i2.sizeLeft().map([&](unsigned j){ return i + j; });  
+      return _i2.sizeLeft().map([&](unsigned j){ 
+          return i + j; 
+      });  
     });
   }
 };
@@ -493,7 +558,7 @@ class Collector {
 public:
   template<class Iter>
   Container operator()(Iter iter) 
-  { return Container::fromIterator(iter); }
+  { return Container::fromIterator(std::move(iter)); }
 };
 
 /** Collects the elemnts of an iterator into a container of type C */
@@ -506,7 +571,7 @@ class HkCollector {
 public:
   template<class Iter>
   Container<ElemT<Iter>> operator()(Iter iter) 
-  { return Container<ElemT<Iter>>::fromIterator(iter); }
+  { return Container<ElemT<Iter>>::fromIterator(std::move(iter)); }
 };
 
 
@@ -628,7 +693,7 @@ public:
   FoldNonEmpty(F combine): _combine(std::move(combine)) {}
   
   template<class Iter>
-  using ResultT = typename std::result_of<F(ElemT<Iter>&&, ElemT<Iter>&&)>::type;
+  using ResultT = typename std::result_of<F(typename std::remove_reference<ElemT<Iter>>::type&&, ElemT<Iter>)>::type;
 
   template<class Iter>
   Option<ResultT<Iter>> operator()(Iter i) 
@@ -636,7 +701,9 @@ public:
     if (i.hasNext()) {
       ResultT<Iter> state = std::move(i.next());
       for (ElemT<Iter> x : i | toStl()) {
-        state = _combine(std::move(state), std::forward<ElemT<Iter>>(x));
+        // state = _combine(std::move(state), std::forward<ElemT<Iter>>(x));
+        // TODO here moveValue
+        state = _combine(std::move(state), std::move(x));
       }
       return Option<ResultT<Iter>>(std::move(state));
     } else {
