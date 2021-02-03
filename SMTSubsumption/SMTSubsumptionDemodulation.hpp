bbf8cffbcd4f1d39f31ce0df2538a3f29ab2d055
Jakob Rath <git@jakobrath.eu>
2021-02-03 12:51:00 +0100

    Copy SMTSubsumption to SMTSubsumptionDemodulation

---
+++ SMTSubsumption/SMTSubsumptionDemodulation.hpp
@@ -0,0 +1,33 @@
+#ifndef SMTSUBSUMPTION_HPP
+#define SMTSUBSUMPTION_HPP
+
+#include "Kernel/Clause.hpp"
+#include "Lib/STL.hpp"
+#include "Inferences/InferenceEngine.hpp"
+
+namespace SMTSubsumption {
+
+
+struct SubsumptionInstance
+{
+  unsigned int number;
+  Kernel::Clause* side_premise;  // also called "base clause"
+  Kernel::Clause* main_premise;  // also called "instance clause"
+  bool subsumed;  // expected result
+};
+
+
+class ProofOfConcept {
+  public:
+    CLASS_NAME(ProofOfConcept);
+    USE_ALLOCATOR(ProofOfConcept);
+
+    void test(Kernel::Clause* side_premise, Kernel::Clause* main_premise);
+    bool checkSubsumption(Kernel::Clause* side_premise, Kernel::Clause* main_premise);
+    void benchmark_micro(vvector<SubsumptionInstance> instances);
+};
+
+
+}
+
+#endif /* !SMTSUBSUMPTION_HPP */
