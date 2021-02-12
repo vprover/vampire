6a056e5556ca7afa6316291db6524ebbec196e9d
Jakob Rath <git@jakobrath.eu>
2021-02-12 07:22:55 +0100

    begin subsat

---
+++ SMTSubsumption/subsat/subsat.cpp
@@ -0,0 +1,12 @@
+#include "subsat.hpp"
+
+#include <iostream>
+
+
+#ifdef SUBSAT_STANDALONE
+int main()
+{
+    std::cout << "hello" << std::endl;
+    return 0;
+}
+#endif
---
+++ SMTSubsumption/subsat/subsat.hpp
@@ -0,0 +1,194 @@
+#ifndef SUBSAT_HPP
+#define SUBSAT_HPP
+
+#include <cassert>
+#include <cstdint>
+#include <limits>
+
+// Ensure NDEBUG and VDEBUG are synchronized
+#ifdef NDEBUG
+  static_assert(!VDEBUG, "");
+#else
+  static_assert(VDEBUG, "");
+#endif
+
+
+// TODO:
+// Once this works, make a separate version 'matchsat',
+// which keeps an array of matches as well.
+// (see my notes on SAT+CSP)
+
+
+namespace SMTSubsumption {
+
+  using std::uint32_t;
+
+
+enum class Value : signed char
+{
+    False = -1,
+    Unknown = 0,
+    True = 1,
+};
+
+
+class Lit;
+
+
+/// Boolean variable represented by its integer index.
+/// Use consecutive indices starting at 0.
+class Var final
+{
+  uint32_t m_index;
+
+public:
+  explicit constexpr Var(uint32_t index) noexcept
+      : m_index{index}
+  {
+      // assert(m_index <= Var::max_index());  // TODO: allow assert in constexpr (C++14 and later)
+  }
+
+  [[nodiscard]] constexpr uint32_t index() const noexcept
+  {
+    return m_index;
+  }
+
+  [[nodiscard]] static constexpr uint32_t max_index() noexcept
+  {
+      return (1u << 31) - 2;
+  }
+
+  [[nodiscard]] static constexpr Var invalid() noexcept
+  {
+    return Var{std::numeric_limits<uint32_t>::max()};
+  }
+
+  // [[nodiscard]] constexpr Lit operator~() const noexcept
+  // {
+  //   return Lit{*this, false};
+  // }
+
+  // [[nodiscard]] constexpr operator Lit() const noexcept
+  // {
+  //   return Lit{*this, true};
+  // }
+};  // Var
+
+static_assert(Var::max_index() == static_cast<uint32_t>(INT32_MAX - 1), "unexpected max variable index");
+static_assert(Var::max_index() < Var::invalid().index(), "valid variable indices overlap with invalid sentinel value");
+
+
+/// Boolean literals represented by integer index.
+/// The least significant bit indicates the sign.
+///
+/// Mapping from variable indices to literal indices:
+///    Lit{0} ... 0
+///   ~Lit{0} ... 1
+///    Lit{1} ... 2
+///   ~Lit{1} ... 3
+///      :
+///      :
+class Lit final
+{
+  uint32_t m_index;
+
+private:
+  friend class Clause;
+  /// Uninitialized value (for clause constructor)
+  Lit() noexcept
+    // : m_index{Lit::invalid().index()}
+  { }
+
+  explicit constexpr Lit(uint32_t index) noexcept
+      : m_index{index}
+  {
+    assert(m_index <= Lit::max_index()); // TODO constexpr assert
+  }
+
+public:
+  explicit constexpr Lit(Var var, bool positive) noexcept
+      : Lit{2*var.index() + static_cast<uint32_t>(!positive)}
+  { }
+
+  [[nodiscard]] static constexpr Lit pos(Var var) noexcept
+  {
+    return Lit{var, true};
+  }
+
+  [[nodiscard]] static constexpr Lit neg(Var var) noexcept
+  {
+    return Lit{var, false};
+  }
+
+  [[nodiscard]] constexpr uint32_t index() const noexcept
+  {
+    return m_index;
+  }
+
+  [[nodiscard]] static constexpr uint32_t max_index() noexcept
+  {
+    static_assert(Var::max_index() < (std::numeric_limits<uint32_t>::max() - 1) / 2, "cannot represent all literals");
+    return 2 * Var::max_index() + 1;
+  }
+
+  [[nodiscard]] static constexpr Lit invalid() noexcept
+  {
+    return Lit{std::numeric_limits<uint32_t>::max()};
+  }
+
+  [[nodiscard]] constexpr bool is_positive() const noexcept
+  {
+    return (m_index & 1) == 0;
+  }
+
+  [[nodiscard]] constexpr bool is_negative() const noexcept
+  {
+    return !is_positive();
+  }
+
+  [[nodiscard]] constexpr Lit operator~() const noexcept
+  {
+    return Lit{m_index ^ 1};
+  }
+}; // Lit
+
+static_assert(Lit::max_index() < Lit::invalid().index(), "valid literal indices overlap with invalid sentinel value");
+
+
+class Clause final
+{
+  uint32_t m_size;      // number of literals
+  Lit m_literals[2];    // actual size is m_size, but C++ does not officially support flexible array members (as opposed to C)
+
+public:
+  using iterator = Lit const*;
+
+  [[nodiscard]] iterator begin() const noexcept
+  {
+      return &m_literals[0];
+  }
+
+  [[nodiscard]] iterator end() const noexcept
+  {
+      return begin() + m_size;
+  }
+
+private:
+  // NOTE: do not use this constructor directly
+  // (it does not allocate enough memory for the literals)
+  Clause(uint32_t size) noexcept
+    : m_size{m_size}
+  {
+  }
+
+  // cannot copy/move because of flexible array member
+  Clause(Clause&) = delete;
+  Clause(Clause&&) = delete;
+  Clause& operator=(Clause&) = delete;
+  Clause& operator=(Clause&&) = delete;
+};  // Clause
+
+
+}
+
+#endif /* !SUBSAT_HPP */
