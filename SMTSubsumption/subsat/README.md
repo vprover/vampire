45a6f10ac477dc7853d3b0dce614b0d224863505
Jakob Rath <git@jakobrath.eu>
2021-02-12 11:52:43 +0100

    Allocation and more

---
+++ SMTSubsumption/subsat/README.md
@@ -0,0 +1,16 @@
+# subsat
+
+Subsolver for subsumption.
+
+
+## Standalone Mode
+
+For testing the solver itself.
+
+How to compile:
+```
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug -G Ninja ..
+ninja
+```
--- SMTSubsumption/subsat/subsat.hpp
+++ SMTSubsumption/subsat/subsat.hpp
@@ -3,49 +3,44 @@
 
 #include <cassert>
 #include <cstdint>
+#include <cstdlib>
 #include <limits>
+#include <new>
 
 // Ensure NDEBUG and VDEBUG are synchronized
 #ifdef NDEBUG
-  static_assert(!VDEBUG, "");
+static_assert(!VDEBUG, "");
 #else
-  static_assert(VDEBUG, "");
+static_assert(VDEBUG, "");
 #endif
-
 
 // TODO:
 // Once this works, make a separate version 'matchsat',
 // which keeps an array of matches as well.
 // (see my notes on SAT+CSP)
 
-
 namespace SMTSubsumption {
 
-  using std::uint32_t;
+using std::uint32_t;
 
-
-enum class Value : signed char
-{
-    False = -1,
-    Unknown = 0,
-    True = 1,
+enum class Value : signed char {
+  False = -1,
+  Unknown = 0,
+  True = 1,
 };
-
 
 class Lit;
 
-
 /// Boolean variable represented by its integer index.
 /// Use consecutive indices starting at 0.
-class Var final
-{
+class Var final {
   uint32_t m_index;
 
 public:
   explicit constexpr Var(uint32_t index) noexcept
       : m_index{index}
   {
-      // assert(m_index <= Var::max_index());  // TODO: allow assert in constexpr (C++14 and later)
+    // assert(m_index <= Var::max_index());  // TODO: how to assert in constexpr constructor?
   }
 
   [[nodiscard]] constexpr uint32_t index() const noexcept
@@ -55,7 +50,7 @@ public:
 
   [[nodiscard]] static constexpr uint32_t max_index() noexcept
   {
-      return (1u << 31) - 2;
+    return (1u << 31) - 2;
   }
 
   [[nodiscard]] static constexpr Var invalid() noexcept
@@ -72,11 +67,10 @@ public:
   // {
   //   return Lit{*this, true};
   // }
-};  // Var
+}; // Var
 
 static_assert(Var::max_index() == static_cast<uint32_t>(INT32_MAX - 1), "unexpected max variable index");
 static_assert(Var::max_index() < Var::invalid().index(), "valid variable indices overlap with invalid sentinel value");
-
 
 /// Boolean literals represented by integer index.
 /// The least significant bit indicates the sign.
@@ -88,27 +82,28 @@ static_assert(Var::max_index() < Var::invalid().index(), "valid variable indices
 ///   ~Lit{1} ... 3
 ///      :
 ///      :
-class Lit final
-{
+class Lit final {
   uint32_t m_index;
 
 private:
   friend class Clause;
   /// Uninitialized value (for clause constructor)
   Lit() noexcept
-    // : m_index{Lit::invalid().index()}
-  { }
+  // : m_index{Lit::invalid().index()}
+  {
+  }
 
   explicit constexpr Lit(uint32_t index) noexcept
       : m_index{index}
   {
-    assert(m_index <= Lit::max_index()); // TODO constexpr assert
+    // assert(m_index <= Lit::max_index()); // TODO: how to assert in constexpr constructor?
   }
 
 public:
   explicit constexpr Lit(Var var, bool positive) noexcept
-      : Lit{2*var.index() + static_cast<uint32_t>(!positive)}
-  { }
+      : Lit{2 * var.index() + static_cast<uint32_t>(!positive)}
+  {
+  }
 
   [[nodiscard]] static constexpr Lit pos(Var var) noexcept
   {
@@ -154,30 +149,79 @@ public:
 
 static_assert(Lit::max_index() < Lit::invalid().index(), "valid literal indices overlap with invalid sentinel value");
 
-
-class Clause final
+inline void* subsat_alloc(std::size_t size)
 {
-  uint32_t m_size;      // number of literals
-  Lit m_literals[2];    // actual size is m_size, but C++ does not officially support flexible array members (as opposed to C)
+#ifdef SUBSAT_STANDALONE
+  void* p = std::malloc(size);
+#else
+  void* p = ALLOC_UNKNOWN(size, "subsat");
+#endif
+  if (!p && size > 0) {
+    throw std::bad_alloc();
+  }
+  return p;
+}
+
+inline void subsat_dealloc(void* p)
+{
+#ifdef SUBSAT_STANDALONE
+  std::free(p);
+#else
+  DEALLOC_UNKNOWN(p, "subsat");
+#endif
+}
+
+
+
+
+
+
+class Clause final {
+  uint32_t m_size;   // number of literals
+  Lit m_literals[2]; // actual size is m_size, but C++ does not officially support flexible array members (as opposed to C)
 
 public:
   using iterator = Lit const*;
 
   [[nodiscard]] iterator begin() const noexcept
   {
-      return &m_literals[0];
+    return &m_literals[0];
   }
 
   [[nodiscard]] iterator end() const noexcept
   {
-      return begin() + m_size;
+    return begin() + m_size;
   }
+
+  /// Number of bytes required by a clause containing 'size' literals.
+  static size_t bytes(uint32_t size) noexcept
+  {
+    size_t const contained_literals = std::extent_v<decltype(m_literals)>;
+    size_t const additional_literals = (size >= contained_literals) ? (size - contained_literals) : 0;
+    size_t const total_bytes = sizeof(Clause) + sizeof(Lit) * additional_literals;
+    return total_bytes;
+  }
+
+  /// Allocate a clause with enough space for 'size' literals.
+  static Clause* create(uint32_t size)
+  {
+    void* p = subsat_alloc(bytes(size));
+    return new (p) Clause{size};
+  }
+
+  // static void* operator new(size_t, uint32_t num_literals)
+  // {
+  //   size_t const contained_literals = std::extent_v<decltype(m_literals)>;
+  //   size_t const additional_literals = std::max(0, static_cast<size_t>(num_literals) - contained_literals);
+  //   size_t const total_bytes = sizeof Clause + sizeof Lit * additional_literals;
+  //   return ::operator new(total_bytes);
+  // }
 
 private:
   // NOTE: do not use this constructor directly
-  // (it does not allocate enough memory for the literals)
+  // because it does not allocate enough memory for the literals
   Clause(uint32_t size) noexcept
-    : m_size{m_size}
+      : m_size{size}
   {
   }
 
@@ -186,9 +230,17 @@ private:
   Clause(Clause&&) = delete;
   Clause& operator=(Clause&) = delete;
   Clause& operator=(Clause&&) = delete;
-};  // Clause
+}; // Clause
 
 
-}
+
+class Solver
+{
+  int x;
+}; // Solver
+
+
+
+} // namespace SMTSubsumption
 
 #endif /* !SUBSAT_HPP */
