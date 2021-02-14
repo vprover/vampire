5b8e2e1439e78cb0287e94a6d525a38b149d3c15
Jakob Rath <git@jakobrath.eu>
2021-02-14 19:54:29 +0100

    Implement unit propagation

--- SMTSubsumption/subsat/subsat.cpp
+++ SMTSubsumption/subsat/subsat.cpp
@@ -1,14 +1,48 @@
 #include "subsat.hpp"
 
+#include <initializer_list>
 #include <iostream>
 
 using namespace SMTSubsumption;
 
 #ifdef SUBSAT_STANDALONE
+
+Clause* make_clause(std::initializer_list<Lit> literals)
+{
+  Clause* cl = Clause::create(literals.size());
+  for (size_t i = 0; i < literals.size(); ++i) {
+    (*cl)[i] = literals.begin()[i];
+  }
+  return cl;
+}
+
+void add_clause(Solver& solver, std::initializer_list<Lit> literals)
+{
+  solver.add_clause(make_clause(literals));
+}
+
 int main()
 {
     std::cout << "hello" << std::endl;
+
     Solver s;
+    Var x = s.new_variable();
+    Var y = s.new_variable();
+    Var z = s.new_variable();
+
+    s.add_clause(make_clause({x, y, z}));
+    s.add_clause(make_clause({x, y, ~z}));
+    s.add_clause(make_clause({x, ~y, z}));
+    s.add_clause(make_clause({x, ~y, ~z}));
+    s.add_clause(make_clause({~x, y, z}));
+    s.add_clause(make_clause({~x, y, ~z}));
+    s.add_clause(make_clause({~x, ~y, z}));
+    s.add_clause(make_clause({~x, ~y, ~z}));
+
+    auto res = s.solve();
+
+    std::cout << "Result: " << res << std::endl;
+
     return 0;
 }
 #endif
--- SMTSubsumption/subsat/subsat.hpp
+++ SMTSubsumption/subsat/subsat.hpp
@@ -5,8 +5,11 @@
 #include <cassert>
 #include <cstdint>
 #include <cstdlib>
+#include <iterator>
 #include <limits>
 #include <new>
+#include <ostream>
+#include <set>
 #include <vector>
 
 // Ensure NDEBUG and VDEBUG are synchronized
@@ -27,7 +30,7 @@ using std::uint32_t;
 
 enum class Value : signed char {
   False = -1,
-  Unknown = 0,
+  Unknown = 0,   // TODO: rename to Unassigned
   True = 1,
 };
 
@@ -70,15 +73,8 @@ public:
     return m_index <= max_index();
   }
 
-  // [[nodiscard]] constexpr Lit operator~() const noexcept
-  // {
-  //   return Lit{*this, false};
-  // }
-
-  // [[nodiscard]] constexpr operator Lit() const noexcept
-  // {
-  //   return Lit{*this, true};
-  // }
+  [[nodiscard]] constexpr Lit operator~() const noexcept;
+  [[nodiscard]] constexpr operator Lit() const noexcept;
 }; // Var
 
 static_assert(Var::max_index() == static_cast<uint32_t>(INT32_MAX - 1), "unexpected max variable index");
@@ -94,6 +90,10 @@ static_assert(Var::max_index() < Var::invalid().index(), "valid variable indices
   return !operator==(lhs, rhs);
 }
 
+[[nodiscard]] constexpr bool operator<(Var lhs, Var rhs) noexcept
+{
+  return lhs.index() < rhs.index();
+}
 
 
 /// Boolean literals represented by integer index.
@@ -127,6 +127,12 @@ public:
   explicit constexpr Lit(Var var, bool positive) noexcept
       : Lit{2 * var.index() + static_cast<uint32_t>(!positive)}
   {
+  }
+
+  [[nodiscard]] static constexpr Lit from_index(uint32_t index) noexcept
+  {
+    assert(index <= Lit::max_index());
+    return Lit{index};
   }
 
   [[nodiscard]] static constexpr Lit pos(Var var) noexcept
@@ -197,6 +203,21 @@ static_assert(Lit::max_index() < Lit::invalid().index(), "valid literal indices 
 
 
 
+
+[[nodiscard]] constexpr Lit Var::operator~() const noexcept
+{
+  return Lit{*this, false};
+}
+
+[[nodiscard]] constexpr Var::operator Lit() const noexcept
+{
+  return Lit{*this, true};
+}
+
+
+
+
+
 inline void* subsat_alloc(std::size_t size)
 {
 #ifdef SUBSAT_STANDALONE
@@ -225,36 +246,60 @@ inline void subsat_dealloc(void* p)
 
 
 class Clause final {
-  uint32_t m_size;   // number of literals
-  Lit m_literals[2]; // actual size is m_size, but C++ does not officially support flexible array members (as opposed to C)
-
 public:
-  using iterator = Lit const*;
+  using iterator = Lit*;
+  using const_iterator = Lit const*;
+  using size_type = uint32_t;
 
-  [[nodiscard]] iterator begin() const noexcept
+  [[nodiscard]] iterator begin() noexcept
   {
     return &m_literals[0];
   }
 
-  [[nodiscard]] iterator end() const noexcept
+  [[nodiscard]] iterator end() noexcept
   {
     return begin() + m_size;
   }
 
-  Lit& operator[](size_t idx) noexcept
+  // [[nodiscard]] const_iterator begin() const noexcept
+  // {
+  //   return &m_literals[0];
+  // }
+
+  // [[nodiscard]] const_iterator end() const noexcept
+  // {
+  //   return begin() + m_size;
+  // }
+
+  [[nodiscard]] const_iterator cbegin() const noexcept
+  {
+    return &m_literals[0];
+  }
+
+  [[nodiscard]] const_iterator cend() const noexcept
+  {
+    return cbegin() + m_size;
+  }
+
+  Lit& operator[](size_type idx) noexcept
   {
     assert(idx < m_size);
     return m_literals[idx];
   }
 
-  Lit const& operator[](size_t idx) const noexcept
+  Lit const& operator[](size_type idx) const noexcept
   {
     assert(idx < m_size);
     return m_literals[idx];
+  }
+
+  size_type size() const noexcept
+  {
+    return m_size;
   }
 
   /// Number of bytes required by a clause containing 'size' literals.
-  static size_t bytes(uint32_t size) noexcept
+  static size_t bytes(size_type size) noexcept
   {
     size_t const embedded_literals = std::extent_v<decltype(m_literals)>;
     size_t const additional_literals = (size >= embedded_literals) ? (size - embedded_literals) : 0;
@@ -263,13 +308,13 @@ public:
   }
 
   /// Allocate a clause with enough space for 'size' literals.
-  static Clause* create(uint32_t size)
+  static Clause* create(size_type size)
   {
     void* p = subsat_alloc(bytes(size));
     return new (p) Clause{size};
   }
 
-  // static void* operator new(size_t, uint32_t num_literals)
+  // static void* operator new(size_t, size_type num_literals)
   // {
   //   size_t const contained_literals = std::extent_v<decltype(m_literals)>;
   //   size_t const additional_literals = std::max(0, static_cast<size_t>(num_literals) - contained_literals);
@@ -280,7 +325,7 @@ public:
 private:
   // NOTE: do not use this constructor directly
   // because it does not allocate enough memory for the literals
-  Clause(uint32_t size) noexcept
+  Clause(size_type size) noexcept
       : m_size{size}
   {
   }
@@ -290,6 +335,10 @@ private:
   Clause(Clause&&) = delete;
   Clause& operator=(Clause&) = delete;
   Clause& operator=(Clause&&) = delete;
+
+private:
+  size_type m_size;    // number of literals
+  Lit m_literals[2];  // actual size is m_size, but C++ does not officially support flexible array members (as opposed to C)
 }; // Clause
 
 template <typename Key>
@@ -359,19 +408,39 @@ private:
   std::vector<T> m_data;
 };
 
-
-
-
-class Watch {
+enum class Result {
+  Sat = 10,
+  Unsat = 20,
 };
 
-using ClauseRef = uint32_t;
+std::ostream& operator<<(std::ostream& os, Result r)
+{
+  switch (r) {
+    case Result::Sat:
+      os << "satisfiable";
+      break;
+    case Result::Unsat:
+      os << "unsatisfiable";
+      break;
+  }
+  return os;
+}
+
+
+
+
+using ClauseRef = uint32_t;  // TODO: make this a struct, with a function to check validity? (operator bool?)
 using Level = uint32_t;
 #define InvalidClauseRef (std::numeric_limits<ClauseRef>::max())
 
 struct VarInfo {
   Level level;
   ClauseRef reason;
+};
+
+struct Watch {
+  // TODO: optimizations: binary clause, blocking literal
+  ClauseRef clause;
 };
 
 
@@ -383,17 +452,17 @@ public:
   [[nodiscard]] Var new_variable()
   {
     reserve_variables(1);
-    return Var{m_used_variables++};
+    return Var{m_used_vars++};
   }
 
   /// Reserve space for n additional variables.
   void reserve_variables(uint32_t count)
   {
-    if (m_used_variables + count <= m_allocated_variables) {
+    if (m_used_vars + count <= m_allocated_vars) {
       return;
     }
-    m_allocated_variables = m_used_variables + count;
-    m_values.reserve(2 * m_allocated_variables);
+    m_allocated_vars = m_used_vars + count;
+    m_values.reserve(2 * m_allocated_vars);
     // TODO
   }
 
@@ -415,15 +484,43 @@ public:
   {
   }
 
-  void solve()
+  Result solve()
   {
-    propagate_units();
+
+    if (m_inconsistent) {
+      return Result::Unsat;
+    }
+
+    // propagate_units();  // TODO do this later when we add optimizations
+
+    while (true) {
+      ClauseRef conflict = propagate();
+      if (conflict != InvalidClauseRef) {
+        if (!analyze(conflict)) {
+          return Result::Unsat;
+        }
+      } else {
+        if (m_unassigned_vars == 0) {
+          return Result::Sat;
+        } else {
+          // TODO: restart? switch mode? reduce clause db?
+          decide();
+        }
+      }
+    }
   }
 
 private:
+  Clause* get_clause(ClauseRef ref)
+  {
+    assert(ref != InvalidClauseRef);
+    assert(ref < m_clauses.size());
+    return m_clauses[ref];
+  }
+
   /// Set the literal to true.
   /// Precondition: literal is not assigned.
-  bool assign(Lit lit, ClauseRef reason)
+  void assign(Lit lit, ClauseRef reason)
   {
     // TODO: log
 
@@ -455,8 +552,8 @@ private:
 
     m_trail.push_back(lit);
 
-    assert(m_unassigned > 0);
-    m_unassigned -= 1;
+    assert(m_unassigned_vars > 0);
+    m_unassigned_vars -= 1;
   }
 
   void propagate_units()
@@ -466,23 +563,187 @@ private:
     }
   }
 
-  void propagate()
+  /// Make a decision.
+  void decide()
   {
-    while (m_propagate_head < m_trail.size()) {
-    }
+    // TODO
   }
 
-  void analyze()
+  /// Unit propagation.
+  /// Returns the conflicting clause when a conflict is encountered,
+  /// or InvalidClauseRef if all unit clauses have been propagated without conflict.
+  ClauseRef propagate()
   {
+    while (m_propagate_head < m_trail.size()) {
+      // p is the next literal to be propagated
+      Lit const lit = m_trail[m_propagate_head];
+      ClauseRef const conflict = propagate_literal(lit);
+      if (conflict != InvalidClauseRef) {
+        return conflict;
+      }
+    }
+    return InvalidClauseRef;
   }
+
+  /// Unit propagation for the given literal.
+  ClauseRef propagate_literal(Lit lit)
+  {
+    assert(checkInvariants());
+    assert(m_values[lit] == Value::True);
+
+    Lit const not_lit = ~lit;
+    std::vector<Watch>& watches = m_watches[not_lit];
+
+    auto q = watches.begin();   // points to updated watch, follows p
+    auto p = watches.cbegin();  // points to currently processed watch
+
+    ClauseRef conflict = InvalidClauseRef;
+
+    while (conflict == InvalidClauseRef && p != watches.cend()) {
+      Watch const& watch = *p;
+      *q++ = *p++;  // keep the watch by default
+
+      // TODO: binary clause optimization
+      // if (p->binary) {
+      // ...
+      // } else {
+      // ... (current code here)
+      // }
+
+      // TODO: blocking literal optimization
+
+      ClauseRef const clause_ref = watch.clause;
+      Clause& clause = *get_clause(clause_ref);
+      assert(clause.size() >= 2);
+
+      // The two watched literals of a clause are stored as the first two literals,
+      // but we don't know which one is not_lit and which one is the other one.
+      // We use this XOR trick to get other_lit without branching.
+      assert(clause[0] == not_lit || clause[1] == not_lit);
+      Lit const other_lit = Lit::from_index( clause[0].index() ^ clause[1].index() ^ not_lit.index() );
+      Value const other_value = m_values[other_lit];
+
+      // Note that other_lit may be different from the blocking literal,
+      // so we must check its value here.
+      if (other_value == Value::True) {
+        // TODO: update blocking literal to other_lit
+        continue;
+      }
+
+      // Normalize watched literal positions
+      clause[0] = other_lit;
+      clause[1] = not_lit;
+
+      // Search for a replacement for not_lit, should be true or unassigned
+      Lit replacement = Lit::invalid();
+      Value replacement_value = Value::False;
+
+      auto replacement_it = clause.begin() + 2;
+      for (; replacement_it != clause.end(); ++replacement_it) {
+        replacement = *replacement_it;
+        replacement_value = m_values[replacement];
+        if (replacement_value != Value::False) {
+          break;
+        }
+      }
+
+      if (replacement_value == Value::True) {
+        // The replacement literal is true,
+        // so it's enough to update the blocking literal.
+        // TODO: update blocking literal to replacement
+        // TODO: think about why this works. (if replacement was assigned after not_lit, it must still have been in the same decision level? so when we backtrack, we're guaranteed to undo both.)
+        assert(m_vars[replacement.var()].level == m_vars[not_lit.var()].level);
+      }
+      else if (replacement_value == Value::Unknown) {
+        // The replacement literal is unassigned.
+        // Unwatch not_lit
+        --q;
+        // Swap watched literal with its replacement
+        clause[1] = replacement;
+        *replacement_it = not_lit;
+        // Watch the replacement literal
+        watch_literal(replacement, /* TODO: other_lit, */ clause_ref);
+      }
+      else if (other_value != Value::Unknown) {
+        // All literals in the clause are false => conflict
+        assert(other_value == Value::False);
+        conflict = watch.clause;
+      }
+      else {
+        // All literals except other_lit are false => propagate
+        assert(other_value == Value::Unknown);
+        assign(other_lit, clause_ref);
+      }
+
+    }  // while
+
+    // Copy remaining watches
+    while (p != watches.cend()) {
+      *q++ = *p++;
+    }
+    watches.resize(std::distance(watches.begin(), q));
+    assert(watches.end() == q);
+
+    return conflict;
+  }  // propagate_literal
+
+
+  /// Watch literal 'lit' in the given clause.
+  void watch_literal(Lit lit, /* TODO: Lit blocking_lit, */ ClauseRef clause_ref)
+  {
+    m_watches[lit].push_back(Watch{ .clause = clause_ref });
+  }
+
+
+  /// Analyze conflict, learn a clause, backjump.
+  /// Returns true if the search should continue.
+  bool analyze(ClauseRef conflict_ref)
+  {
+    assert(conflict_ref != InvalidClauseRef);
+    Clause* conflict = get_clause(conflict_ref);
+    // TODO
+  }
+
+  void backtrack(Level new_level)
+  {
+    assert(new_level <= m_level);
+    // TODO
+  }
+
+#ifndef NDEBUG
+  [[nodiscard]] bool checkInvariants() const
+  {
+    // assigned vars + unassiged vars = used vars
+    assert(m_trail.size() + m_unassigned_vars == m_used_vars);
+
+    // Every variable is at most once on the trail
+    std::set<Var> trail_vars;
+    for (Lit lit : m_trail) {
+      auto [_, inserted] = trail_vars.insert(lit.var());
+      assert(inserted);
+    }
+    assert(trail_vars.size() == m_trail.size());
+    assert(m_trail.size() <= m_used_vars);
+
+    // TODO: (after optimizations) Every stored clause has size >= 3
+
+    // TODO: watch invariants
+    // 1. status of watch literals
+    // 2. watched literals are always the first two
+    // 3. every clause in m_clauses is watched (?) [may not work with deletion?]
+
+    return true;
+  }
+#endif
 
 private:
   bool m_inconsistent;
-  uint32_t m_used_variables;
-  uint32_t m_allocated_variables;
-  uint32_t m_unassigned;  ///< Number of variables that have not yet been assigned
-  std::vector<Lit> m_units;
-  ivector<Lit, ClauseRef> m_reasons;
+  uint32_t m_used_vars;
+  uint32_t m_allocated_vars;
+  uint32_t m_unassigned_vars; ///< Number of variables that have not yet been assigned
+
+  /// Current decision level
+  Level m_level;
 
   /// Current assignment of literals.
   /// We store the value for both literal polarities to make the lookup simpler and branchless.
@@ -492,8 +753,8 @@ private:
   ivector<Var, VarInfo> m_vars;
 
   ivector<ClauseRef, Clause*> m_clauses;
-  ivector<Lit, Watch> m_watches;
-  Level m_level;  ///< Current decision level
+  std::vector<Lit> m_units;
+  ivector<Lit, std::vector<Watch>> m_watches;
 
   std::vector<Lit> m_trail;  ///< Currently true literals in order of assignment; TODO: pre-allocate to #variables
   uint32_t m_propagate_head;  // index into trail, the next literal to propagate
