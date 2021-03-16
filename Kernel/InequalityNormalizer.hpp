217188fd3908bce8aedc16e18fb774f3b2e998dd
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2021-03-16 15:24:49 +0000

    more fine-grainded handling of overflows in evaluation

--- Kernel/InequalityNormalizer.hpp
+++ Kernel/InequalityNormalizer.hpp
@@ -92,8 +92,8 @@ namespace Kernel {
       : _eval(eval) {  }
 
     template<class NumTraits> Option<InequalityLiteral<NumTraits>> normalize(Literal* lit) const;
-    template<class NumTraits> Literal* denormalize(InequalityLiteral<NumTraits> const& lit) const;
   };
+
 
 
 }
@@ -108,7 +108,7 @@ namespace Kernel {
   Option<InequalityLiteral<NumTraits>> InequalityNormalizer::normalize(Literal* lit) const
   {
     CALL("InequalityLiteral<NumTraits>::fromLiteral(Literal*)")
-    DEBUG("in: ", *lit, "(", NumTraits::name(), ")")
+    DEBUG("in: ", *lit, " (", NumTraits::name(), ")")
 
     auto impl = [&]() {
 
