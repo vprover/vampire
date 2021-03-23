529695768a428fb607a5e02e1c0d4cfbc9062512
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2021-03-23 11:42:57 +0000

    refactoring

--- Shell/SearchSpaceDumper.cpp
+++ Shell/SearchSpaceDumper.cpp
@@ -42,80 +42,48 @@ SearchSpaceDumper::~SearchSpaceDumper()
   }
 }
 
-#define SETUP_SERIALIZE_CACHED(key_t, key, use_cache) \
-  BYPASSING_ALLOCATOR \
-  key_t ref = key; \
-  auto& cache = caches.use_cache; \
-  auto iter = cache.find(ref); \
-  if (iter != cache.end()) { \
-    return iter->second; \
-  } \
-  auto idx = caches.serialized.size(); \
-  cache[ref] = idx; \
-  caches.serialized.push_back(json()); \
+struct SerializationContext 
+{
+  map<Clause const*, unsigned> clauses;
+  map<Literal const*, unsigned> literals;
+  map<Term const*, unsigned> terms;
+  map<unsigned, unsigned> functions;
+  map<unsigned, unsigned> predicates;
 
-#define SETUP_SERIALIZE_UNCACHED(key_t, key, use_cache) \
-  BYPASSING_ALLOCATOR \
-  auto idx = caches.serialized.size(); \
-  caches.serialized.push_back(json()); \
+  vector<json> objects;
 
-#define __SERIALIZE_SIG(Type) \
-  polymorphic_serialize& caches, \
-  const Type& self \
-
-struct polymorphic_serialize;
-
-template<class A>
-unsigned serialize(__SERIALIZE_SIG(A));
-
-struct polymorphic_serialize {
-
-  map<const void*, unsigned>& indices;
-  map<unsigned, unsigned>& functions;
-  map<unsigned, unsigned>& predicates;
-  vector<json>& serialized;
+  SerializationContext() {}
+  SerializationContext(SerializationContext const&) = delete;
 
   template<class B>
   unsigned operator()(const B& ser) { return serialize(*this, ser); }
+
+  json toJson() const 
+  {
+    return json(objects);
+  }
 };
 
-// template<class A>
-// unsigned serialize(__SERIALIZE_SIG(A)) {
-//   SETUP_SERIALIZE(&self, indices, const void*)
-//
-//   auto ser = _serialize(self, caches);
-//
-//   json j;
-//   j[get<0>(ser)] = get<1>(ser);
-//
-//   caches.serialized[idx] = j;
-//
-//
-//   return idx;
-// }
+struct Predicate 
+{ unsigned functor; };
 
-struct Function;
-struct Predicate;
-struct Predicate {
-  unsigned functor;
-};
-
-struct Function {
+struct Function 
+{
   unsigned functor;
   const Term& term;
 };
 
 #define __SER_CONST__Predicate 
 
-#define __SER_CONST__Function_(ConstantType, key, ...) \
-  { \
-    ConstantType c; \
-    if (theory->tryInterpretConstant(&fun, c)) { \
-      json j; \
-      j[key] = __VA_ARGS__; \
-      return j; \
-    } \
-  } \
+#define __SER_CONST__Function_(ConstantType, key, ...)                                                        \
+  {                                                                                                           \
+    ConstantType c;                                                                                           \
+    if (theory->tryInterpretConstant(&fun, c)) {                                                              \
+      json j;                                                                                                 \
+      j[key] = __VA_ARGS__;                                                                                   \
+      return j;                                                                                               \
+    }                                                                                                         \
+  }                                                                                                           \
 
 json trySerializeNumber(const Term& fun) {
   __SER_CONST__Function_(IntegerConstantType, "Int", c.toInner())
@@ -125,42 +93,42 @@ json trySerializeNumber(const Term& fun) {
 }
 
 const char* serializeInterpretation(Interpretation i) {
-#define CASE(pref, suff, expr) \
-  case Kernel::Theory::Interpretation::pref ## suff: return expr; \
+#define CASE(pref, suff, expr)                                                                                \
+  case Kernel::Theory::Interpretation::pref ## suff: return expr;                                             \
 
-#define NUM_CASES(SORT) \
-    CASE(SORT, _GREATER , "Greater") \
-    CASE(SORT, _GREATER_EQUAL , "GreaterEqual") \
-    CASE(SORT, _LESS , "Less") \
-    CASE(SORT, _LESS_EQUAL , "LessEqual") \
- \
-    CASE(SORT, _UNARY_MINUS, "Neg") \
-    CASE(SORT, _PLUS, "Add") \
-    CASE(SORT, _MINUS, "Sub") \
-    CASE(SORT, _MULTIPLY, "Mul") \
-    CASE(SORT, _QUOTIENT_E, "QuotientE") \
-    CASE(SORT, _QUOTIENT_T, "QuotientT") \
-    CASE(SORT, _QUOTIENT_F, "QuotientF") \
-    CASE(SORT, _REMAINDER_E, "RemainderE") \
-    CASE(SORT, _REMAINDER_T, "RemainderT") \
-    CASE(SORT, _REMAINDER_F, "RemainderF") \
-    CASE(SORT, _FLOOR, "Floor") \
-    CASE(SORT, _CEILING, "Ceiling") \
-    CASE(SORT, _TRUNCATE, "Truncate") \
-    CASE(SORT, _ROUND, "Round") \
+#define NUM_CASES(SORT)                                                                                       \
+    CASE(SORT, _GREATER , "Greater")                                                                          \
+    CASE(SORT, _GREATER_EQUAL , "GreaterEqual")                                                               \
+    CASE(SORT, _LESS , "Less")                                                                                \
+    CASE(SORT, _LESS_EQUAL , "LessEqual")                                                                     \
+                                                                                                              \
+    CASE(SORT, _UNARY_MINUS, "Neg")                                                                           \
+    CASE(SORT, _PLUS, "Add")                                                                                  \
+    CASE(SORT, _MINUS, "Sub")                                                                                 \
+    CASE(SORT, _MULTIPLY, "Mul")                                                                              \
+    CASE(SORT, _QUOTIENT_E, "QuotientE")                                                                      \
+    CASE(SORT, _QUOTIENT_T, "QuotientT")                                                                      \
+    CASE(SORT, _QUOTIENT_F, "QuotientF")                                                                      \
+    CASE(SORT, _REMAINDER_E, "RemainderE")                                                                    \
+    CASE(SORT, _REMAINDER_T, "RemainderT")                                                                    \
+    CASE(SORT, _REMAINDER_F, "RemainderF")                                                                    \
+    CASE(SORT, _FLOOR, "Floor")                                                                               \
+    CASE(SORT, _CEILING, "Ceiling")                                                                           \
+    CASE(SORT, _TRUNCATE, "Truncate")                                                                         \
+    CASE(SORT, _ROUND, "Round")                                                                               \
 
 
-#define _CONVERSIONS(SORT, _CONV_, str) \
-  CASE(INT, _CONV_##SORT, str) \
-  CASE(RAT, _CONV_##SORT, str) \
-  CASE(REAL, _CONV_##SORT, str) \
+#define _CONVERSIONS(SORT, _CONV_, str)                                                                       \
+  CASE(INT, _CONV_##SORT, str)                                                                                \
+  CASE(RAT, _CONV_##SORT, str)                                                                                \
+  CASE(REAL, _CONV_##SORT, str)                                                                               \
 
-#define CONVERSIONS(_CONV_, str) \
-    _CONVERSIONS(INT , _CONV_, str) \
-    _CONVERSIONS(RAT , _CONV_, str) \
-    _CONVERSIONS(REAL, _CONV_, str) \
+#define CONVERSIONS(_CONV_, str)                                                                              \
+    _CONVERSIONS(INT , _CONV_, str)                                                                           \
+    _CONVERSIONS(RAT , _CONV_, str)                                                                           \
+    _CONVERSIONS(REAL, _CONV_, str)                                                                           \
 
-#define FRAC_CASES(SORT) \
+#define FRAC_CASES(SORT)                                                                                      \
   CASE(SORT, _QUOTIENT, "Quot")
 
   switch (i) {
@@ -195,149 +163,177 @@ const char* serializeInterpretation(Interpretation i) {
 #define IF_FUN_Predicate(...)
 #define IF_FUN_Function(...) __VA_ARGS__
 
-#define _SERIALIZE_FUN_PRED(pred, Predicate) \
-template<class Ser> tuple<const char*, json> _serialize(const Predicate& self, Ser serial) { \
-  json j; \
-  j["name"] = env.signature->get ## Predicate(self.functor)->name(); \
-  json inter; \
-  if (theory->isInterpreted ## Predicate(self.functor)) { \
-    inter = serializeInterpretation(theory->interpret ## Predicate(self.functor));\
-  } \
-  IF_FUN_ ## Predicate (\
-    else { \
-      inter = trySerializeNumber(self.term); \
-    } \
-  ) \
-  \
-  j["inter"] = inter; \
-  return {pred, j}; \
-} \
+#define _SERIALIZE_FUN_PRED(pred, Predicate)                                                                  \
+  json j;                                                                                                     \
+  j["name"] = env.signature->get ## Predicate(self.functor)->name();                                          \
+  json inter;                                                                                                 \
+  if (theory->isInterpreted ## Predicate(self.functor)) {                                                     \
+    inter = serializeInterpretation(theory->interpret ## Predicate(self.functor));                            \
+  }                                                                                                           \
+  IF_FUN_ ## Predicate (                                                                                      \
+    else {                                                                                                    \
+      inter = trySerializeNumber(self.term);                                                                  \
+    }                                                                                                         \
+  )                                                                                                           \
+                                                                                                              \
+  j["inter"] = inter;                                                                                         \
+  return {pred, j};                                                                                           \
 
-_SERIALIZE_FUN_PRED("Pred", Predicate)
-_SERIALIZE_FUN_PRED("Fun", Function)
+template<class C> struct SerializeCached;
 
-template<class Ser> tuple<const char*, json> _serialize(const Term& self, Ser serial) {
-  json j;
-  Function fun = {.functor = self.functor(), .term = self};
-  j["fun"] = serial(fun);
-  vector<unsigned> terms;
-  for (int i = 0; i < self.arity(); i++) {
-    terms.push_back(serial(self[i]));
+template<> struct SerializeCached<Clause>
+{ 
+  static constexpr bool value = true; 
+  static auto id      (Clause const& c)           -> Clause const*          { return &c;          }
+  static auto getCache(SerializationContext& ser) -> decltype(ser.clauses)& { return ser.clauses; }
+  static auto toJson(Clause const& self, SerializationContext& serial) -> tuple<const char*, json> {
+    json j;
+    j["thry_desc"] = self.isPureTheoryDescendant();
+
+    json lits = json::array();
+    for (int i = 0; i < self.size(); i++) {
+      lits[i] = serial(*self[i]);
+    }
+
+    j["lits"] = lits;
+    return { "Clause", j };
   }
-  j["terms"] = terms;
-  return { "Cterm", j };
-}
+};
 
-template<class Ser> tuple<const char*, json> _serialize(const TermList& self, Ser serial) {
-  json j; 
-  if (self.isTerm()) {
-    j["Cterm"] = serial(*self.term());
-  } else if (self.isVar()) {
-    j["Var"] = self.var();
-  } else {
-    ASSERTION_VIOLATION
+template<> struct SerializeCached<Literal>
+{ 
+  static constexpr bool value = true; 
+  static auto id      (Literal const& x)          -> Literal const*          { return &x;           }
+  static auto getCache(SerializationContext& ser) -> decltype(ser.literals)& { return ser.literals; }
+  static auto toJson(Literal const& self, SerializationContext& serial) -> tuple<const char*, json> {
+    json j;
+    j["pos"] = self.isPositive();
+    Predicate p {.functor = self.functor()};
+    j["pred"] = serial(p);
+    vector<unsigned> terms;
+    for (int i = 0; i < self.arity(); i++) {
+      auto x  = serial(self[i]);
+      // DBG(self[i].toString()," -> ", x)
+      terms.push_back(x);
+    }
+    j["terms"] = terms;
+    return { "Lit", j };
   }
-  return { "Term", j };
-}
+};
 
-template<class Ser> tuple<const char*, json> _serialize(const Clause& self, Ser serial) {
-  json j;
-  j["thry_desc"] = self.isPureTheoryDescendant();
-
-  json lits = json::array();
-  for (int i = 0; i < self.size(); i++) {
-    lits[i] = serial(*self[i]);
+template<> struct SerializeCached<Term>
+{ 
+  static constexpr bool value = true; 
+  static auto id      (Term const& x)             -> Term const*          { return &x;        }
+  static auto getCache(SerializationContext& ser) -> decltype(ser.terms)& { return ser.terms; }
+  static auto toJson(Term const& self, SerializationContext& serial) -> tuple<const char*, json> {
+    json j;
+    Function fun = {.functor = self.functor(), .term = self};
+    j["fun"] = serial(fun);
+    vector<unsigned> terms;
+    for (int i = 0; i < self.arity(); i++) {
+      terms.push_back(serial(self[i]));
+    }
+    j["terms"] = terms;
+    return { "Cterm", j };
   }
+};
 
-  j["lits"] = lits;
-  return { "Clause", j };
-}
+template<> struct SerializeCached<Predicate>
+{ 
+  static constexpr bool value = true; 
+  static auto id      (Predicate const& x)         -> decltype(x.functor)       { return x.functor;   }
+  static auto getCache(SerializationContext& ser) -> decltype(ser.predicates)& { return ser.predicates; }
+  static auto toJson(Predicate const& self, SerializationContext& sef) -> tuple<const char*, json> 
+  { _SERIALIZE_FUN_PRED("Pred", Predicate) }
+};
 
+template<> struct SerializeCached<Function>
+{ 
+  static constexpr bool value = true; 
+  static auto id      (Function const& x)          -> decltype(x.functor)      { return x.functor;   }
+  static auto getCache(SerializationContext& ser) -> decltype(ser.functions)& { return ser.functions; }
+  static auto toJson(Function const& self, SerializationContext& sef) -> tuple<const char*, json> 
+  { _SERIALIZE_FUN_PRED("Fun", Function) }
+};
 
-template<class Ser> tuple<const char*, json> _serialize(const Literal& self, Ser serial) {
-  json j;
-  j["pos"] = self.isPositive();
-  Predicate p {.functor = self.functor()};
-  j["pred"] = serial(p);
-  vector<unsigned> terms;
-  for (int i = 0; i < self.arity(); i++) {
-    auto x  = serial(self[i]);
-    // DBG(self[i].toString()," -> ", x)
-    terms.push_back(x);
+template<> struct SerializeCached<TermList> 
+{ 
+  static constexpr bool value = false; 
+  static auto toJson(TermList const& self, SerializationContext& serial) -> tuple<const char*, json> {
+    json j; 
+    if (self.isTerm()) {
+      j["Cterm"] = serial(*self.term());
+    } else if (self.isVar()) {
+      j["Var"] = self.var();
+    } else {
+      ASSERTION_VIOLATION
+    }
+    return { "Term", j };
   }
-  j["terms"] = terms;
-  return { "Lit", j };
-}
+};
 
-#define __SERIALIZE(cached, key_t, key, cache) \
-    SETUP_SERIALIZE_ ## cached(key_t, key, cache) \
-   \
-    auto ser = _serialize(self, caches); \
-   \
-    json j; \
-    j[get<0>(ser)] = get<1>(ser); \
-   \
-    caches.serialized[idx] = j; \
-   \
-    return idx; \
+template<class C, typename std::enable_if<SerializeCached<C>::value, bool>::type = true>
+unsigned setupKey(SerializationContext& ctxt, C const& self) 
+{
+    BYPASSING_ALLOCATOR                    
+    auto& cache = SerializeCached<C>::getCache(ctxt);          
+    auto id = SerializeCached<C>::id(self);
+    auto iter = cache.find(id);              
+    if (iter != cache.end()) {                 
+      return iter->second;                      
+    }                                            
+    unsigned idx = ctxt.objects.size();
+    cache[id] = idx;                    
+    ctxt.objects.push_back(json()); 
+    return idx;
+};
 
-#define CACHED_SERIALIZE(Type, key_t, key, cache) \
-  template<> \
-  unsigned serialize<Type>(__SERIALIZE_SIG(Type)) {\
-    __SERIALIZE(CACHED, key_t, key, cache) \
-  } \
+template<class C, typename std::enable_if<!SerializeCached<C>::value, bool>::type = true>
+unsigned setupKey(SerializationContext& ctxt, C const& self) 
+{
+    BYPASSING_ALLOCATOR                
+    auto idx = ctxt.objects.size();
+    ctxt.objects.push_back(json());
+    return idx;
+};
 
-#define UNCACHED_SERIALIZE(Type, key_t, key, cache) \
-  template<> \
-  unsigned serialize<Type>(__SERIALIZE_SIG(Type)) {\
-    __SERIALIZE(UNCACHED, key_t, key, cache) \
-  } \
-
-
-CACHED_SERIALIZE(Function, unsigned, self.functor, functions)
-CACHED_SERIALIZE(Predicate, unsigned, self.functor, predicates)
-CACHED_SERIALIZE(Term, const void*, &self, indices)
-CACHED_SERIALIZE(Clause, const void*, &self, indices)
-CACHED_SERIALIZE(Literal, const void*, &self, indices)
-UNCACHED_SERIALIZE(TermList, -, -, -)
-
-
-
+template<class C>
+unsigned serialize(SerializationContext& ctxt, C const& self) 
+{
+  unsigned idx = setupKey<C>(ctxt, self);
+                                                        
+  auto ser = SerializeCached<C>::toJson(self, ctxt);                   
+                                                          
+  json j;                                                  
+  j[get<0>(ser)] = get<1>(ser);                             
+                                                             
+  ctxt.objects[idx] = j;                                 
+                                                               
+  return idx;
+};
 
 void SearchSpaceDumper::dumpFile(const vstring& out) const 
 {
   CALL("SearchSpaceDumper::dumpFile")
   DBG("dumping searchspace to file ", env.options->searchSpaceOutput());
   BYPASSING_ALLOCATOR
-  std::map<const void*, unsigned> indices;
-  std::map<unsigned, unsigned> predicates;
-  std::map<unsigned, unsigned> functions;
-  std::vector<json> serialized;
 
-  auto caches = polymorphic_serialize {
-    .indices = indices,
-    .functions = functions,
-    .predicates = predicates,
-    .serialized = serialized,
-  };
+  SerializationContext ctxt;
 
   DBG("serializing...");
   for (auto c : _clauses) {
-    serialize(caches, *c);
+    serialize(ctxt, *c);
   }
   DBG("writing...");
   cout << out.c_str() << endl;
   ofstream f{ out.c_str() };
-  // f << json(serialized) << endl;
-  f << json(serialized).dump(3) << endl; 
+
+  f << ctxt.toJson().dump(3) << endl; 
 
   DBG("finished.");
-  
-  // pk.pack(std::string("Log message .packer 1"));
-  // pk.pack(std::string("Log message ... 2"));
-  // pk.pack(std::string("Log message ... 3"));
-
 }
+
 void SearchSpaceDumper::add(Kernel::Clause* clause) 
 {
   clause->incRefCnt();
