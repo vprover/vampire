d67c8de77b7f73bfe615f1a79e7e373cf08f6acb
Jakob Rath <git@jakobrath.eu>
2021-03-26 19:11:34 +0100

    No need to record aux value for unit clauses

--- Inferences/ForwardSubsumptionAndResolution.cpp
+++ Inferences/ForwardSubsumptionAndResolution.cpp
@@ -259,10 +259,6 @@ bool ForwardSubsumptionAndResolution::perform(Clause* cl, Clause*& replacement, 
     SLQueryResultIterator rit=_unitIndex->getGeneralizations( (*cl)[li], false, false);
     while(rit.hasNext()) {
       Clause* premise=rit.next().clause;
-      if(premise->hasAux()) {
-	continue;
-      }
-      premise->setAux(nullptr);
       if(ColorHelper::compatible(cl->color(), premise->color()) ) {
         premises = pvi( getSingletonIterator(premise) );
         env.statistics->forwardSubsumed++;
