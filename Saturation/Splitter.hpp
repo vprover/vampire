52b0d10f066f2deeb8c802427d2068680884d500
Martin Suda <sudam@cs.man.ac.uk>
2021-05-22 17:46:09 +0200

    the actual deepire implementation in SaturationAlg and Splitter

--- Saturation/Splitter.hpp
+++ Saturation/Splitter.hpp
@@ -208,6 +208,11 @@ public:
   }
   Clause* getComponentClause(SplitLevel name) const;
 
+  Clause* getCausalParent(Clause* compCl) const {
+    ASS(compCl->isComponent());
+    return _causalParents.get(compCl);
+  }
+
   SplitLevel splitLevelCnt() const { return _db.size(); }
   unsigned maxSatVar() const { return _sat2fo.maxSATVar(); }
 
@@ -276,6 +281,8 @@ private:
    */
   Stack<SplitRecord*> _db;
   DHMap<Clause*,SplitLevel> _compNames;
+
+  DHMap<Clause*,Clause*> _causalParents;
 
   /**
    * Definitions of ground components C and ~C are shared and placed at the slot of C.
