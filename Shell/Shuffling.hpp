26edb4954259f926372929db860d58b7ce66379f
Martin Suda <sudam@cs.man.ac.uk>
2021-06-10 16:11:39 +0200

    deep shuffling of formulas; recursive version

--- Shell/Shuffling.hpp
+++ Shell/Shuffling.hpp
@@ -36,9 +36,13 @@ public:
   void shuffle(Problem&);
   void shuffle(UnitList*&);
 
+  // shuffling should not require allocating new memory, so all the objects are modified "in place"
+
   void shuffle(Unit*);     // shuffle a unit; just dispatches to either shuffle(Clause*) or shuffle(Formula*)
   void shuffle(Clause*);   // shuffling a clause; it assumes literals are shared (i.e. nothing "special" in them anyway) so it does not touch those
   void shuffle(Formula*);  // shuffling a formula; will try to descend also to the term level, to shuffle around commutative operators and formulas inside terms (if applicable)
+  void shuffle(Literal*);
+  void shuffle(TermList);
 
   template<typename Arrayish>
   void shuffleArray(Arrayish& a, unsigned len) {
@@ -80,6 +84,48 @@ public:
 
     list = aux[0];
   }
+
+  // list2 is assumed to be of the same length as list1; they are suffled "in sync"
+  template<typename T, typename S>
+  void shuffleTwoList(List<T>*& list1, List<S>*& list2) {
+    CALL("Shuffling::shuffleTwoList");
+
+    unsigned len = List<T>::length(list1);
+
+    if (len <= 1) {
+      return;
+    }
+
+    DArray<std::pair<List<T>*,List<S>*>> aux(len);
+    unsigned idx = 0;
+
+    List<T>* els1 = list1;
+    List<S>* els2 = list2;
+    while (els1 != nullptr) {
+      ASS_NEQ(els2,0);
+      aux[idx++] = std::make_pair(els1,els2);
+      els1 = els1->tail();
+      els2 = els2->tail();
+    }
+    ASS_EQ(els2,0);
+    shuffleArray(aux,len);
+
+    // reconnect the links
+    len--; // we need a special handling of the last element (and also a one smaller condition)
+    aux[len].first->setTail(nullptr);
+    aux[len].second->setTail(nullptr);
+    for(idx = 0; idx < len; idx++) {
+      aux[idx].first->setTail(aux[idx+1].first);
+      aux[idx].second->setTail(aux[idx+1].second);
+    }
+
+    list1 = aux[0].first;
+    list2 = aux[0].second;
+  }
+
+private:
+
+
 
 }; // class Shuffling
 
