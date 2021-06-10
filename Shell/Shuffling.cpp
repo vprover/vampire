17de9762e8683fee3f07cfb6121005bafcaae656
Martin Suda <sudam@cs.man.ac.uk>
2021-06-10 16:57:55 +0200

    an iterative version of shufling

--- Shell/Shuffling.cpp
+++ Shell/Shuffling.cpp
@@ -78,151 +78,165 @@ void Shuffling::shuffle(Clause* clause)
 
   shuffleArray(*clause,clause->length());
 
+  // literals must be shared in typical clauses (I think), so let's not even try shuffling them
+
   clause->notifyLiteralReorder();
 }
 
-// TOOD: first trying to do this recursively, then turn it into the stack solution
+// iterative implementation of shuffling Formula* / Literal* / TermList
+void Shuffling::shuffleIter(Shufflable sh) {
+  CALL("Shuffling::shuffleIter");
 
-void Shuffling::shuffle(Formula* fla)
-{
-  CALL("Shuffling::shuffle (Formula*)");
+  static Stack<Shufflable> todo;
+  ASS(todo.isEmpty());
 
-  switch (fla->connective() ) {
-  case LITERAL:
-  {
-    shuffle(fla->literal());
-    return;
-  }
-  case AND:
-  case OR:
-  {
-    // multi-ary; shuffling subformula list
-    shuffleList(*fla->argsPtr());
+  todo.push(sh);
 
-    // recurse to subformulas
-    FormulaList::Iterator fs(fla->args());
-    while (fs.hasNext()) {
-      shuffle(fs.next());
-    }
-    return;
-  }
-  case IFF:
-  case XOR:
-  {
-    // binary; can swap
-    if (Random::getBit()) {
-      fla->leftRightSwap();
-    }
-    // intentional fall-through
-  case IMP:
-    // binary; cannot simply swap
+  do {
+    ASS(todo.isNonEmpty());
+    sh = todo.pop();
 
-    // in any case, recurse to subformulas
-    shuffle(fla->left());
-    shuffle(fla->right());
-    return;
-  }
-  case NOT:
-  {
-    // unary; just bubble through
-    shuffle(fla->uarg());
-    return;
-  }
-  case FORALL:
-  case EXISTS:
-  {
-    // can even shuffle the variables in the quantifier!
-    if (fla->sorts()) { // need to shuffle sorts in sync with vars, if they are there
-      shuffleTwoList(*fla->varsPtr(),*fla->sortsPtr());
-    } else {
-      shuffleList(*fla->varsPtr());
-    }
+    sh_updated:
 
-    shuffle(fla->qarg());
-    return;
-  }
-  case BOOL_TERM:
-  {
-    // cout << "Bef: " << fla->toString() << endl;
-    shuffle(fla->getBooleanTerm());
-    // cout << "Aft: " << fla->toString() << endl;
-    return;
-  }
-  case TRUE:
-  case FALSE:
-  {
-    // bottom reached; nothing to be done
-    return;
-  }
-  case NAME: // NamedFormula is only used by AVATAR proof documentation; so ignored here
-  default:
-    ASSERTION_VIOLATION;
-  }
-}
+    if (sh.is<Formula*>()) {
+      Formula* fla = sh.unwrap<Formula*>();
 
-void Shuffling::shuffle(Literal* lit)
-{
-  CALL("Shuffling::shuffle (Literal*)");
+      fla_updated:
 
-  if (!lit->shared()) {
-    //cout << "Bef: " << lit->toString() << endl;
-
-    if (lit->commutative() && Random::getBit()) {
-      lit->argSwap();
-    }
-
-    Term::Iterator it(lit);
-    while (it.hasNext()) {
-      shuffle(it.next());
-    }
-
-    //cout << "Aft: " << lit->toString() << endl;
-  }
-}
-
-void Shuffling::shuffle(TermList tl)
-{
-  CALL("Shuffling::shuffle (TermList)");
-
-  if (tl.isTerm()) {
-    Term* t = tl.term();
-    if (!t->shared()) {
-      //cout << "Bef: " << t->toString() << endl;
-
-      if (t->isSpecial()) {
-        Term::SpecialTermData* sd = t->getSpecialData();
-        switch (sd->getType()) {
-          case Term::SF_ITE:
-            shuffle(sd->getCondition());
-            shuffle(*t->nthArgument(0));
-            shuffle(*t->nthArgument(1));
-            break;
-
-          case Term::SF_FORMULA:
-            shuffle(sd->getFormula());
-            break;
-
-          case Term::SF_LET:
-          case Term::SF_LET_TUPLE:
-            shuffle(sd->getBinding());
-            shuffle(*t->nthArgument(0));
-            break;
-
-          case Term::SF_TUPLE:
-            shuffle(TermList(sd->getTupleTerm()));
-            break;
-
-          default:
-            ASSERTION_VIOLATION_REP(tl.toString());
+      switch (fla->connective() ) {
+        case LITERAL:
+        {
+          sh = Shufflable(fla->literal());
+          goto sh_updated;
+          break; // I know, unreachable
         }
-      } else {
-        Term::Iterator it(t);
+        case AND:
+        case OR:
+        {
+          // multi-ary; shuffling subformula list
+          shuffleList(*fla->argsPtr());
+
+          // recurse to subformulas
+          FormulaList::Iterator fs(fla->args());
+          while (fs.hasNext()) {
+            todo.push(Shufflable(fs.next()));
+          }
+          break;
+        }
+        case IFF:
+        case XOR:
+        {
+          // binary; can swap
+          if (Random::getBit()) {
+            fla->leftRightSwap();
+          }
+          // intentional fall-through
+        case IMP:
+          // binary; cannot simply swap
+
+          // in any case, recurse to subformulas
+          todo.push(Shufflable(fla->left()));
+          fla = fla->right();
+          goto fla_updated;
+          break; // I know, unreachable
+        }
+        case NOT:
+        {
+          // unary; just bubble through
+          fla = fla->uarg();
+          goto fla_updated;
+          break; // I know, unreachable
+        }
+        case FORALL:
+        case EXISTS:
+        {
+          // can even shuffle the variables in the quantifier!
+          if (fla->sorts()) { // need to shuffle sorts in sync with vars, if they are there
+            shuffleTwoList(*fla->varsPtr(),*fla->sortsPtr());
+          } else {
+            shuffleList(*fla->varsPtr());
+          }
+
+          fla = fla->qarg();
+          goto fla_updated;
+          break; // I know, unreachable;
+        }
+        case BOOL_TERM:
+        {
+          sh = Shufflable(fla->getBooleanTerm());
+          goto sh_updated;
+          break; // I know, unreachable;
+        }
+        case TRUE:
+        case FALSE:
+        {
+          // bottom reached; nothing to be done
+          break;
+        }
+        case NAME: // NamedFormula is only used by AVATAR proof documentation; so ignored here
+        default:
+          ASSERTION_VIOLATION;
+        }
+    } else if (sh.is<Literal*>()) {
+      Literal* lit = sh.unwrap<Literal*>();
+
+      if (!lit->shared()) {
+        if (lit->commutative() && Random::getBit()) {
+          lit->argSwap();
+        }
+
+        Term::Iterator it(lit);
         while (it.hasNext()) {
-          shuffle(it.next());
+          todo.push(Shufflable(it.next()));
         }
       }
+    } else {
+      ASS(sh.is<TermList>());
 
-      //cout << "Aft: " << t->toString() << endl;
+      TermList tl = sh.unwrap<TermList>();
+
+      tl_updated:
+
+      if (tl.isTerm()) {
+        Term* t = tl.term();
+        if (!t->shared()) {
+          if (t->isSpecial()) {
+            Term::SpecialTermData* sd = t->getSpecialData();
+            switch (sd->getType()) {
+              case Term::SF_ITE:
+                todo.push(Shufflable(sd->getCondition()));
+                todo.push(Shufflable(*t->nthArgument(0)));
+                tl = *t->nthArgument(1);
+                goto tl_updated;
+                break; // I know, unreachable;
+
+              case Term::SF_FORMULA:
+                todo.push(Shufflable(sd->getFormula()));
+                break;
+
+              case Term::SF_LET:
+              case Term::SF_LET_TUPLE:
+                todo.push(Shufflable(sd->getBinding()));
+                tl = *t->nthArgument(0);
+                goto tl_updated;
+                break; // I know, unreachable;
+
+              case Term::SF_TUPLE:
+                tl = TermList(sd->getTupleTerm());
+                goto tl_updated;
+                break; // I know, unreachable;
+
+              default:
+                ASSERTION_VIOLATION_REP(tl.toString());
+            }
+          } else {
+            Term::Iterator it(t);
+            while (it.hasNext()) {
+              todo.push(Shufflable(it.next()));
+            }
+          }
+        }
+      }
     }
-  }
+  } while (todo.isNonEmpty());
 }
