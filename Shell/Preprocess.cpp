10b546ccbf53bb8ff16f196ef7ba0c6ddad41b35
Martin Suda <sudam@cs.man.ac.uk>
2021-06-22 11:42:41 +0200

    more shuffling during preprocessing

--- Shell/Preprocess.cpp
+++ Shell/Preprocess.cpp
@@ -111,9 +111,10 @@ void Preprocess::preprocess(Problem& prb)
   if (_options.shuffleInput()) {
     env.statistics->phase=Statistics::SHUFFLING;
     if (env.options->showPreprocessing())
-      env.out() << "shuffling" << std::endl;
+      env.out() << "shuffling1" << std::endl;
 
-    Shuffling().shuffle(prb);
+    // shuffling at the very beginning
+    Shuffling::shuffle(prb);
   }
 
   if (prb.hasInterpretedOperations()) {
@@ -225,6 +226,15 @@ void Preprocess::preprocess(Problem& prb)
     preprocess1(prb);
   }
 
+  if (_options.shuffleInput()) {
+    env.statistics->phase=Statistics::SHUFFLING;
+    if (env.options->showPreprocessing())
+      env.out() << "shuffling2" << std::endl;
+
+    // axioms have been added, fool eliminated; moreover, after flattening, more opportunity for shuffling inside
+    Shuffling::shuffle(prb);
+  }
+
   // stop here if clausification is not required
   if (!_clausify) {
     return;
@@ -250,6 +260,15 @@ void Preprocess::preprocess(Problem& prb)
       env.out() << "preprocess 2 (ennf,flatten)" << std::endl;
 
     preprocess2(prb);
+  }
+
+  if (_options.shuffleInput()) {
+    env.statistics->phase=Statistics::SHUFFLING;
+    if (env.options->showPreprocessing())
+      env.out() << "shuffling3" << std::endl;
+
+    // more flattening -> more shuffling
+    Shuffling::shuffle(prb);
   }
 
   if (prb.mayHaveFormulas() && _options.newCNF() && 
@@ -415,6 +434,15 @@ void Preprocess::preprocess(Problem& prb)
      BlockedClauseElimination bce;
      bce.apply(prb);
    }
+
+   if (_options.shuffleInput()) {
+       env.statistics->phase=Statistics::SHUFFLING;
+       if (env.options->showPreprocessing())
+         env.out() << "shuffling4" << std::endl;
+
+       // cnf and many other things happened - shuffle one more time
+       Shuffling::shuffle(prb);
+     }
 
    if (env.options->showPreprocessing()) {
      UnitList::Iterator uit(prb.units());
