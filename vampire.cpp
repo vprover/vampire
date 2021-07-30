c4431a3a8b1589a4b691c479b030ad106593828b
Martin Suda <sudam@cs.man.ac.uk>
2021-07-30 15:13:16 +0200

    make the tracing more stable by only loading it after preprocessing of the given problem

--- vampire.cpp
+++ vampire.cpp
@@ -197,15 +197,15 @@ void doProving()
   // One call to randomize before preprocessing (see Options)
   env.options->randomizeStrategy(0);
 
-  if (env.options->traceProofs().size() > 0) {
-    env.tracer = new ProofTracer();
-    env.tracer->init(env.options->traceProofs());
-  }
-
   ScopedPtr<Problem> prb(getPreprocessedProblem());
 
   // Then again when the property is here (this will only randomize non-default things if an option is set to do so)
   env.options->randomizeStrategy(prb->getProperty()); 
+
+  if (env.options->traceProofs().size() > 0) {
+    env.tracer = new ProofTracer();
+    env.tracer->init(env.options->traceProofs());
+  }
 
   // this will provide warning if options don't make sense for problem
   //env.options->checkProblemOptionConstraints(prb->getProperty()); 
