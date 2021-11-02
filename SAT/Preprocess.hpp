f8d6b1ce69e29b7b5d966a6da6cb6b287e3fb82d
Michael Rawson <michael@rawsons.uk>
2021-11-02 22:16:27 +0100

    remove unused material in SAT::Preprocess

--- SAT/Preprocess.hpp
+++ SAT/Preprocess.hpp
@@ -18,38 +18,12 @@
 
 #include "Forwards.hpp"
 
-#include "Lib/VirtualIterator.hpp"
-
-#include "SATClause.hpp"
-
 namespace SAT {
 
 class Preprocess
 {
 public:
-  static SATClauseIterator filterPureLiterals(unsigned varCnt, SATClauseIterator clauses);
-  static bool filterPureLiterals(unsigned varCnt, SATClauseList*& clauses);
-
-  static void propagateUnits(SATClauseIterator clauses,
-  	SATClauseIterator& resUnits, SATClauseIterator& resNonUnits);
-
-  /* UNUSED; moreover, not compatible with the convetion that variables start from 1
-  static SATClauseIterator randomizeVariables(unsigned varCnt, SATClauseIterator clauses);
-  static SATClauseIterator reorderVariablesByResolvability(unsigned varCnt, SATClauseIterator clauses);
-  static SATClauseIterator reorderVariablesByConflicts(unsigned varCnt, SATClauseIterator clauses,
-	  unsigned* conflictCnts);
-	  */
   static SATClause* removeDuplicateLiterals(SATClause* cl);
-  static SATClauseIterator removeDuplicateLiterals(SATClauseIterator clauses);
-
-  static SATClauseIterator generate(unsigned literalsPerClause,
-	  unsigned varCnt, float clausesPerVariable);
-
-protected:
-  static void createVarProfile(unsigned var, DArray<unsigned>& profile, DArray<SATClauseList*>& clsByVar,
-      Set<unsigned>& fixed);
-  static SATClauseIterator permutateVariables(unsigned varCnt, SATClauseIterator clauses,
-	  unsigned* permutation);
 };
 
 };
