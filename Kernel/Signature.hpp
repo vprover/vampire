17458859b5af1d5e0ddcf855992338e32c70c203
Ahmed Bhayat <ahmed_bhayat@hotmail.com>
2022-03-23 22:47:42 +0000

    changing the name of the arrow constructor to make proof output TPTP compatible

--- Kernel/Signature.hpp
+++ Kernel/Signature.hpp
@@ -714,7 +714,7 @@ class Signature
 
   unsigned getArrowConstructor(){
     bool added = false;
-    unsigned arrow = addTypeCon(">",2, added);
+    unsigned arrow = addTypeCon("fun",2, added);
     if(added){
       _arrowCon = arrow;
       TermList ss = AtomicSort::superSort();
