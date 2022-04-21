be0f57f3fb7083cecc46df1533d5496cf41aaa3f
Michael Rawson <michael@rawsons.uk>
2022-04-21 13:14:40 +0100

    remove UNIX_USE_SIGALRM, improve signal safety

--- Lib/Timer.hpp
+++ Lib/Timer.hpp
@@ -16,33 +16,13 @@
 #ifndef __Timer__
 #define __Timer__
 
-#include <iostream>
-
+#include <atomic>
 #include "Debug/Assertion.hpp"
-#include "Forwards.hpp"               // to declare checked_delete a fried for ScopedPtr's destruction to work
 #include "Allocator.hpp"
 #include "VString.hpp"
 
-#ifndef UNIX_USE_SIGALRM
-//SIGALRM causes some problems with debugging
-//[one problem might have been removed, so it's worth checking if the demand for UNIX_USE_SIGALRM in VDEBUG arises]
-#define UNIX_USE_SIGALRM 1 // MS: only the UNIX_USE_SIGALRM seems to be working currently (experiment with SMTCOMP mode); the problem with debugging under UNIX_USE_SIGALRM might have really gone, so let's try
-#endif
-
-//we don't need SIGALRM in Api, and it causes problems debugging
-#ifdef VAPI_LIBRARY
-#if VAPI_LIBRARY
-
-#undef UNIX_USE_SIGALRM
-#define UNIX_USE_SIGALRM 0
-
-#endif
-#endif
-
 namespace Lib
 {
-
-using namespace std;
 
 /**
  * Class implementing timers.
@@ -50,17 +30,10 @@ using namespace std;
  */
 class Timer
 {
-  Timer(bool mustIncludeChildren=false)
-    :
-    _mustIncludeChildren(mustIncludeChildren),
-    _running(false),
-    _elapsed(0)
-  { (void)_mustIncludeChildren; // MS: to get it of the unused private field warning when UNIX_USE_SIGALRM
-    ensureTimerInitialized(); }
-
+  Timer() : _running(false), _elapsed(0) { ensureTimerInitialized(); }
   ~Timer() { deinitializeTimer(); }
   friend void ::checked_delete<Timer>(Timer*);
-  
+ 
 public:
   CLASS_NAME(Timer);
   USE_ALLOCATOR(Timer);
@@ -108,8 +81,6 @@ public:
     return elapsed();
   }
 
-  void makeChildrenIncluded();
-
   static void ensureTimerInitialized();
   static void deinitializeTimer();
   static vstring msToSecondsString(int ms);
@@ -124,12 +95,8 @@ public:
   // (when instruction counting is supported and an instruction limit is set)
   static unsigned elapsedMegaInstructions();
 
-  static bool s_limitEnforcement;
+  static std::atomic<bool> s_limitEnforcement;
 private:
-
-  /** true if the timer must account for the time spent in
-   * children (otherwise it may or may not) */
-  bool _mustIncludeChildren;
   /** true if the timer is running */
   bool _running;
   /** last start time */
@@ -139,7 +106,6 @@ private:
 
   int miliseconds();
 
-#if UNIX_USE_SIGALRM
   static void suspendTimerBeforeFork();
   static void restoreTimerAfterFork();
 
@@ -147,7 +113,6 @@ private:
 
   static long s_ticksPerSec;
   static int s_initGuarantedMiliseconds;
-#endif
 
   /** elapsed time in ticks */
   inline
