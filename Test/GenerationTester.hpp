8d307ae54ad7403d33d8e2ff03f9e6994f2e5c8d
Marton Hajdu <mezpusz@hotmail.com>
2022-04-22 09:30:33 +0200

    Refine unit testing

--- Test/GenerationTester.hpp
+++ Test/GenerationTester.hpp
@@ -56,7 +56,7 @@ public:
     : _rule()
   {}
 
-  virtual bool eq(Kernel::Clause const* lhs, Kernel::Clause const* rhs)
+  virtual bool eq(Kernel::Clause const* lhs, Kernel::Clause const* rhs, BacktrackData& btd)
   { return TestUtils::eqModAC(lhs, rhs); }
 
   friend class TestCase;
@@ -137,7 +137,7 @@ public:
     auto& sExp = this->_expected;
     auto  sRes = Stack<Kernel::Clause*>::fromIterator(res.clauses);
 
-    if (!TestUtils::permEq(sExp, sRes, [&](auto exp, auto res) { return exp.matches(simpl, res); })) {
+    if (!TestUtils::permEq(sExp, sRes, [&](auto exp, auto res, BacktrackData& btd) { return exp.matches(simpl, res, btd); })) {
       testFail(sRes, sExp);
     }
 
