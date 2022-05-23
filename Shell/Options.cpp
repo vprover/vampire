555300e44680ebb9af7a13cdf62f68b3c4c9c8bc
Martin Suda <martin.suda@cvut.cz>
2022-05-23 08:32:50 +0200

    even more refactoring and add const_max and const_min

--- Shell/Options.cpp
+++ Shell/Options.cpp
@@ -2207,14 +2207,17 @@ void Options::init()
 
     _symbolPrecedence = ChoiceOptionValue<SymbolPrecedence>("symbol_precedence","sp",SymbolPrecedence::ARITY,
                                                             {"arity","occurrence","reverse_arity","unary_first",
+                                                            "const_max", "const_min",
                                                             "scramble","frequency","unary_frequency",
                                                             "reverse_frequency", "weighted_frequency","reverse_weighted_frequency"});
     _symbolPrecedence.description="Vampire uses term orderings which require a precedence relation between symbols.\n"
-                                  "Arity orders symbols by their arity (and reverse_arity takes the reverse of this) and occurence orders symbols by the order they appear in the problem.\n"
-                                  "Then we have a few precedence generating schemes adopted from E: frequency - sort by occurence frequency making rare symbols large, reverse does the opposite,\n"
-                                  "(For the weighted versions, each symbol occurence counts as many time as is the lenght of the clause in which it occurs.)\n"
-                                  "unary_first is like arity, except that unary symbols are maximal (and ties are broken by frequency),"
-                                  "unary_frequency is like frequency, except that unary symbols are maximal.";
+                                  "Arity orders symbols by their arity (and reverse_arity takes the reverse of this) and occurence orders symbols by the order they appear in the problem. "
+                                  "Then we have a few precedence generating schemes adopted from E: frequency - sort by frequency making rare symbols large, reverse does the opposite, "
+                                  "(For the weighted versions, each symbol occurence counts as many times as is the lenght of the clause in which it occurs.) "
+                                  "unary_first is like arity, except that unary symbols are maximal (and ties are broken by frequency), "
+                                  "unary_frequency is like frequency, except that unary symbols are maximal, "
+                                  "const_max makes constants the largest, then falls back to arity, "
+                                  "const_min makes constants the smallest, then falls back to reverse_arity.";
     _lookup.insert(&_symbolPrecedence);
     _symbolPrecedence.onlyUsefulWith(InferencingSaturationAlgorithm());
     _symbolPrecedence.tag(OptionTag::SATURATION);
