829c6337802b159385ad05f0b1d36a49337b95c6
Martin Suda <sudam@cs.man.ac.uk>
2022-05-27 19:59:44 +0200

    made also work for non-ground (and maybe even polymorphic)

--- Shell/Twee.cpp
+++ Shell/Twee.cpp
@@ -46,10 +46,6 @@ using Kernel::UnitList;
  # - already encountered subterms reuse older definitions
  */
 class Definizator : public BottomUpTermTransformer {
-
-  bool _groundOnly;
-  Renaming _renaming;
-
   public: // so that Twee::apply can directly access
     // all the new definitions (as clauses) introduced along the way
     UnitList* newUnits;
@@ -59,55 +55,127 @@ class Definizator : public BottomUpTermTransformer {
     UnitList* premises;
 
     // for each relevant term, cache the introduced symbol and the corresponding definition
-    DHMap<Term*,std::pair<unsigned,Unit*>> _cache;  
+    DHMap<Term*,std::pair<unsigned,Clause*>> _cache;
 
-    Definizator(bool groundOnly) : _groundOnly(groundOnly), newUnits(UnitList::empty()) {}
+    Definizator(bool groundOnly) : newUnits(UnitList::empty()), _groundOnly(groundOnly) {}
+  private:
+    bool _groundOnly;
+
+    unsigned _typeArity;
+    TermStack _allVars; // including typeVars, which will come first, then termVars
+    TermStack _termVarSorts;
+
+    // a helper function to collect terms variables and their sorts
+    // all stored in the above private fields to be looked up by transformSubterm
+    void scanVars(Term* t) {
+      CALL("Definizator::scanVars");
+
+      static DHSet<unsigned> varSeen;
+      varSeen.reset();
+      _typeArity = 0;
+      _allVars.reset();
+      static TermStack termVars;
+      termVars.reset();
+      _termVarSorts.reset();
+
+      // fake scanVars cheaply
+      if (t->ground()) return;
+
+      VariableWithSortIterator it(t);
+      while (it.hasNext()) {
+        auto varAndSort = it.next();
+        unsigned v = varAndSort.first.var();
+        TermList s = varAndSort.second;
+        if (varSeen.insert(v)) {
+          if(s == AtomicSort::superSort()) {
+            _allVars.push(TermList(v,false));
+          } else {
+            termVars.push(TermList(v,false));
+            _termVarSorts.push(varAndSort.second);
+          }
+        }
+      }
+      _typeArity = _allVars.size(); // allVars only collected typeVars until now
+      for(unsigned i = 0; i < termVars.size(); i++){
+        _allVars.push(termVars[i]);
+      }
+
+      ASS_EQ(_typeArity+_termVarSorts.size(), _allVars.size())
+    }
 
   protected:
     TermList transformSubterm(TermList trm) override {
-        cout << "tf: " << trm.toString() << endl;
-        if (trm.isVar()) return trm;
-        Term* t = trm.term();
-        if (t->isSort() || t->arity() == 0 || (!t->ground() && _groundOnly)) return trm;
+      CALL("Definizator::transformSubterm");
 
-        if (!t->ground()) {
-          // as we go bottom up, t is never too big (well, it could be wide, but at least not deep)          
-          _renaming.reset();
-          _renaming.normalizeVariables(t);
-          Term* key = _renaming.apply(t);
+      // cout << "tf: " << trm.toString() << endl;
+      if (trm.isVar()) return trm;
+      Term* t = trm.term();
+      if (t->isSort() || t->arity() == 0 || (!t->ground() && _groundOnly)) return trm;
 
-          // TODO: care about sorts?
-        }
-        
-        std::pair<unsigned,Unit*> symAndDef; 
-        TermList res;
-        if (!_cache.find(t,symAndDef)) {
-          TermList sort = SortHelper::getResultSort(t);
-          OperatorType *type = OperatorType::getConstantsType(sort);
-          symAndDef.first = env.signature->addFreshFunction(0, "sF");
-          env.signature->getFunction(symAndDef.first)->setType(type);
-          TermList constant = TermList(Term::createConstant(symAndDef.first));
-          Literal* equation = Literal::createEquality(true, TermList(t), constant, sort);
-          Inference inference(NonspecificInference0(UnitInputType::AXIOM,InferenceRule::FUNCTION_DEFINITION));      
-          Clause *clause = new (1) Clause(1, inference);
-          clause->literals()[0] = equation;
+      Term* key = t;
+      if (!t->ground()) {
+        // as we go bottom up, t is never too big (well, it could be wide, but at least not deep)
+        static Renaming rnm;
+        rnm.reset();
+        rnm.normalizeVariables(t);
+        key = rnm.apply(t);
+      }
 
-          // record globally
-          UnitList::push(clause,newUnits);          
-          if(env.options->showPreprocessing()) {
-              env.out() << "[PP] twee: " << clause->toString() << std::endl;
-          }
+      std::pair<unsigned,Clause*> symAndDef;
+      TermList res;
+      if (!_cache.find(key,symAndDef)) {
+        TermList outSort = SortHelper::getResultSort(t);
+        unsigned newSym;
+        Clause* newDef;
+        scanVars(t);
 
-          symAndDef.second = clause;
-          _cache.insert(t,symAndDef);
-          res = constant;
+        // will the definition folding decrease term weight?
+        // (whether we will also demodulate in this direction may depend on the ordering, but with a constant-weight KBO we will)
+        if (t->weight() > _allVars.size()+1) {
+          // this is always true in the ground case (where t->weight()>=2 and _allVars.size() == 0)
+
+          newSym = env.signature->addFreshFunction(_allVars.size(), "sF");
+          OperatorType* type = OperatorType::getFunctionType(_termVarSorts.size(),_termVarSorts.begin(),outSort,_typeArity);
+          env.signature->getFunction(newSym)->setType(type);
+
+          // res is used both to replace here, but also in the new definition
+          res = TermList(Term::create(newSym,_allVars.size(),_allVars.begin()));
+
+          // (we don't care the definition is not rectified, as long as it's correct)
+          // it is correct, because the lhs below is t and not key
+          Literal* equation = Literal::createEquality(true, TermList(t), res, outSort);
+          Inference inference(NonspecificInference0(UnitInputType::AXIOM,InferenceRule::FUNCTION_DEFINITION));
+          newDef = new (1) Clause(1, inference);
+          newDef->literals()[0] = equation;
         } else {
-          res = TermList(Term::createConstant(symAndDef.first));          
+          // linear term, don't replace (and remember it in cache)
+          symAndDef.first = 0;
+          symAndDef.second = nullptr;
+          _cache.insert(key,symAndDef);
+          return trm;
         }
-        // record as a new premise
-        UnitList::push(symAndDef.second,premises);
-        cout << "r: " << res.toString() << endl;
-        return res;
+
+        // record globally
+        UnitList::push(newDef,newUnits);
+        if(env.options->showPreprocessing()) {
+          env.out() << "[PP] twee: " << newDef->toString() << std::endl;
+        }
+        symAndDef.first = newSym;
+        symAndDef.second = newDef;
+        _cache.insert(key,symAndDef);
+      } else {
+        if (symAndDef.second == nullptr) {
+          // we recalled this term is not worth replacing
+          return trm;
+        }
+
+        scanVars(t);
+        res = TermList(Term::create(symAndDef.first,_allVars.size(),_allVars.begin()));
+      }
+      // record as a new premise
+      UnitList::push(symAndDef.second,premises);
+      // cout << "r: " << res.toString() << endl;
+      return res;
     }
 };
 
@@ -132,9 +200,9 @@ void Shell::Twee::apply(Problem &prb, bool groundOnly)
     newLits.reset();
     for (unsigned i = 0; i < c->size(); i++) {
       Literal* l = c->literals()[i];
-      cout << "L: " << l->toString() << endl;
+      // cout << "L: " << l->toString() << endl;
       Literal* nl = df.transform(l);
-      cout << "NL: " << nl->toString() << endl;
+      // cout << "NL: " << nl->toString() << endl;
       newLits.push(nl);
     }
     if (df.premises) {
