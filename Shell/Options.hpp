31caf5ddf939c5f173b07da6016ffb9d9640cafe
Martin Suda <sudam@cs.man.ac.uk>
2022-05-30 17:48:01 +0200

    add one more value for avatar_nonsplittable_components, it's being implicelty enforced for anc!=off and for sas=z3, so let's have it explicit

--- Shell/Options.hpp
+++ Shell/Options.hpp
@@ -611,8 +611,9 @@ public:
   enum class SplittingNonsplittableComponents : unsigned int {
     ALL = 0,
     ALL_DEPENDENT = 1,
-    KNOWN = 2,
-    NONE = 3
+    GROUND_UNITS = 2,
+    KNOWN = 3,
+    NONE = 4
   };
 
   enum class CCUnsatCores : unsigned int {
