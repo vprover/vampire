7f8d05db3fd2bb5a7a3b79f18d61991dde42c397
Martin Suda <sudam@cs.man.ac.uk>
2022-07-10 13:28:09 +0200

    clean getExtraSchedules for shuffling

--- CASC/PortfolioMode.hpp
+++ CASC/PortfolioMode.hpp
@@ -48,7 +48,7 @@ private:
   bool searchForProof();
   bool performStrategy(Shell::Property* property);
   void getSchedules(Property& prop, Schedule& quick, Schedule& fallback);
-  void getExtraSchedules(Property& prop, Schedule& old, Schedule& extra, bool add_extra, int time_multiplier); 
+  void getExtraSchedules(Property& prop, Schedule& old, Schedule& extra, bool add_extra, float time_multiplier); 
   bool runSchedule(Shell::Property *property, Schedule schedule);
   bool runScheduleAndRecoverProof(Shell::Property *property, Schedule schedule);
   [[noreturn]] void runSlice(vstring sliceCode, int remainingTime);
