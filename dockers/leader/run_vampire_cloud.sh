cdbca5f02f58294c652366e041c7b5bb6320c539
Giles Reger <giles.reger@manchester.ac.uk>
2022-07-21 08:18:24 -0700

    Fixing cloud solver

--- dockers/leader/run_vampire_cloud.sh
+++ dockers/leader/run_vampire_cloud.sh
@@ -10,26 +10,22 @@ log "Running as master node in cloud setup"
 
 ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 
-echo "$ip" | cat > /tmp/pp
 WORKERS=$1
+PROBLEM=$2
+echo "$PROBLEM" | cat > /tmp/pp
 while read worker 
 do
-  echo "Sending $worker the main IP address $ip"
-  scp /tmp/pp $worker:/tmp
+  echo "Sending $worker the problem path" 
+  until scp /tmp/pp $worker:/tmp
+  do
+    echo "Trying to send path to $worker"
+  done
 done < $WORKERS 
 
-mkdir /tmp/status
-echo "dummy" | cat > /tmp/status/dummy
-while true
-do
-  echo "Check if workers ready"
-  ls /tmp/status
-  sleep 2
-done
+OUT=$(echo $PROBLEM | rev | cut -d"/" -f2- | rev)
+sleep 2
+ls $OUT
 
-PROBLEM=$2
-
-mkdir /tmp/results
 availablecores=$(nproc)
 log "master details -> $ip:$availablecores"
 
@@ -39,16 +35,14 @@ log "main IP: $ip"
 # In this setup we don't need to know who the children are
 
 # Run vampire in the background whilst checking if we have a result
-echo "For now turn off vampire on master"
-#/competition/vampire --mode smtcomp --ignore_missing on --bad_option off --cores 0 $PROBLEM &> /tmp/results/result_${ip} & 
+/competition/vampire --mode smtcomp --ignore_missing on --bad_option off --cores 0 $PROBLEM &> $OUT/result_${ip} & 
 
 # We should be terminated after 20 minutes but let's count down from 30 minutes just in case 
 counter=1500
 while [ $counter -gt 0 ]; do
-    for f in /tmp/results/*
+    for f in $OUT/result_*
     do
-      # In reality, a file should only exist in /tmp/results if there is a solution
-      # but let's check if it contains a solution
+      # Let's check if it contains a solution
       if grep -q "sat" $f; then
         cat < $f
         exit 0
--- dockers/worker/worker_vampire.sh
+++ dockers/worker/worker_vampire.sh
@@ -7,31 +7,30 @@ log () {
 }
 
 echo "Child node starting, waiting for problem path"
+pwd
 
 # get own ip and num cpus
 ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 availablecores=$(nproc)
 
-MAIN_IP=$(jq .ipVer leader_node_status.json)
-
-echo "Try send ready to ${MAIN_IP}"
-
-# Let's tell the leader that we're ready
-echo "I am ${ip} and ready" | cat > /tmp/ready_${ip}
-until scp /tmp/ready_${ip} ${MAIN_IP}:/tmp/status
-do
-  echo "Trying to send ready"
-done 
+#MAIN_IP=$(jq .nodeIp competition/leader_node_status.json)
+#echo "Try send ready to ${MAIN_IP}"
 
 # We assume that leader is going to create a file called pp containing the problem path
 PROBLEM="none"
 ls /tmp
 while [ ! -f /tmp/pp ] 
 do
-  PROBLEM=$(head -n 1 /tmp/pp)
-  echo "problem path $PROBLEM found"
+  sleep 1
 done
 
+PROBLEM=$(head -n 1 /tmp/pp)
+echo "problem path $PROBLEM found"
+
+
+OUT=$(head -1 /tmp/pp | rev | cut -d"/" -f2- | rev)
+# Let's tell the leader that we're ready
+echo "I am ${ip} and ready" | cat > $OUT/ready_${ip}
 
 log "I am a child node -> $ip:$availablecores, reporting to the master node -> ${MAIN_IP}, solving $PROBLEM"
 
@@ -43,15 +42,16 @@ IFS=. read -r a b c d <<< "$ip"
 rand="$((a * 256 ** 3 + b * 256 ** 2 + c * 256 + d))"
 echo "Using random number $rand"
 
-vampire --mode smtcomp --ignore_missing on --bad_option off --cores 0 -si on -rs on --random_seed $rand $PROBLEM > /tmp/result_${ip}
+competition/vampire --mode smtcomp --ignore_missing on --bad_option off --cores 0 -si on -rs on --random_seed $rand $PROBLEM > /tmp/result_${ip}
 
 cat < /tmp/result_${ip}
 
 echo "Sending result, should only get here if have a solution as run_vampire_worker should not terminate otherwise" 
-until scp /tmp/result_${ip} ${MAIN_IP}:/tmp/results
-do
-    echo "Sleeping 5 seconds and trying again"
-done
+cp /tmp/result_${ip} $OUT
+ls $OUT
+
+rm /tmp/pp
+rm /tmp/result*
 
 log "done! goodbye"
 ps -ef | grep sshd
