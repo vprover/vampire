8d5be03598dca83a0bebea58d0d32a23267c2514
Ahmed Bhayat <ahmed_bhayat@hotmail.com>
2022-08-08 13:11:11 +0100

    fixing assertion

--- Shell/Skolem.cpp
+++ Shell/Skolem.cpp
@@ -572,7 +572,7 @@ Formula* Skolem::skolemise (Formula* f)
         }
 
 #if VDEBUG
-        ASS(first_pass || sym == last_sym+1);
+        ASS(!name_reuse || first_pass || sym == last_sym+1);
         last_sym = sym;
 #endif
         // in case we are reusing and there is more than one f->vars() in the block
