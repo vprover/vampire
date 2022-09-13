999b570f12dd5f465f39376120e2521c09e142d1
Michael Rawson <michael@rawsons.uk>
2022-09-13 18:23:19 +0100

    generating inferences

---
+++ Inferences/Subterm.hpp
@@ -0,0 +1,31 @@
+/*
+ * This file is part of the source code of the software program
+ * Vampire. It is protected by applicable
+ * copyright laws.
+ *
+ * This source code is distributed under the licence found here
+ * https://vprover.github.io/license.html
+ * and in the source directory
+ */
+/**
+ * @file Subterm.hpp
+ * Inferences about a subterm relation
+ */
+
+#include "InferenceEngine.hpp"
+
+namespace Inferences {
+
+class SubtermGIE : public GeneratingInferenceEngine
+{
+public:
+  CLASS_NAME(SubtermGIE);
+  USE_ALLOCATOR(SubtermGIE);
+
+  void attach(SaturationAlgorithm* salg);
+  void detach();
+
+  ClauseIterator generateClauses(Clause* premise);
+};
+
+}
--- Saturation/SaturationAlgorithm.cpp
+++ Saturation/SaturationAlgorithm.cpp
@@ -68,6 +68,7 @@
 #include "Inferences/GlobalSubsumption.hpp"
 #include "Inferences/InnerRewriting.hpp"
 #include "Inferences/TermAlgebraReasoning.hpp"
+#include "Inferences/Subterm.hpp"
 #include "Inferences/SLQueryBackwardSubsumption.hpp"
 #include "Inferences/Superposition.hpp"
 #include "Inferences/ArgCong.hpp"
@@ -1572,6 +1573,7 @@ SaturationAlgorithm* SaturationAlgorithm::createFromOptions(Problem& prb, const 
     if (opt.termAlgebraInferences()) {
       gie->addFront(new InjectivityGIE());
     }
+    gie->addFront(new SubtermGIE());
   }
 
   CompositeSGI* sgi = new CompositeSGI();
