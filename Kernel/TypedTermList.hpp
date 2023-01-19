5c0a769dade661cda9d18f4471f2ac5c43061896
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2023-01-19 09:54:34 +0100

    made TypedTermList its own file

---
+++ Kernel/TypedTermList.hpp
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the source code of the software program
+ * Vampire. It is protected by applicable
+ * copyright laws.
+ *
+ * This source code is distributed under the licence found here
+ * https://vprover.github.io/license.html
+ * and in the source directory
+ */
+
+#ifndef __Kernel_TypedTermList__
+#define __Kernel_TypedTermList__
+
+#include "Kernel/SortHelper.hpp"
+
+namespace Kernel {
+using SortId = Kernel::TermList;
+
+// TODO move to other class
+class TypedTermList : public TermList
+{
+  SortId _sort;
+public:
+  CLASS_NAME(TypedTermList)
+  // // TODO get rid of default constructor
+  // TypedTermList() : TermList(), _sort() {}
+  TypedTermList(TermList t, SortId sort) : TermList(t), _sort(sort) { ASS_NEQ(sort, AtomicSort::superSort()) }
+  TypedTermList(Term* t) : TypedTermList(TermList(t), SortHelper::getResultSort(t)) {}
+  SortId sort() const { return _sort; }
+  friend std::ostream& operator<<(std::ostream& out, TypedTermList const& self) 
+  { return out << (TermList const&) self << ": " << self._sort; }
+};
+
+} // namespace Kernel 
+
+
+#endif // __Kernel_TypedTermList__
