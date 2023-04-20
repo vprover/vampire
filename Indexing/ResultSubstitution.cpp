e979d15cbde9c165cb4177dbb32eac263a794f93
Johannes Schoisswohl <johannes.schoisswohl@myotis.at>
2023-04-20 15:31:25 +0200

    starting to untangle Result and Gen/Inst substitutions

--- Indexing/ResultSubstitution.cpp
+++ Indexing/ResultSubstitution.cpp
@@ -119,41 +119,42 @@ bool InstSubstitution::isRenamingOnQuery(TermList t)
   }
   return true;
 }
-/**
- * Test whether this substitution object is a renaming on the variables of @param t
- * @param result indicates whether we mean the "bank" applied 
- * to a result term or a query one (cf. applyToQuery/applyToResult)
- */
-bool ResultSubstitution::isRenamingOn(TermList t, bool result) 
-{
-  CALL("ResultSubstitution::isRenamingOn");
 
-  DHMap<TermList,TermList> renamingInMaking;
-
-  VariableIterator it(t);
-  while(it.hasNext()) {
-    TermList v = it.next();
-    ASS(v.isVar());
-
-    TermList vSubst;
-    if (result) {
-      ASS(isIdentityOnQueryWhenResultBound());
-      // code trees don't implement general apply, but satisfy the assertion which makes the following OK
-      vSubst = applyToBoundResult(v);
-    } else {
-      ASS(isIdentityOnResultWhenQueryBound());
-      // the above holds, for a change, for the used substitution trees
-      vSubst = applyToBoundQuery(v);
-    }
-    if (!vSubst.isVar()) {
-      return false;
-    }
-    TermList vStored;
-    if (!renamingInMaking.findOrInsert(v,vStored,vSubst) && vStored != vSubst) {
-      return false;
-    }
-  }
-  return true;
-}
+// /**
+//  * Test whether this substitution object is a renaming on the variables of @param t
+//  * @param result indicates whether we mean the "bank" applied 
+//  * to a result term or a query one (cf. applyToQuery/applyToResult)
+//  */
+// bool ResultSubstitution::isRenamingOn(TermList t, bool result) 
+// {
+//   CALL("ResultSubstitution::isRenamingOn");
+//
+//   DHMap<TermList,TermList> renamingInMaking;
+//
+//   VariableIterator it(t);
+//   while(it.hasNext()) {
+//     TermList v = it.next();
+//     ASS(v.isVar());
+//
+//     TermList vSubst;
+//     if (result) {
+//       ASS(isIdentityOnQueryWhenResultBound());
+//       // code trees don't implement general apply, but satisfy the assertion which makes the following OK
+//       vSubst = applyToBoundResult(v);
+//     } else {
+//       ASS(isIdentityOnResultWhenQueryBound());
+//       // the above holds, for a change, for the used substitution trees
+//       vSubst = applyToBoundQuery(v);
+//     }
+//     if (!vSubst.isVar()) {
+//       return false;
+//     }
+//     TermList vStored;
+//     if (!renamingInMaking.findOrInsert(v,vStored,vSubst) && vStored != vSubst) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
 } // namespace Indexing
