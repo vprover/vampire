e92e6405e53493de7d2a754b112e5b6b1b16ab0c
Michael Rawson <michael@rawsons.uk>
2023-05-17 16:13:09 +0100

    add comments to Recycled<T>

--- Lib/Recycled.hpp
+++ Lib/Recycled.hpp
@@ -28,11 +28,28 @@ struct DefaultReset
 struct NoReset 
 { template<class T> void operator()(T& t) {  } };
 
-/** A smart that lets you keep memory allocated, and reuse it.
+/** A smart pointer that lets you keep memory allocated, and reuse it.
  * Constructs an object of type T on the heap. When the Recycled<T> is destroyed,
- * the object is not returned, but the object is reset using Reset::operator(), 
+ * the object is not returned, but the object is reset using Reset::operator(),
  * and returned to a pool of pre-allocated objects. When the next Recycled<T> is
  * constructed an object from the pool is returned instead of allocating heap memory.
+ *
+ * This may be useful if profiling suggests it, but in general you should not need this.
+ * If you think you do, there are the following tradeoffs:
+ * - more construction overhead than both automatic and (slightly) static storage
+ * - extra indirection: Recycled<T> internally holds a pointer to T
+ * - shares allocations of type T between all instances of Recycled<T>
+ * - safer than static storage due to the Reset mechanism
+ * - Recycled<T> is entrant, whereas static is not
+ * - "leaks" memory, retaining the peak number of concurrently-live T objects
+ * - implements move semantics where T may not
+ * - "destruction" often very cheap because T is never destroyed
+ *
+ * To summarise:
+ * 1. Use automatic storage unless you really have to do something else.
+ * 2. If you have to, use Recycled<T> by preference to static for safety reasons.
+ * 3. If you really must and you are sure of correctness (reentrancy, reset logic),
+ *    use static storage.
  */
 template<class T, class Reset = DefaultReset>
 class Recycled
@@ -44,7 +61,7 @@ class Recycled
     static Stack<unique_ptr<T>> mem;
     return mem;
   }
-public: 
+public:
 
   Recycled()
     : _ptr(mem().isNonEmpty() ? mem().pop() : make_unique<T>()) 
@@ -59,7 +76,7 @@ public:
 
 
   ~Recycled()
-  { 
+  {
     if (_ptr) {
       _reset(*_ptr);
       mem().push(std::move(_ptr));
