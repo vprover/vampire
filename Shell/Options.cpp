106a71ac0067e692d310c3b85d478bce0d86037c
Martin Suda <martin.suda@cvut.cz>
2023-06-11 18:13:30 +0000

    apa and cbe potentially meaningful together (although known to be suboptimal)

--- Shell/Options.cpp
+++ Shell/Options.cpp
@@ -1808,7 +1808,6 @@ void Options::init()
     _addProxyAxioms = BoolOptionValue("add_proxy_axioms","apa",false);
     _addProxyAxioms.description="Add logical proxy axioms";
     _lookup.insert(&_addProxyAxioms);
-    _addProxyAxioms.onlyUsefulWith(_complexBooleanReasoning.is(equal(false)));
     _addProxyAxioms.addProblemConstraint(hasHigherOrder());    
     _addProxyAxioms.tag(OptionTag::HIGHER_ORDER);
 
@@ -1896,7 +1895,8 @@ void Options::init()
     _complexBooleanReasoning = BoolOptionValue("complex_bool_reasoning","cbe",true);
     _complexBooleanReasoning.description=
     "Switches on primitive instantiation and elimination of leibniz equality";
-    // _complexBooleanReasoning.addConstraint(If(equal(true)).then(_addProxyAxioms.is(equal(false)))); // see _addProxyAxioms.onlyUsefulWith
+    // add this back to warn users about a suboptimal conf; but actually the two options do something together
+    // _complexBooleanReasoning.addConstraint(If(equal(true)).then(_addProxyAxioms.is(equal(false))));
     _lookup.insert(&_complexBooleanReasoning);
     _complexBooleanReasoning.addProblemConstraint(hasHigherOrder());
     _complexBooleanReasoning.tag(OptionTag::HIGHER_ORDER);
