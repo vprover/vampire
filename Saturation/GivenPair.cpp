312aa61045d8cc148b923281b4c6edadbcc228e4
Michael Rawson <michael@rawsons.uk>
2023-09-26 16:33:55 +0100

    clauses given by input, backward simplification

--- Saturation/GivenPair.cpp
+++ Saturation/GivenPair.cpp
@@ -14,10 +14,12 @@
 
 namespace Saturation {
 MainLoopResult GivenPairAlgorithm::runImpl() {
-  while(!by_age.isEmpty()) {
-    auto given = by_age.pop();
-    Clause *left = given.first;
-    Clause *right = given.second;
+  while(true) {
+    unsigned left_index, right_index;
+    std::cin >> left_index >> right_index;
+
+    Clause *left = by_number.get(left_index);
+    Clause *right = by_number.get(right_index);
 
     _generating_container.add(left);
     // TODO check whether clause is indicated redundant
@@ -25,60 +27,85 @@ MainLoopResult GivenPairAlgorithm::runImpl() {
     while(left_children.hasNext())
       addNewClause(left_children.next());
     _generating_container.remove(left);
-
-    _generating_container.add(right);
-    // TODO check whether clause is indicated redundant
-    auto right_children = _generator->generateSimplify(left).clauses;
-    while(right_children.hasNext())
-      addNewClause(right_children.next());
-    _generating_container.remove(right);
   }
-  return Statistics::TerminationReason::SATISFIABLE;
 }
 
 void GivenPairAlgorithm::addNewClause(Clause *cl) {
 start:
+  if(env.options->showNew())
+    std::cout << "[SA] new: " << cl->toString() << std::endl;
+
   if(isRefutation(cl))
     throw RefutationFoundException(cl);
 
   // immediate simplification
   while(true) {
     Clause *simplified = _immediateSimplifier->simplify(cl);
-    if(!simplified)
+    if(!simplified) {
       // TODO ref counts
+      if(env.options->showReductions())
+        std::cout << "[SA] deleted: " << cl->toString() << std::endl;
       return;
+    }
+
     if(simplified == cl)
       break;
+
+    if(env.options->showReductions())
+      std::cout << "[SA] replaced: " << cl->toString() << " with " << simplified->toString() << std::endl;
     cl = simplified;
   }
 
   // "forward" simplification from active set
   FwSimplList::Iterator fsit(_fwSimplifiers);
   while(fsit.hasNext()) {
-    ForwardSimplificationEngine* fse=fsit.next();
+    ForwardSimplificationEngine *fse=fsit.next();
     Clause *simplified = nullptr;
     ClauseIterator premises;
     if(fse->perform(cl, simplified, premises)) {
-      if(!simplified)
+      if(!simplified) {
+        if(env.options->showReductions())
+          std::cout << "[SA] deleted: " << cl->toString() << std::endl;
         return;
+      }
+
+      if(env.options->showReductions())
+        std::cout << "[SA] replaced: " << cl->toString() << " with " << simplified->toString() << std::endl;
       cl = simplified;
       goto start;
     }
   }
 
-  //TODO "backward" simplification
+  // "backward" simplification into the active set
+  BwSimplList::Iterator bsit(_bwSimplifiers);
+  while (bsit.hasNext()) {
+    BackwardSimplificationEngine *bse=bsit.next();
+    BwSimplificationRecordIterator simplifications;
+    bse->perform(cl, simplifications);
+    while(simplifications.hasNext()) {
+      BwSimplificationRecord record = simplifications.next();
+      Clause *redundant = record.toRemove;
+      Clause *replacement = record.replacement;
+
+      _active->remove(redundant);
+      if(!replacement) {
+        if(env.options->showReductions())
+          std::cout << "[SA] deleted: " << redundant->toString() << std::endl;
+        continue;
+      }
+
+      if(env.options->showReductions())
+        std::cout << "[SA] replaced: " << redundant->toString() << " with " << replacement->toString() << std::endl;
+      addNewClause(replacement);
+    }
+  }
 
   if(env.options->showActive())
     std::cout << "[SA] active: " << cl->toString() << std::endl;
-
   _selector->select(cl);
   cl->setStore(Clause::ACTIVE);
   env.statistics->activeClauses++;
   _active->add(cl);
-
-  ClauseIterator it = _active->clauses();
-  while(it.hasNext())
-    // TODO ref counts
-    by_age.insert({cl, it.next()});
+  by_number.insert(cl->number(), cl);
 }
 }
--- Saturation/GivenPair.hpp
+++ Saturation/GivenPair.hpp
@@ -25,14 +25,6 @@ class GivenPairAlgorithm: public SaturationAlgorithm {
 public:
   GivenPairAlgorithm(Problem& prb, const Options& opt) : SaturationAlgorithm(prb, opt), _generating_container(opt) {}
 
-  struct ByAge {
-    static Comparison compare(std::pair<Clause *, Clause *> left, std::pair<Clause *, Clause *> right) {
-      unsigned left_age = left.first->age() + left.second->age();
-      unsigned right_age = right.first->age() + right.second->age();
-      return Int::compare(left_age, right_age);
-    }
-  };
-
 protected:
   MainLoopResult runImpl() override;
   void addNewClause(Clause* cl) override;
@@ -43,9 +35,9 @@ protected:
   ClauseContainer* getSimplifyingClauseContainer() override { return _active; }
   PassiveClauseContainer* getPassiveClauseContainer() override { return nullptr; }
 
-  BinaryHeap<std::pair<Clause *, Clause *>, ByAge> by_age;
-
   ActiveClauseContainer _generating_container;
+
+  DHMap<unsigned, Clause *> by_number;
 };
 }
 #endif
