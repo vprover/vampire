ded06ed19305ba07ff59d4007a2fe8d0a1fda7d1
Martin Suda <martin.suda@cvut.cz>
2023-10-10 14:17:21 +0000

    some options should not get printed as part of decode

--- Shell/Options.cpp
+++ Shell/Options.cpp
@@ -3564,6 +3564,13 @@ vstring Options::generateEncodedOptions() const
     forbidden.insert(&_encode);
     forbidden.insert(&_decode);
     forbidden.insert(&_sampleStrategy);
+
+    forbidden.insert(&_memoryLimit);
+    forbidden.insert(&_proof);
+    forbidden.insert(&_inputSyntax);
+#ifdef __linux__
+    forbidden.insert(&_parsingDoesNotCount);
+#endif
     forbidden.insert(&_ignoreMissing); // or maybe we do!
   }
 
