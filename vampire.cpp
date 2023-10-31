ed3a3f688d34c91ded8186bf74dccc8f4e222819
Michael Rawson <michael@rawsons.uk>
2023-10-31 11:56:54 +0000

    remove odd behaviour if we eliminated conjecture

--- vampire.cpp
+++ vampire.cpp
@@ -521,13 +521,6 @@ void clausifyMode(bool theory)
       env.out() << TPTPPrinter::toString(cl) << "\n";
     }
   }
-  if(!printed_conjecture && UIHelper::haveConjecture()){
-    unsigned p = env.signature->addFreshPredicate(0,"p");
-    Clause* c = new(2) Clause(2,NonspecificInference0(UnitInputType::NEGATED_CONJECTURE,InferenceRule::INPUT));
-    (*c)[0] = Literal::create(p,0,true,false,0);
-    (*c)[1] = Literal::create(p,0,false,false,0);
-    env.out() << TPTPPrinter::toString(c) << "\n";
-  }
   env.endOutput();
 
   if (env.options->latexOutput() != "off") { outputClausesToLaTeX(prb.ptr()); }
