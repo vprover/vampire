a5c4d03700aae981b4e9c83b2e190255a1b2ebe9
Michael Rawson <michael@rawsons.uk>
2024-04-02 16:21:19 +0100

    initialise to empty() rather than zero

--- Kernel/Term.hpp
+++ Kernel/Term.hpp
@@ -101,7 +101,8 @@ class TermList {
 public:
   // divide by 4 because of the tag, by 2 to split the space evenly
   static const unsigned SPEC_UPPER_BOUND = (UINT_MAX / 4) / 2;
-  TermList() : _content(0) {};
+  /* default constructor, satisfying isEmpty() */
+  TermList() : _content(FUN) {}
   /** creates a term list containing a pointer to a term */
   explicit TermList(Term* t) : _content(0) {
     // NB we also zero-initialise _content so that the spare bits are zero on 32-bit platforms
@@ -176,9 +177,11 @@ public:
   /** make the term into a special variable with a given number */
   inline void makeSpecialVar(unsigned vnumber)
   { _content = vnumber * 4 + SPEC_VAR; }
-  /** create an term empty (so that isEmpty() returns true) */
+  /** create an term empty (so that isEmpty() returns true)
+   *  (can just be the default constructor now)
+   */
   inline static TermList empty()
-  { TermList out; out._content = FUN; return out; }
+  { return TermList(); }
   /** the top of a term is either a function symbol or a variable id. this class is model this */
   class Top {
     using Inner = Coproduct<unsigned, unsigned>;
