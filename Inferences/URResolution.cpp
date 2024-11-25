c64d3603810562f68b7b610b60967cda2910c31c
Martin Suda <martin.suda@cvut.cz>
2024-11-25 15:33:54 +0100

    the longest premise of URR comes first

--- Inferences/URResolution.cpp
+++ Inferences/URResolution.cpp
@@ -182,7 +182,6 @@ struct URResolution::Item
   Clause* generateClause() const
   {
     UnitList* premLst = 0;
-    UnitList::push(_orig, premLst);
     Literal* single = 0;
     unsigned clen = _lits.size();
     for(unsigned i=0; i<clen; i++) {
@@ -197,6 +196,7 @@ struct URResolution::Item
         UnitList::push(premise, premLst);
       }
     }
+    UnitList::push(_orig, premLst);
 
     Inference inf(GeneratingInferenceMany(InferenceRule::UNIT_RESULTING_RESOLUTION, premLst));
     Clause* res;
