5c42e8aeed341639cc5da5709128c7640160d2da
Marton Hajdu <mezpusz@hotmail.com>
2024-12-06 13:24:42 +0100

    Perfectly share polynomials in OrderingComparator

--- Kernel/OrderingComparator.hpp
+++ Kernel/OrderingComparator.hpp
@@ -55,6 +55,7 @@ protected:
   };
 
   struct Node;
+  struct Polynomial;
 
   struct Branch {
     Node* node() const { return _node; }
@@ -72,6 +73,9 @@ protected:
     template<typename S, typename T> Branch(S&& s, T&& t) {
       setNode(new Node(std::forward<S>(s), std::forward<T>(t)));
     }
+    Branch(const Polynomial* p) {
+      setNode(new Node(p));
+    }
     ~Branch();
     Branch(const Branch& other);
     Branch& operator=(const Branch& other);
@@ -84,8 +88,17 @@ protected:
 
   friend std::ostream& operator<<(std::ostream& out, const Node& node);
   friend std::ostream& operator<<(std::ostream& out, const BranchTag& t);
+  friend std::ostream& operator<<(std::ostream& out, const Polynomial& poly);
 
   using VarCoeffPair = std::pair<unsigned,int>;
+
+  struct Polynomial {
+    static const Polynomial* get(int constant, const Stack<VarCoeffPair>& vcs);
+
+    int constant;
+    Stack<VarCoeffPair> pos;
+    Stack<VarCoeffPair> neg;
+  };
 
   using Trace = TermPartialOrdering;
 
@@ -114,8 +127,8 @@ protected:
       : tag(T_DATA), data(data), alternative(alternative) {}
     explicit Node(TermList lhs, TermList rhs)
       : tag(T_TERM), lhs(lhs), rhs(rhs) {}
-    explicit Node(uint64_t w, Stack<VarCoeffPair>* varCoeffPairs)
-      : tag(T_POLY), w(w), varCoeffPairs(varCoeffPairs) {}
+    explicit Node(const Polynomial* p)
+      : tag(T_POLY), poly(p) {}
     Node(const Node&) = delete;
     Node& operator=(const Node&) = delete;
 
@@ -130,12 +143,11 @@ protected:
     union {
       void* data = nullptr;
       TermList lhs;
-      int64_t w;
+      const Polynomial* poly;
     };
     union {
       Branch alternative;
       TermList rhs;
-      Stack<VarCoeffPair>* varCoeffPairs;
     };
 
     Branch eqBranch;
