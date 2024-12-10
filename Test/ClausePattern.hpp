a1012ff790c7cd168790e097ae3c361bfbef7bd9
joe-hauns <joe-hauns@protonmail.com>
2024-12-10 14:45:09 +0100

    extended GenerationTester functionality and refactored its interface

--- Test/ClausePattern.hpp
+++ Test/ClausePattern.hpp
@@ -34,26 +34,27 @@ struct AnyOf
  * A Clause matches a pattern Clause, if they are equal.
  * A Clause matches an AnyOf pattern if it matches both of the subpatterns.
  */
-class ClausePattern : Coproduct<Kernel::Clause const*, AnyOf>
+class ClausePattern : Coproduct<Kernel::Clause*, AnyOf>
 {
+  using Copro =  Coproduct<Kernel::Clause*, AnyOf>;
 public:
-  ClausePattern(Kernel::Clause const* clause) 
-    : Coproduct<Kernel::Clause const*, AnyOf>(clause) {}
+  ClausePattern(Kernel::Clause* clause) 
+    : Copro(clause) {}
 
-  ClausePattern(ClausePattern l, ClausePattern r) : Coproduct<Kernel::Clause const*, AnyOf>(AnyOf {
+  ClausePattern(ClausePattern l, ClausePattern r) : Copro(AnyOf {
         std::make_unique<ClausePattern>(std::move(l)),
         std::make_unique<ClausePattern>(std::move(r))
       }) {}
 
   template<class EqualityOperator>
-  bool matches(EqualityOperator& equality, Kernel::Clause const* result);
+  bool matches(EqualityOperator& equality, Kernel::Clause* result);
   friend std::ostream& operator<<(std::ostream& out, ClausePattern const& self);
 };
 
 inline std::ostream& operator<<(std::ostream& out, ClausePattern const& self) 
 {
   return self.match(
-      [&](Kernel::Clause const* const& self) -> std::ostream&
+      [&](Kernel::Clause* const& self) -> std::ostream&
       { return out << pretty(self); },
 
       [&](AnyOf const& self)  -> std::ostream&
@@ -61,21 +62,21 @@ inline std::ostream& operator<<(std::ostream& out, ClausePattern const& self)
 }
 
 template<class EqualityOperator>
-bool ClausePattern::matches(EqualityOperator& equality, Kernel::Clause const* result)
+bool ClausePattern::matches(EqualityOperator& equality, Kernel::Clause* result)
 {
   return match(
-      [&](Kernel::Clause const*& self) 
+      [&](Kernel::Clause* self) 
       { return equality.eq(result, self); },
 
       [&](AnyOf& self) 
       { return self.lhs->matches(equality, result) || self.rhs->matches(equality, result); });
 }
 
-inline ClausePattern anyOf(Kernel::Clause const* lhs) 
+inline ClausePattern anyOf(Kernel::Clause* lhs) 
 { return ClausePattern(lhs); }
 
 template<class... As>
-inline ClausePattern anyOf(Kernel::Clause const* lhs, Kernel::Clause const* rhs, As... rest) 
+inline ClausePattern anyOf(Kernel::Clause* lhs, Kernel::Clause* rhs, As... rest) 
 { return ClausePattern(lhs, anyOf(rhs, rest...)); }
 
 
--- UnitTests/tFunctionDefinitionRewriting.cpp
+++ UnitTests/tFunctionDefinitionRewriting.cpp
@@ -22,7 +22,7 @@
 
 using namespace Test;
 
-REGISTER_GEN_TESTER(FunctionDefinitionRewriting)
+REGISTER_GEN_TESTER(Generation::GenerationTester<FunctionDefinitionRewriting>(FunctionDefinitionRewriting()))
 
 namespace FunctionDefinitionRewritingTest {
 
@@ -59,7 +59,7 @@ ClauseStack fnDefContext() {
 }
 
 TEST_GENERATION(test_00,
-    Generation::TestCase()
+    Generation::AsymmetricTest()
       .setup(setup)
       .options({ { "function_definition_rewriting", "on"} })
       .input( clause({  b != f(b, y), p(x) }))
@@ -67,7 +67,7 @@ TEST_GENERATION(test_00,
     )
 
 TEST_GENERATION(test_01,
-    Generation::TestCase()
+    Generation::AsymmetricTest()
       .setup(setup)
       .context(fnDefContext())
       .options({ { "function_definition_rewriting", "on"} })
@@ -78,7 +78,7 @@ TEST_GENERATION(test_01,
     )
 
 TEST_GENERATION(test_02,
-    Generation::TestCase()
+    Generation::AsymmetricTest()
       .setup(setup)
       .context(fnDefContext())
       .options({ { "function_definition_rewriting", "on"} })
@@ -90,7 +90,7 @@ TEST_GENERATION(test_02,
 
 // no rewrites (matching is used instead of unification)
 TEST_GENERATION(test_03,
-    Generation::TestCase()
+    Generation::AsymmetricTest()
       .setup(setup)
       .context(fnDefContext())
       .options({ { "function_definition_rewriting", "on"} })
@@ -100,7 +100,7 @@ TEST_GENERATION(test_03,
 
 // multiple rewritten positions in a literal and multiple rewrite rules
 TEST_GENERATION(test_04,
-    Generation::TestCase()
+    Generation::AsymmetricTest()
       .setup(setup)
       .context(fnDefContext())
       .options({ { "function_definition_rewriting", "on"} })
@@ -114,7 +114,7 @@ TEST_GENERATION(test_04,
 
 // each literal is rewritten in a clause
 TEST_GENERATION(test_05,
-    Generation::TestCase()
+    Generation::AsymmetricTest()
       .setup(setup)
       .context(fnDefContext())
       .options({ { "function_definition_rewriting", "on"} })
@@ -127,7 +127,7 @@ TEST_GENERATION(test_05,
 
 // equational tautologies are discarded
 TEST_GENERATION(test_06,
-    Generation::TestCase()
+    Generation::AsymmetricTest()
       .setup(setup)
       .context(fnDefContext())
       .options({ { "function_definition_rewriting", "on"} })
