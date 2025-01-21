fbbb84ae586a557bd75dc7c6c540b08198b76698
joe-hauns <joe-hauns@protonmail.com>
2025-01-21 15:29:37 +0100

    license header

--- Kernel/QKbo.cpp
+++ Kernel/QKbo.cpp
@@ -1,3 +1,13 @@
+/*
+ * This file is part of the source code of the software program
+ * Vampire. It is protected by applicable
+ * copyright laws.
+ *
+ * This source code is distributed under the licence found here
+ * https://vprover.github.io/license.html
+ * and in the source directory
+ */
+
 #include "QKbo.hpp"
 #include "Term.hpp"
 #include "NumTraits.hpp"
