21c44da4c05be8d66c97149421bf4e69861acb81
joe-hauns <joe-hauns@protonmail.com>
2025-02-26 15:00:57 +0100

    extended Kernel/ALASCA/Term.hpp

--- Kernel/ALASCA/Term.hpp
+++ Kernel/ALASCA/Term.hpp
@@ -16,6 +16,7 @@
 #include "Kernel/TermIterators.hpp"
 #include "Lib/Reflection.hpp"
 #include "Lib/STL.hpp"
+#include "Lib/VirtualIterator.hpp"
 
 #define DEBUG(lvl, ...) if (lvl < 0) { DBG(__VA_ARGS__) }
 
@@ -24,7 +25,7 @@ namespace Kernel {
 
 #define OPS_FROM_TO_TERM(Type) \
   auto asTuple() const { return std::tuple(toTerm()); } \
-  IMPL_EQ_FROM_COMPARE(Type) \
+  IMPL_COMPARISONS_FROM_TUPLE(Type) \
   IMPL_HASH_FROM_TUPLE(Type) \
   friend std::ostream& operator<<(std::ostream& out, Type const& self) { return out << self.toTerm(); } \
 
@@ -44,17 +45,37 @@ namespace Kernel {
     using Numeral = typename NumTraits::ConstantType;
     using ASig = AlascaSignature<NumTraits>;
     Numeral _numeral;
+    // TODO make this an Option<TermList>
     TermList _term;
   public:
+
     AlascaMonom(Numeral numeral, TermList term) 
       : _numeral(std::move(numeral))
-      , _term(std::move(term)) {  }
+      , _term(std::move(term)) 
+    {  
+      ASS(ASig::one() == _term || !ASig::isNumeral(_term))
+    }
+
+    AlascaMonom(Numeral numeral) 
+      : AlascaMonom(numeral, NumTraits::one()) 
+    {  }
+
+    bool isNumeral() const { return ASig::one() == _term; }
+
     AlascaMonom(int numeral, TermList term) : AlascaMonom(Numeral(numeral), term) {}
+
+    friend AlascaMonom operator*(Numeral n, AlascaMonom const& self) { return AlascaMonom(self.numeral() * n, self.atom()); }
+    friend AlascaMonom operator*(int n, AlascaMonom const& t) { return Numeral(-1) * t; }
+    friend AlascaMonom operator/(AlascaMonom const& t, Numeral n) { return n.inverse() * t; }
+    friend AlascaMonom operator/(AlascaMonom const& t, int n) { return *t / Numeral(n); }
+    AlascaMonom operator-() const { return -1 * *this; }
+
+    Numeral const& numeral() const { return _numeral; }
+    TermList const& atom() const { return _term; }
 
     template<class N>
     friend class AlascaTermApplNum;
     TermList toTerm() const { return ASig::linMul(_numeral, _term); }
-
 
     auto asTuple() const { return std::tie(_term, _numeral); }
     IMPL_COMPARISONS_FROM_TUPLE(AlascaMonom)
@@ -69,6 +90,7 @@ namespace Kernel {
   /* T can be either TermList or TermSpec */
   template<class NumTraits>
   class AlascaTermApplNum {
+    // TODO set the normalized cache of this term appropriately, so it doesn't have to be re-normalized
     mutable Option<TermList> _self;
     using Numeral = typename AlascaSignature<NumTraits>::Numeral;
     // TODO small size optimization
@@ -76,6 +98,10 @@ namespace Kernel {
 
     AlascaTermApplNum(Stack<AlascaMonom<NumTraits>> self) : _sum(std::move(self)) {}
   public:
+
+
+    unsigned nSummands() const { return _sum.size(); }
+    auto& monomAt(unsigned i) const { return _sum[i]; }
     static AlascaTermApplNum normalize(Term* t);
     auto iterSummands() const { return arrayIter(_sum); }
     TermList toTerm() const { return _self.unwrapOrInit([&]() -> TermList { return NumTraits::sum(arrayIter(_sum).map([](auto& m) { return m.toTerm(); })); }); }
@@ -109,7 +135,7 @@ namespace Kernel {
     TermList toTerm() const { return _self.apply([](auto& self) { return self.toTerm(); }); }
     OPS_FROM_TO_TERM(AlascaTermAppl);
   };
-
+  
   /* an alasca term of numerals sort NumTraits */
   template<class NumTraits>
   class AlascaTermNum {
@@ -118,17 +144,21 @@ namespace Kernel {
     // the internal representation is either a `Var` a variable together with a sort annotation, or an applcation term `Appl`
     // Appl terms are cached for each Term*, thus they are present as a Pointer (Appl*), while variables cannot be cached, 
     // thus they are plain owned data
-    using Inner = Coproduct<Var, Appl*>;
+    using Inner = Coproduct<Appl*, Var>;
     Inner _self;
 
 
     AlascaTermNum(Inner inner) : _self(std::move(inner)) {}
   public:
+    template<class Iter>
+    static AlascaTermNum fromCorrectlySortedIter(Iter iter);
     using Numeral = typename NumTraits::ConstantType;
 
     // TODO
     friend AlascaTermNum operator*(Numeral n, AlascaTermNum const&);
 
+    friend AlascaTermNum operator/(AlascaTermNum const& t, Numeral n) { return n.inverse() * t; }
+    friend AlascaTermNum operator/(AlascaTermNum const& t, int n) { return t / Numeral(n); }
     friend AlascaTermNum operator*(int n, AlascaTermNum const& t) { return Numeral(-1) * t; }
     AlascaTermNum operator-() const { return -1 * *this; }
 
@@ -158,17 +188,23 @@ namespace Kernel {
         auto x1 = iter.next();
         while (iter.hasNext()) {
           auto x2 = iter.next();
-          ASS(x1.term < x2.term);
+          ASS(x1.atom() < x2.atom());
           x1 = x2;
         }
       }
       for (auto x : iterSummands()) {
-        ASS(x.numeral != 0)
+        ASS(x.numeral() != 0)
       }
     ) }
 
     OPS_FROM_TO_TERM(AlascaTermNum);
   };
+
+  using AlascaTermNumAny  = Coproduct<
+        AlascaTermNum<IntTraits>
+      , AlascaTermNum<RealTraits>
+      , AlascaTermNum<RatTraits>
+      >;
 
   // TODO rename to AlascaTerm
   class AnyAlascaTerm 
@@ -204,8 +240,14 @@ namespace Kernel {
     { return _self.match([&](auto& owned) { return owned.toTerm(); }
                         ,[&](auto& ptr  ) { return ptr->toTerm(); }); }
     
-    template<class NumTraits>
-    Option<AlascaTermNum<NumTraits>> downcast() const;
+    Option<AlascaTermNumAny> asSum() const;
+    
+    Option<AlascaTermNumAny> asNonTrivialSum() const;
+
+    IterTraits<VirtualIterator<AnyAlascaTerm>> iterSubterms() const;
+    
+    template<class NumTraits> Option<AlascaTermNum<NumTraits>> asSum() const;
+    template<class NumTraits> Option<AlascaTermNum<NumTraits>> asNonTrivalSum() const;
 
     OPS_FROM_TO_TERM(AnyAlascaTerm);
   };
@@ -219,28 +261,33 @@ namespace Kernel {
 
   template<class NumTraits>
   AlascaTermApplNum<NumTraits> AlascaTermApplNum<NumTraits>::normalize(Term* t) {
+    using ASig = AlascaSignature<NumTraits>;
 
     using Monom = AlascaMonom<NumTraits>;
     Stack<Monom> done;
     RStack<Monom> todo;
-    todo->push(Monom { .numeral = 1, .term = TermList(t), });
+    todo->push(Monom(1, TermList(t)));
     while (todo->isNonEmpty()) {
       auto next = todo->pop();
 
       optionIfThenElse(
           [&]() { return NumTraits::ifAdd([&](auto l, auto r) {
-            todo->push(Monom { next.numeral, l });
-            todo->push(Monom { next.numeral, r });
+            todo->push(Monom(next.numeral(), l));
+            todo->push(Monom(next.numeral(), r));
             }); },
-          [&]() { return NumTraits::ifNumMul([&](auto k, auto t) {
-            todo->push(Monom { next.numeral * k, t });
+          [&]() { return ASig::ifNumMul([&](auto k, auto t) {
+            todo->push(Monom(next.numeral() * k, t));
+            }); },
+          [&]() { return ASig::ifNumeral([&](auto k) {
+            todo->push(Monom(next.numeral() * k));
             }); },
           [&]() { 
-            if (next.numeral != 0) {
-              auto inner = AnyAlascaTerm::normalize(TypedTermList(next.term, NumTraits::sort()))
-                        .template downcast<NumTraits>();
+            if (next.numeral() != 0) {
+              auto inner = AnyAlascaTerm::normalize(TypedTermList(next.atom(), NumTraits::sort()))
+                        .template asSum<NumTraits>()
+                        .unwrap();
               for (auto m : inner.iterMonoms()) {
-                done->push(Monom { next.numeral, m });
+                done->push(Monom(next.numeral(), m));
               }
             }
           });
@@ -248,17 +295,17 @@ namespace Kernel {
 
     /* summing up */
     if (done.size() != 0) {
-      done.sort([](auto& l, auto& r) { return l.term < r.term; });
+      done.sort([](auto& l, auto& r) { return l.atom() < r.atom(); });
       auto sz = 0;
       auto i = 0;
       while (i < done.size()) {
         ASS_L(sz, i)
-        if (done[sz].term == done[i].term) {
-          done[sz].numeral += done[i].numeral;
+        if (done[sz].atom() == done[i].atom()) {
+          done[sz]._numeral += done[i].numeral();
           i++;
         } else {
-          ASS(done[sz].term < done[i].term)
-          if (done[sz].numeral == 0) {
+          ASS(done[sz].atom() < done[i].atom())
+          if (done[sz].numeral() == 0) {
             done[sz] = done[i];
             i++;
           } else {
@@ -268,7 +315,7 @@ namespace Kernel {
           }
         }
       }
-      if (done[sz].numeral != 0) {
+      if (done[sz].numeral() != 0) {
         sz++;
       }
       done.pop(done.size() - sz);
