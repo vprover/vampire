83f5d525ab60130f719eca74ed24add6605923ff
joe-hauns <joe-hauns@protonmail.com>
2025-03-28 13:04:01 +0100

    lookahead for SubtermFactoring

--- Inferences/ALASCA/SubtermFactoring.cpp
+++ Inferences/ALASCA/SubtermFactoring.cpp
@@ -36,59 +36,61 @@ struct Application
   TermList term1() const { return atomAt(i); }
   TermList term2() const { return atomAt(j); }
 
+  static auto iter(AlascaState& shared, SelectedAtom atom)
+  {
+    return atom.iterSelectedSubterms()
+         .filterMap([](auto t) { return t.asSum(); })
+         .filter([](auto& s) { return s.apply([](auto& s) { return s.nSummands() >= 2; }); })
+         .flatMap([=](auto t_anyNum) {
+             return coproductIter(t_anyNum.applyCo([=](auto t) {
+                 return range(0, t.nSummands() - 1)
+                    .flatMap([=](auto i) {
+                        return range(i + 1, t.nSummands())
+                          .map([=](auto j) {
+                             return Application { ._atom = atom, ._sum = t_anyNum, .i = i, .j = j, };
+                          })
+                          .inspect([](auto& a) { DEBUG(a) })
+                          .filter([](auto& appl) { return appl.term1().isTerm()
+                                                        && appl.term2().isTerm()
+                                                        && appl.term1().term()->functor() == appl.term2().term()->functor()
+                                                        ;  })
+                          .filterMap([&shared](auto appl) {
+                            return shared.unify(appl.term1(), appl.term2())
+                              .map([&](auto unif) {
+                                  return std::make_pair(std::move(appl), std::move(unif));
+                              });
+                          });
+                    });
+             })); 
+         });
+   }
+
+
   static auto iter(AlascaState& shared, Clause* cl)
   {
     return ALASCA::Superposition::Rhs::activePositions(shared, cl)
-      .flatMap([&](auto atom) {
-          // return coproductIter(sel_lit.map(
-          //    [](SelectedSummand& x) { return iterItems(TypedTermList(x.selectedAtom(), x.sort())); },
-          //    [](SelectedUninterpretedEquality& x) {  return iterItems(TypedTermList(x.biggerSide(), x.literal()->eqArgSort())); },
-          //    [](SelectedUninterpretedPredicate& x) { return termArgIterTyped(x.literal()); }
-          // ))
-          return atom.iterSelectedSubterms()
-          // .flatMap([&](AnyAlascaTerm activePos) {
-          //   return AnyAlascaTerm::normalize(activePos).bottomUpIter()
-               .filterMap([](auto t) { return t.asSum(); })
-               .filter([](auto& s) { return s.apply([](auto& s) { return s.nSummands() >= 2; }); })
-               .flatMap([=](auto t_anyNum) {
-                   return coproductIter(t_anyNum.applyCo([=](auto t) {
-                       return range(0, t.nSummands() - 1)
-                          .flatMap([=](auto i) {
-                              return range(i + 1, t.nSummands())
-                                .map([=](auto j) {
-                                   return Application { ._atom = atom, ._sum = t_anyNum, .i = i, .j = j, };
-                                })
-                                .inspect([](auto& a) { DEBUG(a) })
-                                .filter([](auto& appl) { return appl.term1().isTerm()
-                                                              && appl.term2().isTerm()
-                                                              && appl.term1().term()->functor() == appl.term2().term()->functor()
-                                                              ;  })
-                                ;
-                          });
-                   })); 
-               });
-              // }) ;
-      })
-    ;
-  }
+      .flatMap([&](auto atom) { return iter(shared, std::move(atom)); });
+   }
 };
+
+
+VirtualIterator<std::tuple<>> SubtermFactoring::lookaheadResultEstimation(SelectedAtom const& selection) {
+  return pvi(dropElementType(Application::iter(*_shared, selection)));
+}
 
 ClauseIterator SubtermFactoring::generateClauses(Clause* premise)
 {
   return pvi(Application::iter(*_shared, premise)
-    .filterMap([=](auto appl) {
-        return _shared->unify(appl.term1(), appl.term2())
-          .map([&](auto unif) {
-              return Clause::fromIterator(
-                  concatIters(
-                    premise->iterLits()
-                      .map([&](auto l) { return unif.subs().apply(l, 0); }),
-                    arrayIter(unif.computeConstraintLiterals())
-                    ),
-                  // TODO make its own InferenceRule
-                  Inference(GeneratingInference1(Kernel::InferenceRule::ALASCA_TERM_FACTORING, premise))
-                  );
-          });
+    .map([=](auto appl_unif) {
+      auto& unif = appl_unif.second;
+      return Clause::fromIterator(
+          concatIters(
+            premise->iterLits()
+              .map([&](auto l) { return unif.subs().apply(l, 0); }),
+            arrayIter(unif.computeConstraintLiterals())),
+          // TODO make its own InferenceRule
+          Inference(GeneratingInference1(Kernel::InferenceRule::ALASCA_TERM_FACTORING, premise))
+          );
     }));
 }
 
--- Inferences/ALASCA/SubtermFactoring.hpp
+++ Inferences/ALASCA/SubtermFactoring.hpp
@@ -42,9 +42,7 @@ public:
   void attach(SaturationAlgorithm* salg) final override { GeneratingInferenceEngine::attach(salg); }
   void detach() final override { GeneratingInferenceEngine::detach(); }
 
-  /** TODO 2 should we make this a correct estimation */
-  virtual VirtualIterator<std::tuple<>> lookaheadResultEstimation(SelectedAtom const& selection) override
-  { return pvi(dropElementType(range(0,0))); }
+  virtual VirtualIterator<std::tuple<>> lookaheadResultEstimation(SelectedAtom const& selection) override;
 
   ClauseIterator generateClauses(Clause* premise) final override;
 
