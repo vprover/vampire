4de409645b99c82c10c07a8e8334abff73a2c81f
Alex Gabriel <a.gabriel@tudelft.nl>
2025-07-14 11:58:12 +0200

    Update Vampire.hpp
    fixed typo

--- Interface/Vampire.hpp
+++ Interface/Vampire.hpp
@@ -24,7 +24,7 @@ namespace Vampire {
 void init(); // please call this at the start
 bool loadTPTP(std::string tag, std::string theory); // returns true on success; state should remain consistent anyway
 std::vector<std::string> listTheories();
-bool popTeories(unsigned popCnt); // will try to pop popCnt many theories from the theory stack; returns true if there were enough theories on the stack to pop, even in the false case theories will be popped
+bool popTheories(unsigned popCnt); // will try to pop popCnt many theories from the theory stack; returns true if there were enough theories on the stack to pop, even in the false case theories will be popped
 
 bool runProver(std::string commandLine); // will only return true and actually run, if the prover is not running already (or the result of the previous call has not been retrieved yet)
 
