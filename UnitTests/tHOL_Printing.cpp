521c626e1185b4f3752b615b8fd11d980a9b9c57
Matthias Hetzenberger <mhetzenberger@gmail.com>
2025-10-01 14:47:04 +0200

    Sync nameless conversion tests

--- UnitTests/tHOL_Printing.cpp
+++ UnitTests/tHOL_Printing.cpp
@@ -26,10 +26,6 @@ TermList mkConst(const std::string& name, TermList sort) {
   return TermList(Term::createConstant(nameIndex));
 }
 
-// TermList LAM(std::initializer_list<unsigned> vars, std::initializer_list<TermList> varSorts, Kernel::TypedTermList body) {
-//   return TermList(HOL::create::lambda(vars, varSorts, body));
-// }
-
 constexpr Options::HPrinting RAW = Options::HPrinting::RAW;
 constexpr Options::HPrinting DB_INDICES = Options::HPrinting::DB_INDICES;
 constexpr Options::HPrinting PRETTY = Options::HPrinting::PRETTY;
@@ -38,71 +34,140 @@ constexpr Options::HPrinting TPTP = Options::HPrinting::TPTP;
 #define LAM(...) TermList(HOL::create::lambda(__VA_ARGS__))
 #define APP(...) HOL::create::app(__VA_ARGS__)
 
+void runTest(const TermList& term, const std::map<Options::HPrinting, std::string>& reps) {
+  for (const auto& [printOpt, expected] : reps) {
+    env.options->setHolPrinting(printOpt);
+    ASS_EQ(term.toString(), expected)
+  }
+}
+
+class Defs {
+  static Defs* _instance;
+
+  Defs() {
+    srt = mkAtomicSort("srt");
+    fSrt = TermList(AtomicSort::arrowSort(srt, srt));
+    x0 = TermList::var(0);
+    x1 = TermList::var(1);
+    f = mkConst("f", fSrt);
+  }
+
+public:
+  static Defs* instance();
+  TermList srt, fSrt, x0, x1, f;
+};
+
+Defs* Defs::_instance = nullptr;
+
+Defs* Defs::instance()
+{
+  if (_instance == nullptr) {
+    _instance = new Defs();
+  }
+  return _instance;
+}
+
 TEST_FUN(hol_print_1) {
   env.setHigherOrder(true);
 
+  auto srt = Defs::instance()->srt;
+  auto fSrt = Defs::instance()->fSrt;
+  auto x0 = Defs::instance()->x0;
+  auto x1 = Defs::instance()->x1;
+  auto f = Defs::instance()->f;
 
-  auto srt = mkAtomicSort("srt");
-  auto fSrt = TermList(AtomicSort::arrowSort(srt, srt));
-  auto x0 = TermList::var(0);
-  auto x1 = TermList::var(1);
-  auto f = mkConst("f", fSrt);
+  runTest(
+    LAM({x0.var(), x1.var()}, {fSrt, srt}, {x1, srt}),
+    { {RAW,        "(^[X0 : (srt > srt), X1 : srt] : (X1))"},
+      {DB_INDICES, "(^[X0 : (srt > srt), X1 : srt] : (X1))"},
+      {PRETTY,     "(λX0 : (srt → srt), X1 : srt : (X1))"},
+      {TPTP,       "(^[X0 : (srt > srt), X1 : srt] : (X1))"} }
+  );
+  
+  runTest(
+    LAM({x0.var(), x1.var()}, {fSrt, srt}, {APP(fSrt, x0, x1), srt}),
+    { {RAW,        "(^[X0 : (srt > srt), X1 : srt] : (vAPP(srt,srt,X0,X1)))"},
+      {DB_INDICES, "(^[X0 : (srt > srt), X1 : srt] : ((X0 @ X1)))"},
+      {PRETTY,     "(λX0 : (srt → srt), X1 : srt : ((X0 X1)))"},
+      {TPTP,       "(^[X0 : (srt > srt), X1 : srt] : ((X0 @ X1)))"} }
+  );
 
-  std::list<std::pair<TermList, std::map<Options::HPrinting, std::string>>> tests;
+  runTest(
+    APP(f, x1),
+    { {RAW,        "vAPP(srt,srt,f,X1)"},
+      {DB_INDICES, "(f @ X1)"},
+      {PRETTY,     "(f X1)"},
+      {TPTP,       "(f @ X1)"} }
+  );
 
-  // tests.push_back({LAM({x0.var(), x1.var()}, {fSrt, srt}, {x1, srt}),
-  //                  {{RAW, "(^[X0 : (srt > srt), X1 : srt] : (X1))"},
-  //                   {DB_INDICES, "(^[X0 : (srt > srt), X1 : srt] : (X1))"},
-  //                   {PRETTY, "(λX0 : (srt → srt), X1 : srt : (X1))"},
-  //                   {TPTP, "(^[X0 : (srt > srt), X1 : srt] : (X1))"}}});
-  //
-  // tests.push_back({LAM({x0.var(), x1.var()}, {fSrt, srt}, {APP(fSrt, x0, x1), srt}),
-  //                 {{RAW, "(^[X0 : (srt > srt), X1 : srt] : (vAPP(srt,srt,X0,X1)))"},
-  //                  {DB_INDICES, "(^[X0 : (srt > srt), X1 : srt] : ((X0 @ X1)))"},
-  //                  {PRETTY, "(λX0 : (srt → srt), X1 : srt : ((X0 X1)))"},
-  //                  {TPTP, "(^[X0 : (srt > srt), X1 : srt] : ((X0 @ X1)))"}}});
+  runTest(
+    LAM({x1.var()}, {srt}, {APP(f, x1), srt}),
+    { {RAW,        "(^[X1 : srt] : (vAPP(srt,srt,f,X1)))"},
+      {DB_INDICES, "(^[X1 : srt] : ((f @ X1)))"},
+      {PRETTY,     "(λX1 : srt : ((f X1)))"},
+      {TPTP,       "(^[X1 : srt] : ((f @ X1)))"} }
+  );
 
-  tests.push_back({APP(f, x1),
-                  {{RAW, "vAPP(srt,srt,f,X1)"},
-                   {DB_INDICES, "(f @ X1)"},
-                   {PRETTY, "(f X1)"},
-                   {TPTP, "(f @ X1)"}}});
+  runTest(
+    LAM({x1.var()}, {fSrt}, {LAM({x1.var()}, {srt}, {x1, srt}), fSrt}),
+    { {RAW,        "(^[X1 : (srt > srt)] : ((^[X1 : srt] : (X1))))" }, 
+      {DB_INDICES, "(^[X1 : (srt > srt)] : ((^[X1 : srt] : (X1))))" }, 
+      {PRETTY,     "(λX1 : (srt → srt) : ((λX1 : srt : (X1))))" }, 
+      {TPTP,       "(^[X1 : (srt > srt)] : ((^[X1 : srt] : (X1))))" } }
+  );
+}
 
-  tests.push_back({LAM({x1.var()}, {srt}, {APP(f, x1), srt}),
-                  {{RAW, ""},
-                   {DB_INDICES, ""},
-                   {PRETTY, ""},
-                   {TPTP, ""}}});
+TEST_FUN(hol_print_2) {
+  env.setHigherOrder(true);
 
-  for (const auto& [term, opts] : tests) {
-    for (const auto& [printOpt, expected] : opts) {
-      env.options->setHolPrinting(printOpt);
-      std::cout << term.toString() << std::endl;
-      ASS_EQ(term.toString(), expected)
-    }
-  }
+  auto srt = Defs::instance()->srt;
+  auto fSrt = Defs::instance()->fSrt;
+  auto x0 = Defs::instance()->x0;
+  auto x1 = Defs::instance()->x1;
+  auto f = Defs::instance()->f;
 
-  // auto zero = LAM({x0.var(), x1.var()}, {fSrt, srt}, {x1, srt});
-  // ASS_EQ(zero.toString(true), "(^[X0 : (srt > srt), X1 : srt] : (X1))")
-  // ASS_EQ(HOL::convert::toNameless(zero).toString(true), "(^[Y0 : srt > srt]: ((^[Y1 : srt]: (Y1))))")
-  //
-  // auto one = LAM({x0.var(), x1.var()}, {fSrt, srt}, {HOL::create::app(fSrt, x0, x1), srt});
-  // ASS_EQ(one.toString(true), "(^[X0 : (srt > srt), X1 : srt] : ((X0 @ X1)))")
-  // ASS_EQ(HOL::convert::toNameless(one).toString(true), "(^[Y0 : srt > srt]: ((^[Y1 : srt]: (Y0 @ Y1))))")
-  //
-  // auto t1 = HOL::create::app(f, x1);
-  // ASS_EQ(t1.toString(true), "f @ X1")
-  // ASS_EQ(HOL::convert::toNameless(t1).toString(true), "f @ X1")
-  //
-  // auto t2 = LAM({x1.var()}, {srt}, {t1, srt});
-  // ASS_EQ(t2.toString(true), "(^[X1 : srt] : ((f @ X1)))")
-  // ASS_EQ(HOL::convert::toNameless(t2).toString(true), "(^[Y0 : srt]: (f @ Y0))")
-  //
-  // auto t3 = LAM({x1.var()}, {fSrt}, {LAM({x1.var()}, {srt}, {x1, srt}), fSrt});
-  // std::cout << t3.toString() << std::endl;
-  // // std::cout << SortHelper::getResultSort(t3.term()).toString() << std::endl;
-  // std::cout << HOL::convert::toNameless(t3).toString() << std::endl; // TODO
-  // // ASS_EQ(t3.toString(true), "(^[X1 : srt] : ((f @ X1)))")
+  using HOL::convert::toNameless;
+
+  runTest(
+    toNameless(LAM({x0.var(), x1.var()}, {fSrt, srt}, {x1, srt})),
+    { {RAW,        "vLAM(srt > srt,srt > srt,vLAM(srt,srt,db0(srt)))" },
+      {DB_INDICES, "(^db0 : srt > srt. ((^db1 : srt. (db0_0))))" },
+      {PRETTY,     "(λy0 : srt → srt. (λy1 : srt. y1))" },
+      {TPTP,       "(^[Y0 : srt > srt]: ((^[Y1 : srt]: (Y1))))" } }
+  );
+
+  runTest(
+    toNameless(LAM({x0.var(), x1.var()}, {fSrt, srt}, {APP(fSrt, x0, x1), srt})),
+    { {RAW,        "vLAM(srt > srt,srt > srt,vLAM(srt,srt,vAPP(srt,srt,db1(srt > srt),db0(srt))))" },
+      {DB_INDICES, "(^db0 : srt > srt. ((^db1 : srt. (db1_1 @ db0_0))))" },
+      {PRETTY,     "(λy0 : srt → srt. (λy1 : srt. (y0 y1)))" },
+      {TPTP,       "(^[Y0 : srt > srt]: ((^[Y1 : srt]: (Y0 @ Y1))))" } }
+  );
+
+  runTest(
+    toNameless(APP(f, x1)),
+    { {RAW,        "vAPP(srt,srt,f,X1)"},
+      {DB_INDICES, "(f @ X1)"},
+      {PRETTY,     "(f X1)"},
+      {TPTP,       "(f @ X1)"} }
+  );
+
+  runTest(
+    toNameless(LAM({x1.var()}, {srt}, {APP(f, x1), srt})),
+    { {RAW,        "vLAM(srt,srt,vAPP(srt,srt,f,db0(srt)))"},
+      {DB_INDICES, "(^db0 : srt. (f @ db0_0))"},
+      {PRETTY,     "(λy0 : srt. (f y0))"},
+      {TPTP,       "(^[Y0 : srt]: (f @ Y0))"} }
+  );
+
+  runTest(
+    toNameless(LAM({x1.var()}, {fSrt}, {LAM({x1.var()}, {srt}, {x1, srt}), fSrt})),
+    { {RAW,        "vLAM(srt > srt,srt > srt,vLAM(srt,srt,db0(srt)))" },
+      {DB_INDICES, "(^db0 : srt > srt. ((^db1 : srt. (db0_0))))" },
+      {PRETTY,     "(λy0 : srt → srt. (λy1 : srt. y1))" },
+      {TPTP,       "(^[Y0 : srt > srt]: ((^[Y1 : srt]: (Y1))))" }}
+  );
 }
 
 #undef LAM
+#undef APP
