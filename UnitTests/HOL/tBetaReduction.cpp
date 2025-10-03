61febff0c9156c0286c55b9a6f97b4cd3cf12b49
Matthias Hetzenberger <mhetzenberger@gmail.com>
2025-10-03 11:52:02 +0200

    More beta reduction tests

--- UnitTests/HOL/tBetaReduction.cpp
+++ UnitTests/HOL/tBetaReduction.cpp
@@ -15,6 +15,7 @@
 using namespace Test::HOL;
 using HOL::create::app;
 using HOL::convert::toNameless;
+using HOL::reduce::betaNF;
 
 TEST_FUN(beta_reduction_1) {
   env.setHigherOrder(true);
@@ -28,7 +29,7 @@ TEST_FUN(beta_reduction_1) {
 
   unsigned reds;
   for (const auto term : testTerms) {
-    auto reduced = HOL::reduce::betaNF(
+    auto reduced = betaNF(
       toNameless(app(D.srt, D.srt, id, term)), &reds
     );
 
@@ -53,7 +54,7 @@ TEST_FUN(beta_reduction_2) {
 
     for (const auto term2 : testTerms) {
       auto constFn = lam({D.x0, D.srt}, {term1, D.srt});
-      auto reduced = HOL::reduce::betaNF(
+      auto reduced = betaNF(
         toNameless(app(D.srt, D.srt, constFn, term2)), &reds
       );
 
@@ -77,7 +78,7 @@ TEST_FUN(beta_reduction_3) {
     auto t2 = toNameless(term);
     ASS(term == t2 && term.toString() == t2.toString())
 
-    auto reduced = HOL::reduce::betaNF(
+    auto reduced = betaNF(
       term, &reds
     );
 
@@ -94,7 +95,6 @@ TEST_FUN(beta_reduction_3) {
     termSort = TermList(AtomicSort::arrowSort(D.srt, termSort));
 
     for (const auto term : testTerms) {
-
       auto t2 = toNameless(term);
       ASS(term != t2 && term.toString() != t2.toString())
 
@@ -107,4 +107,33 @@ TEST_FUN(beta_reduction_3) {
       ASS_EQ(reduced.toString(), t2.toString())
     }
   }
+}
+
+TEST_FUN(beta_reduction_4) {
+  env.setHigherOrder(true);
+  const auto& D = *Defs::instance();
+
+  // (λ x0:(α -> α). x0 a) (λ x0:α. x0)
+  auto term = app(D.fSrt, D.srt, lam({D.x0, D.fSrt}, {app(D.srt, D.srt, D.x0, D.a), D.srt}), lam({D.x0, D.srt}, {D.x0, D.srt}));
+
+  auto t1 = toNameless(term);
+  ASS_EQ(t1.toString(true), "(^[Y0 : srt > srt]: (Y0 @ a)) @ (^[Y0 : srt]: (Y0))")
+  unsigned reds;
+  ASS_EQ(betaNF(t1, &reds), D.a)
+  ASS_EQ(reds, 2)
+
+  auto t2 = toNameless(app(D.srt, D.srt, D.f, term));
+  ASS_EQ(t2.toString(true), "f @ ((^[Y0 : srt > srt]: (Y0 @ a)) @ (^[Y0 : srt]: (Y0)))")
+  ASS_EQ(betaNF(t2, &reds), app(D.srt, D.srt, D.f, D.a))
+  ASS_EQ(reds, 2)
+
+  auto t3 = toNameless(app(D.srt, D.fSrt, lam({D.x0, D.srt}, D.f), term));
+  ASS_EQ(t3.toString(true), "(^[Y0 : srt]: (f)) @ ((^[Y0 : srt > srt]: (Y0 @ a)) @ (^[Y0 : srt]: (Y0)))")
+  ASS_EQ(betaNF(t3, &reds), D.f)
+  ASS_EQ(reds, 1)
+
+  auto t4 = toNameless(app(D.srt, D.srt, lam({D.x0, D.srt}, {D.x0, D.srt}), term));
+  ASS_EQ(t4.toString(true), "(^[Y0 : srt]: (Y0)) @ ((^[Y0 : srt > srt]: (Y0 @ a)) @ (^[Y0 : srt]: (Y0)))")
+  ASS_EQ(betaNF(t4, &reds), D.a)
+  ASS_EQ(reds, 3)
 }
