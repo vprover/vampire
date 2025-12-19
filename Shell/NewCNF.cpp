dd1833941db6c2158d91ac6b4733d4f2288f49ed
Michael Rawson <michael@rawsons.uk>
2025-12-19 19:08:45 +0000

    more UBSan fixes, look for this in sanity-check script

--- Shell/NewCNF.cpp
+++ Shell/NewCNF.cpp
@@ -402,10 +402,9 @@ TermList NewCNF::findITEs(TermList ts, Stack<unsigned> &variables, Stack<Formula
 bool NewCNF::shouldInlineITE(unsigned iteCounter) {
   /*
    * MS : TODO:
-   * Since "_iteInliningThreshold = (unsigned)ceil(log2(namingThreshold))",
-   * which evaluates to 0 for 0, and since "namingThreshold == 0" means
-   * "never name anything", it would make sense to add
-   * "|| _iteInliningThreshold == 0" here.
+   * Since _iteInliningThreshold is when namingThreshold is 0,
+   * and since "namingThreshold == 0" means "never name anything",
+   * it would make sense to add "|| _iteInliningThreshold == 0" here.
    */
   return _forInduction || iteCounter < _iteInliningThreshold;
 }
