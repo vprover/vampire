ad9e5adcf8fcd4129809d16d05997a29c20ea836
Martin Suda <martin.suda@cvut.cz>
2026-02-23 06:52:08 +0100

    bring in the FeatureIterator (will we want to use it?)

--- Shell/Property.hpp
+++ Shell/Property.hpp
@@ -53,13 +53,13 @@ public:
    * CASC category
    */
   enum Category {
+    FEQ, // FOL only
+    FNE, // FOL only
     NEQ,
     HEQ,
     PEQ,
     HNE,
     NNE,
-    FEQ,
-    FNE,
     EPR,
     UEQ
   };
@@ -255,14 +255,66 @@ public:
   unsigned sortsUsed() const { return _sortsUsed; }
   bool onlyFiniteDomainDatatypes() const { return _onlyFiniteDomainDatatypes; }
   bool knownInfiniteDomain() const { return _knownInfiniteDomain; }
-  
-  void setSMTLIBLogic(SMTLIBLogic smtLibLogic) { 
+
+  void setSMTLIBLogic(SMTLIBLogic smtLibLogic) {
     CALL("Property::setSMTLIBLogic");
-    _smtlibLogic = smtLibLogic; 
+    _smtlibLogic = smtLibLogic;
   }
-  SMTLIBLogic getSMTLIBLogic() const { 
-    return _smtlibLogic; 
+  SMTLIBLogic getSMTLIBLogic() const {
+    return _smtlibLogic;
   }
+
+  class FeatureIterator {
+    Property* _prop;
+    unsigned _featureId;
+    unsigned _log10Atoms = 0;
+
+  public:
+    inline static constexpr unsigned NUM_FEATURES = 15;
+
+    FeatureIterator(Property* prop) :
+      _prop(prop), _featureId(0) {}
+
+    bool hasNext() {
+      return _featureId < NUM_FEATURES;
+    }
+
+    float next() {
+      _featureId++;
+      switch (_featureId) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+          return _prop->category() == _featureId+1; // because the first two, 0 and 1, are FOL-only and we ignore them
+        case 8:
+          return (_prop->props() & PR_HAS_X_EQUALS_Y)>0;
+        case 9:
+          // function definitions should be gone (under the default fde=all), but for 1125 some remain even after elimination
+          return (_prop->props() & PR_HAS_FUNCTION_DEFINITIONS)>0;
+          // skipped theory stuff
+        case 10:
+          return (_prop->props() & PR_SORTS)>0; // there is around 1000 multisorted problems
+          // PR_ESSENTIALLY_GROUND; only 370, skipping
+        case 11:
+          _log10Atoms = log10(1+_prop->atoms());
+          return _log10Atoms == 0;
+        case 12:
+          return _log10Atoms == 1;
+        case 13:
+          return _log10Atoms == 2;
+        case 14:
+          return _log10Atoms == 3;
+        case 15:
+          return _log10Atoms > 3;
+        default:
+          ASSERTION_VIOLATION;
+      }
+    }
+  };
 
   bool allNonTheoryClausesGround(){ return _allNonTheoryClausesGround; }
 
