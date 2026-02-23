a139e3a1c9c158cc0339adba6ab0734db337828d
Martin Suda <martin.suda@cvut.cz>
2026-02-23 13:10:17 +0100

    more Random stuff

--- Lib/Random.hpp
+++ Lib/Random.hpp
@@ -58,11 +58,15 @@ public:
     return std::uniform_int_distribution<int>(0,modulus-1)(_eng[invisible]);
   }
 
+  static float getFloat(float min, float max, std::size_t invisible=0) {
+    return std::uniform_real_distribution<float>(min,max)(_eng[invisible]);
+  }
+
   static double getDouble(double min, double max, std::size_t invisible=0) {
     CALL("Random::getDouble");
     return std::uniform_real_distribution<double>(min,max)(_eng[invisible]);
   }
-  
+
   /**
    * Return a random bit.
    */
