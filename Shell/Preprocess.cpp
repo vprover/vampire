2f4580df30746c22c3ce18032c6ba89c6ff7f023
Martin Suda <martin.suda@cvut.cz>
2026-02-23 07:46:23 +0100

    also neural models like eating sine levels

--- Shell/Preprocess.cpp
+++ Shell/Preprocess.cpp
@@ -222,7 +222,8 @@ prb.getProperty();
     DistinctGroupExpansion().apply(prb);
   }
 
-  if (_options.sineToAge() || _options.useSineLevelSplitQueues() || (_options.sineToPredLevels() != Options::PredicateSineLevels::OFF)) {
+  if (_options.sineToAge() || _options.useSineLevelSplitQueues() || (_options.sineToPredLevels() != Options::PredicateSineLevels::OFF) ||
+       (!_options.neuralClauseEvaluationModel().empty())) { // our feature vector also includes the sine levels
     env.statistics->phase=Statistics::SINE_SELECTION;
 
     if (_options.sineToPredLevels() != Options::PredicateSineLevels::OFF) {
