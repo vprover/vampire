b7cc61ff490140356270850aba31191000e5bb45
Martin Suda <martin.suda@cvut.cz>
2026-03-05 16:28:24 +0100

    try it with absl::btree_set

--- Kernel/ClauseQueue.cpp
+++ Kernel/ClauseQueue.cpp
@@ -13,180 +13,49 @@
  * @since 30/12/2007 Manchester
  */
 
-
-#include "Lib/Allocator.hpp"
-#include "Lib/Random.hpp"
 #include "Clause.hpp"
 #include "ClauseQueue.hpp"
 
-#define MAX_HEIGHT 31
-
 using namespace std;
-using namespace Lib;
 using namespace Kernel;
 
 ClauseQueue::ClauseQueue()
-    : _height(0)
+    : _set(Comparator{this})
 {
-  void* mem = ALLOC_KNOWN(sizeof(Node)+MAX_HEIGHT*sizeof(Node*),
-          "ClauseQueue::Node");
-  _left = reinterpret_cast<Node*>(mem);
-  _left->nodes[0] = 0;
 }
 
-/** Temporary!!! */
-ClauseQueue::~ClauseQueue ()
+ClauseQueue::~ClauseQueue()
 {
-  removeAll();
+}
 
-  DEALLOC_KNOWN(_left,sizeof(Node)+MAX_HEIGHT*sizeof(Node*),"ClauseQueue::Node");
-} // ClauseQueue::~ClauseQueue
-
-/**
- * Bind @b v to @b t.
- * @pre @b v must previously be unbound
- */
 void ClauseQueue::insert(Clause* c)
 {
-  // select a random height between 0 and top
-  unsigned h = 0;
-  while (Random::getBit()) {
-    h++;
-  }
-  if (h > _height) {
-    if (_height < MAX_HEIGHT) {
-      _height++;
-    }
-    h = _height;
-    _left->nodes[h] = 0;
-  }
-  void* mem = ALLOC_KNOWN(sizeof(Node)+h*sizeof(Node*),
-			  "ClauseQueue::Node");
-  Node* newNode = reinterpret_cast<Node*>(mem);
-  newNode->clause = c;
+  _set.insert(c);
+}
 
-  // left is a node with a value smaller than that of newNode and having
-  // a large enough height.
-  // this node is on the left of the inserted one
-  Node* left = _left;
-  // lh is the height on which we search for the next node
-  unsigned lh = _height;
-  for (;;) {
-    Node* next = left->nodes[lh];
-    if (next == 0 || lessThan(c,next->clause)) {
-      if (lh <= h) {
-	left->nodes[lh] = newNode;
-	newNode->nodes[lh] = next;
-      }
-      if (lh == 0) {
-	return;
-      }
-      lh--;
-      continue;
-    }
-    left = next;
-  }
-} // ClauseQueue::insert
-
-/**
- * Remove the clause c from the queue.
- * @since 30/12/2007 Manchester
- */
 bool ClauseQueue::remove(Clause* c)
 {
-  unsigned h = _height;
-  Node* left = _left;
+  return _set.erase(c) > 0;
+}
 
-  for (;;) {
-    Node* next = left->nodes[h];
-    if (next && c == next->clause) {
-      unsigned height = h;
-      // found, first change the links going to next
-      for (;;) {
-	left->nodes[h] = next->nodes[h];
-	if (h == 0) {
-	  break;
-	}
-	h--;
-	while (left->nodes[h] != next) {
-	  left = left->nodes[h];
-	}
-      }
-      // deallocate the node
-      DEALLOC_KNOWN(next,
-		    sizeof(Node)+height*sizeof(Node*),
-		    "ClauseQueue::Node");
-      while (_height > 0 && ! _left->nodes[_height]) {
-	_height--;
-      }
-      return true;
-    }
-
-    if (next == 0 || lessThan(c,next->clause)) {
-      if(h==0) {
-
-#if VDEBUG
-       ClauseQueue::Iterator it(*this);
-       while(it.hasNext()){
-         ASS(it.next()!=c);
-       }
-#endif
-
-	return false;
-      }
-      h--;
-    }
-    else {
-      left = next;
-    }
-  }
-} // ClauseQueue::remove
-
-
-/**
- * Remove the leftmost clause c from the queue.
- * @since 30/12/2007 Manchester
- */
 Clause* ClauseQueue::pop()
 {
-  ASS(_height >= 0);
-  ASS(_left->nodes[0] != 0);
+  ASS(!_set.empty());
 
-  Node* node = _left->nodes[0];
-  unsigned h = 0;
-  _left->nodes[0] = node->nodes[0];
-  while (h < _height && _left->nodes[h+1] == node) {
-    h++;
-    _left->nodes[h] = node->nodes[h];
-  }
-  // now h is the height of the node
-  Clause* c = node->clause;
-
-  // deallocate the node
-  DEALLOC_KNOWN(node,
-		sizeof(Node)+h*sizeof(Node*),
-		"ClauseQueue::Node");
-  while (_height > 0 && ! _left->nodes[_height]) {
-    _height--;
-  }
-
+  auto it = _set.begin();
+  Clause* c = *it;
+  _set.erase(it);
   return c;
-} // ClauseQueue::pop
+}
 
-/**
- * Remove all clauses from the queue.
- * @since 31/12/2007 Manchester
- */
 void ClauseQueue::removeAll()
 {
-  while (_left->nodes[0]) {
-    pop();
-  }
-} // removeAll
+  _set.clear();
+}
 
 void ClauseQueue::output(std::ostream& str) const
 {
-  for (const Node* node = _left->nodes[0]; node; node=node->nodes[0]) {
-    str << node->clause->toString() << '\n';
+  for (Clause* c : _set) {
+    str << c->toString() << '\n';
   }
-} // ClauseQueue::output
+}
--- Kernel/ClauseQueue.hpp
+++ Kernel/ClauseQueue.hpp
@@ -17,21 +17,21 @@
 #ifndef __ClauseQueue__
 #define __ClauseQueue__
 
-#if VDEBUG
 #include <ostream>
-#endif
 
 #include "Debug/Assertion.hpp"
 
 #include "Lib/Reflection.hpp"
+
+#include "absl/container/btree_set.h"
 
 namespace Kernel {
 
 class Clause;
 
 /**
- * A clause queue organised as a skip list. The comparison of elements
- * is made using the virtual function compare.
+ * A clause priority queue backed by absl::btree_set for cache-locality.
+ * The comparison of elements is made using the virtual function lessThan.
  * @since 30/12/2007 Manchester
  */
 class ClauseQueue
@@ -45,25 +45,24 @@ public:
   Clause* pop();
   /** True if the queue is empty */
   bool isEmpty() const
-  { return _left->nodes[0] == 0; }
+  { return _set.empty(); }
   void output(std::ostream&) const;
 
   friend class Iterator;
 protected:
   /** comparison of clauses */
   virtual bool lessThan(Clause*,Clause*) = 0;
-  /** Nodes in the skip list */
-  class Node {
-  public:
-    /** Clause at this node */
-    Clause* clause;
-    /** Links to other nodes on the right, can be of any length */
-    Node* nodes[1];
+
+private:
+  struct Comparator {
+    ClauseQueue* queue;
+    bool operator()(Clause* a, Clause* b) const {
+      return queue->lessThan(a, b);
+    }
   };
-  /** Height of the leftmost node minus 1 */
-  unsigned _height;
-  /** the leftmost node with the dummy key and value */
-  Node* _left;
+
+  using SetType = absl::btree_set<Clause*, Comparator>;
+  SetType _set;
 
 public:
   /** Iterator over the queue
@@ -75,38 +74,24 @@ public:
 
     /** Create a new iterator */
     inline explicit Iterator(ClauseQueue& queue)
-      : _current(queue._left)
+      : _it(queue._set.begin()), _end(queue._set.end())
     {}
     /** true if there is a next clause */
     inline bool hasNext() const
-    { return _current->nodes[0]; }
+    { return _it != _end; }
     /** return the next clause */
     inline Clause* next()
     {
-      _current = _current->nodes[0];
-      ASS(_current);
-      return _current->clause;
+      ASS(_it != _end);
+      return *_it++;
     }
   private:
-    /** Current node */
-    Node* _current;
+    /** Current position */
+    SetType::const_iterator _it;
+    /** End position */
+    SetType::const_iterator _end;
   }; // class ClauseQueue::Iterator
 
-//  class DelIterator {
-//  public:
-//    explicit DelIterator(ClauseQueue& queue)
-//    { }
-//
-//    bool hasNext()
-//    { }
-//
-//    Clause* next()
-//    { }
-//
-//    void del()
-//    { }
-//  private:
-//  };
 }; // class ClauseQueue
 
 } // namespace Kernel
