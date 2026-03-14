876bcd94c6b7c577a6fbc477582ea6d65cf168bb
Marton Hajdu <mezpusz@hotmail.com>
2026-03-14 20:21:18 +0100

    TermFunIterator should only return function symbols, not type con symbols; some function definitions were missed due to this

--- Kernel/TermIterators.cpp
+++ Kernel/TermIterators.cpp
@@ -422,7 +422,7 @@ TermFunIterator::TermFunIterator (const Term* t)
 {
   _hasNext = true;
   _next = t->functor();
-  _stack.push(t->args());
+  _stack.push(t->termArgs());
 } // TermFunIterator::TermFunIterator
 
 
@@ -448,7 +448,7 @@ bool TermFunIterator::hasNext ()
     _hasNext = true;
     const Term* t = ts->term();
     _next = t->functor();
-    _stack.push(t->args());
+    _stack.push(t->termArgs());
     return true;
   }
   return false;
