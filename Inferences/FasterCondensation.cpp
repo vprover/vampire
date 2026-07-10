b3682fe54e3224f0ba15c15cb41d33b66a84bc4e
Marton Hajdu <mezpusz@hotmail.com>
2026-07-10 13:24:31 +0200

    Clean up FasterCondensation a bit; make some parts more efficient

--- Inferences/FasterCondensation.cpp
+++ Inferences/FasterCondensation.cpp
@@ -14,12 +14,9 @@
 
 #include "Kernel/Clause.hpp"
 #include "Kernel/Inference.hpp"
-#include "Kernel/Matcher.hpp"
 #include "Kernel/SubstHelper.hpp"
-#include "Kernel/Substitution.hpp"
 #include "Kernel/Term.hpp"
 #include "Kernel/TermIterators.hpp"
-#include <deque>
 
 #include "FasterCondensation.hpp"
 
@@ -78,87 +75,10 @@ bool match(Literal* base, Literal* instance, const DHSet<TermList>& instVars, Su
   return true;
 }
 
-TermList deref(TermList t, const Substitution& subst)
-{
-  while (t.isVar()) {
-    TermList res;
-    if (!subst.findBinding(t.var(), res)) {
-      break;
-    }
-    t = res;
-  }
-  return t;
-}
-
-bool occurs(unsigned var, TermList t, const Substitution& subst)
-{
-  Recycled<DHSet<TermList>> encountered;
-  Recycled<Stack<TermList>> todo;
-  todo->push(t);
-
-  while (todo->isNonEmpty()){
-    auto dt = deref(todo->pop(), subst);
-    if (!encountered->find(dt)) {
-      encountered->insert(dt);
-      if (dt.isVar()) {
-        if(dt.var() == var) {
-          return true;
-        }
-      } else {
-        todo->loadFromIterator(anyArgIter(dt.term()));
-      }
-    }
-  }
-
-  return false;
-}
-
-// TODO implement equality unification properly
-bool unify(Literal* lit, Literal* other, Substitution& subst, bool canBind)
-{
-  ASS_NEQ(lit, other);
-  if (lit->header() != other->header()) {
-    return false;
-  }
-
-  Stack<std::pair<TermList, TermList>> todo;
-  for (unsigned i = 0; i < lit->arity(); i++) {
-    todo.emplace(*lit->nthArgument(i), *other->nthArgument(i));
-  }
-
-  // Save encountered unification pairs to avoid
-  // recomputing their unification
-  DHSet<std::pair<TermList, TermList>> encountered;
-
-  while (todo.isNonEmpty()) {
-    auto [lhs, rhs] = todo.pop();
-    auto dt1 = deref(lhs, subst);
-    auto dt2 = deref(rhs, subst);
-    if (dt1 == dt2) {
-    } else if(dt1.isVar() && canBind && !occurs(dt1.var(), dt2, subst)) {
-      subst.bindUnbound(dt1.var(), dt2);
-    } else if(dt2.isVar() && canBind && !occurs(dt2.var(), dt1, subst)) {
-      subst.bindUnbound(dt2.var(), dt1);
-    } else if(dt1.isTerm() && dt2.isTerm() && dt1.term()->functor() == dt2.term()->functor()) {
-
-      for (unsigned i = 0; i < dt1.term()->arity(); i++) {
-        auto pair = std::make_pair(*dt1.term()->nthArgument(i), *dt2.term()->nthArgument(i));
-        if (encountered.insert(pair)) {
-          todo.emplace(pair);
-        }
-      }
-
-    } else {
-      return false;
-    }
-  }
-  return true;
-}
-
-bool instantiated(Literal* lit, const Substitution& subst)
+bool instantiated(const DHSet<TermList>& litVars, const Substitution& subst)
 {
   DEBUG("instantiated ", *lit);
-  return iterTraits(VariableIterator(lit)).any([&subst](TermList var) {
+  return iterTraits(litVars.iter()).any([&subst](TermList var) {
     TermList temp;
     auto res = subst.findBinding(var.var(), temp);
     if (res) {
@@ -169,85 +89,7 @@ bool instantiated(Literal* lit, const Substitution& subst)
   });
 }
 
-namespace {
-struct State {
-  Literal* toEliminate;
-  DHSet<Literal*> eliminated;
-  Substitution subst;
-  bool unifiedOnce = false;
-};
-
-std::ostream& operator<<(std::ostream& out, const State& state) {
-  return out << *state.toEliminate << " " << state.eliminated << " " << state.subst;
-}
-}
-
-// Clause* FasterCondensation::simplify(Clause* cl)
-// {
-//   if (cl->length() <= 1) {
-//     return cl;
-//   }
-
-//   std::deque<State> states;
-//   for (const auto& lit : *cl) {
-//     states.push_back({ .toEliminate = lit });
-//   }
-
-//   while (!states.empty()) {
-//     auto& curr = states.front();
-//     DEBUG("curr ", curr);
-//     for (const auto& other : *cl) {
-//       if (curr.eliminated.contains(other) || curr.toEliminate == other) {
-//         continue;
-//       }
-//       Substitution subst = curr.subst;
-//       if (!unify(curr.toEliminate, other, subst, !curr.unifiedOnce)) {
-//         continue;
-//       }
-//       DEBUG("unified ", *other, " new subst ", subst);
-
-//       Literal* newToEliminate = nullptr;
-//       DHSet<Literal*> eliminated;
-//       eliminated.loadFromIterator(curr.eliminated.iter());
-//       eliminated.insert(curr.toEliminate);
-//       for (const auto& other2 : *cl) {
-//         if (eliminated.contains(other2)) {
-//           continue;
-//         }
-//         if (instantiated(other2, subst)) {
-//           newToEliminate = other2;
-//           break;
-//         }
-//       }
-//       if (!newToEliminate) {
-//         return Clause::fromIterator(cl->iterLits().filter([&](Literal* lit) {
-//           auto res = !eliminated.contains(lit);
-//           ASS(!res || !instantiated(lit, subst));
-//           return res;
-//         }), SimplifyingInference1(InferenceRule::CONDENSATION, cl));
-//       }
-//       states.push_back({
-//         .toEliminate = newToEliminate,
-//         .eliminated = std::move(eliminated),
-//         .subst = std::move(subst),
-//         .unifiedOnce = true,
-//       });
-//     }
-//     states.pop_front();
-//   }
-
-//   return cl;
-// }
-
-DHSet<TermList> getForbiddenVars(const Substitution& subst)
-{
-  DHSet<TermList> res;
-  res.loadFromIterator(iterTraits(subst.items())
-    .flatMap([](auto arg) { return VariableIterator(arg.second); }));
-  return res;
-}
-
-bool tryExtend(Substitution& subst, const Substitution& other) {
+bool tryExtend(Substitution& subst, const DHSet<unsigned>& unchanged, const std::vector<DHSet<TermList>>& litVars, const Substitution& other, const DHSet<TermList>& unchanged2) {
   // in this case we would have duplicate literal removal
   ASS(!subst.isEmpty());
   ASS(!other.isEmpty());
@@ -255,18 +97,17 @@ bool tryExtend(Substitution& subst, const Substitution& other) {
   DEBUG("subst ", subst);
   DEBUG("other ", other);
 
-  auto forbidden1 = getForbiddenVars(subst);
-  auto forbidden2 = getForbiddenVars(other);
-
   for (const auto& [v,t] : iterTraits(subst.items())) {
     TermList tmp;
-    if (forbidden2.contains(TermList::var(v)) || (other.findBinding(v, tmp) && t != tmp)) {
+    if (unchanged2.contains(TermList::var(v)) || (other.findBinding(v, tmp) && t != tmp)) {
       return false;
     }
   }
   for (const auto& [v,t] : iterTraits(other.items())) {
-    if (forbidden1.contains(TermList::var(v))) {
-      return false;
+    for (const auto& i : iterTraits(unchanged.iter())) {
+      if (litVars[i].contains(TermList::var(v))) {
+        return false;
+      }
     }
     ALWAYS(subst.bind(v, t));
   }
@@ -289,7 +130,7 @@ Clause* FasterCondensation::simplify(Clause* cl)
     litVars[i].loadFromIterator(VariableIterator(lit));
   }
 
-  std::vector<Stack<Substitution>> litSubsts(cl->size());
+  std::vector<Stack<std::pair<Substitution, unsigned>>> litSubsts(cl->size());
 
   for (unsigned i = 0; i < cl->size(); i++) {
     auto lit = (*cl)[i];
@@ -304,7 +145,7 @@ Clause* FasterCondensation::simplify(Clause* cl)
       Substitution subst;
 
       if (match(lit, other, litVars[j], subst)) {
-        litSubsts[i].push(std::move(subst));
+        litSubsts[i].emplace(std::move(subst), j);
       }
       if (lit->isEquality()) {
         if (lit->isTwoVarEquality()) {
@@ -316,7 +157,7 @@ Clause* FasterCondensation::simplify(Clause* cl)
         if (match(lit->termArg(0), other->termArg(1), litVars[j], subst2) &&
             match(lit->termArg(1), other->termArg(0), litVars[j], subst2))
         {
-          litSubsts[i].push(std::move(subst2));
+          litSubsts[i].emplace(std::move(subst2), j);
         }
       }
     }
@@ -326,19 +167,23 @@ Clause* FasterCondensation::simplify(Clause* cl)
 
   struct State {
     Substitution subst;
-    LiteralStack unchanged;
+    DHSet<unsigned> unchanged; // indices of unchanged literals
     unsigned litI = 0;
     int substI = -1;
   };
 
   for (unsigned i = 0; i < cl->size(); i++) {
 
-    for (const auto& s : litSubsts[i]) {
+    for (const auto& [s,j] : litSubsts[i]) {
 
       DEBUG("trying with index ", i, " and subst ", s);
 
       Stack<State> todo;
-      todo.emplace(s, LiteralStack(), 0, -1);
+      {
+        DHSet<unsigned> unchanged;
+        unchanged.insert(j);
+        todo.emplace(s, std::move(unchanged), 0, -1);
+      }
 
       while (todo.isNonEmpty()) {
         auto curr = todo.pop();
@@ -349,11 +194,11 @@ Clause* FasterCondensation::simplify(Clause* cl)
         // arrived at the end of clause
         if (curr.litI >= cl->size()) {
           DEBUG("finalizing");
-          if (iterTraits(curr.unchanged.iter()).any([&curr](auto l) { return instantiated(l, curr.subst); })) {
+          if (iterTraits(curr.unchanged.iter()).any([&curr,&litVars](auto i) { return instantiated(litVars[i], curr.subst); })) {
             // some earlier literal has been instantiated, fail
-            continue;
+            INVALID_OPERATION("shouldn't happen");
           }
-          auto res = Clause::fromIterator(curr.unchanged.iter(), SimplifyingInference1(InferenceRule::CONDENSATION, cl));
+          auto res = Clause::fromIterator(iterTraits(curr.unchanged.iter()).map([cl](auto i) { return (*cl)[i]; }), SimplifyingInference1(InferenceRule::CONDENSATION, cl));
           if (!validateResult(cl, res, curr.subst)) {
             INVALID_OPERATION("incorrect faster condensation " + res->toString() + " from " + cl->toString());
           }
@@ -365,18 +210,21 @@ Clause* FasterCondensation::simplify(Clause* cl)
           continue;
         }
         // one option is to try the next substitution
-        todo.emplace(curr.subst, curr.unchanged, curr.litI, curr.substI+1);
+        DHSet<unsigned> unchanged;
+        unchanged.loadFromIterator(curr.unchanged.iter());
+        todo.emplace(curr.subst, std::move(unchanged), curr.litI, curr.substI+1);
         // the other option is to move to the next literal
         if (curr.substI == -1) {
-          if (!instantiated((*cl)[curr.litI], curr.subst)) {
+          if (!instantiated(litVars[curr.litI], curr.subst)) {
             DEBUG("unchanged ", (*cl)[curr.litI]);
-            curr.unchanged.push((*cl)[curr.litI]);
+            curr.unchanged.insert(curr.litI);
             curr.litI++;
             curr.substI = -1;
             todo.push(std::move(curr));
           }
-        } else if (tryExtend(curr.subst, litSubsts[curr.litI][curr.substI])) {
+        } else if (tryExtend(curr.subst, curr.unchanged, litVars, litSubsts[curr.litI][curr.substI].first, litVars[litSubsts[curr.litI][curr.substI].second])) {
           DEBUG("subst extended");
+          curr.unchanged.insert(litSubsts[curr.litI][curr.substI].second);
           curr.litI++;
           curr.substI = -1;
           todo.push(std::move(curr));
