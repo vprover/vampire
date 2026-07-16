ea89614521da0b03f09e4400d2819999d30a046f
Marton Hajdu <mezpusz@hotmail.com>
2026-07-16 14:33:48 +0200

    Output 'definition' for the eq.proxy definition too

--- Kernel/InferenceStore.cpp
+++ Kernel/InferenceStore.cpp
@@ -473,6 +473,7 @@ protected:
     case InferenceRule::FOOL_MATCH_DEFINITION:
     case InferenceRule::GENERAL_SPLITTING_COMPONENT:
     case InferenceRule::INEQUALITY_SPLITTING_NAME_INTRODUCTION:
+    case InferenceRule::EQUALITY_PROXY_DEFINITION:
       return "definition";
     default:
       return "plain";
@@ -894,8 +895,8 @@ protected:
     case InferenceRule::SKOLEMIZE:
     case InferenceRule::SKOLEM_SYMBOL_INTRODUCTION:
     case InferenceRule::EQUALITY_PROXY_REPLACEMENT:
-    case InferenceRule::EQUALITY_PROXY_AXIOM1:
-    case InferenceRule::EQUALITY_PROXY_AXIOM2:
+    case InferenceRule::EQUALITY_PROXY_DEFINITION:
+    case InferenceRule::EQUALITY_PROXY_AXIOM:
     case InferenceRule::NEGATED_CONJECTURE:
     case InferenceRule::RECTIFY:
     case InferenceRule::FLATTEN:
@@ -1556,8 +1557,8 @@ protected:
     case InferenceRule::SKOLEMIZE:
     case InferenceRule::SKOLEM_SYMBOL_INTRODUCTION:
     case InferenceRule::EQUALITY_PROXY_REPLACEMENT:
-    case InferenceRule::EQUALITY_PROXY_AXIOM1:
-    case InferenceRule::EQUALITY_PROXY_AXIOM2:
+    case InferenceRule::EQUALITY_PROXY_DEFINITION:
+    case InferenceRule::EQUALITY_PROXY_AXIOM:
     case InferenceRule::NEGATED_CONJECTURE:
     case InferenceRule::RECTIFY:
     case InferenceRule::FLATTEN:
